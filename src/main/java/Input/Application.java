ed5e8eb01f0b5232b6a34d15908bb847dd603355
MichalGuz <michalguz@gmail.com>
2019-05-27 01:13:01 +0200

    Create method main, create new object of InputString, execute method inputString()

--- src/main/java/Input/Application.java
+++ src/main/java/Input/Application.java
@@ -1,4 +1,8 @@
 package Input;
 
 public class Application {
+    public static void main(String[] args) {
+        InputString inputString = new InputString();
+        inputString.inputString();
+    }
 }
