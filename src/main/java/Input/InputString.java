63ea31384b3eb1c042d4f4dc06fc6641e5734e21
MichalGuz <michalguz@gmail.com>
2019-05-27 00:58:30 +0200

    Create sout method printing a prompt

--- src/main/java/Input/InputString.java
+++ src/main/java/Input/InputString.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class InputString {
     public void inputString(){
 
+//      create Scanner object
         Scanner input = new Scanner(System.in);
+
+//      output the prompt
+        System.out.println("Enter the line of text");
+
+
 
     }
 }
