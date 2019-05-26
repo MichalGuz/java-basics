225ffcd7432730d4262e733b9e2f36fbeb1f25ff
MichalGuz <michalguz@gmail.com>
2019-05-27 01:02:07 +0200

    Add necessary comment

--- src/main/java/Input/InputString.java
+++ src/main/java/Input/InputString.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class InputString {
     public void inputString(){
 
-//      create Scanner object
+        //create Scanner object
         Scanner input = new Scanner(System.in);
-
-//      output the prompt
+        //output the prompt
         System.out.println("Enter the line of text");
 
+        //wait for the user to enter a line of text
         String line = input.nextLine();
 
     }
