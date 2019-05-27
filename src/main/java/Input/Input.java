99b2dc48df9c340405358bd47b00e8e800f502c6
MichalGuz <michalguz@gmail.com>
2019-05-27 22:59:52 +0200

    Rename variable in method inputString, and create new object Scanner in method inputInt

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -6,12 +6,12 @@ public class Input {
     public void inputString(){
 
         //create Scanner object
-        Scanner input = new Scanner(System.in);
+        Scanner input0 = new Scanner(System.in);
         //output the prompt
         System.out.println("Enter the line of text");
 
         //wait for the user to enter a line of text
-        String line = input.nextLine();
+        String line = input0.nextLine();
 
         //Tell them what they entered
         System.out.println("You entered " + line);
@@ -19,6 +19,7 @@ public class Input {
 
     public void inputInt(){
 
+        Scanner input1 = new Scanner(System.in);
 
     }
 }
