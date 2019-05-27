2a9a57ca881d7b1be38ae103bad5f08902f7f351
MichalGuz <michalguz@gmail.com>
2019-05-27 23:04:23 +0200

    Create body of method inputInt

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -11,15 +11,21 @@ public class Input {
         System.out.println("Enter the line of text");
 
         //wait for the user to enter a line of text
-        String line = input0.nextLine();
+        String line0 = input0.nextLine();
 
         //Tell them what they entered
-        System.out.println("You entered " + line);
+        System.out.println("You entered " + line0);
     }
 
     public void inputInt(){
 
         Scanner input1 = new Scanner(System.in);
+
+        System.out.println("Enter the integer");
+
+        Integer line1 = input1.nextInt();
+
+        System.out.println("Your integer is: " + line1 );
 
     }
 }
