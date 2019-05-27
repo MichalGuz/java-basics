3d53f43f5ba087a47505359748865ef15fbda1bd
MichalGuz <michalguz@gmail.com>
2019-05-27 23:51:36 +0200

    Create methods of printing items from arrays to show the differences betweeen 1D and 2D array

--- src/main/java/arrays/MultiDimensionalArray.java
+++ src/main/java/arrays/MultiDimensionalArray.java
@@ -7,13 +7,21 @@ public class MultiDimensionalArray {
         // 1D array
         int[] values = {10, 20, 30, 40, 50};
 
+        // only need 1 index to access values
+        System.out.println("First item in array: " + values[0]);
+        System.out.println("Last item in array: " + values[4]);
+
         // 2D array
         int[][] grid = {
                 {10, 11, 12, 13},
                 {20, 21, 22, 23, 24},
                 {30, 31, 32},
-                {40, 41, 42, 43},
+                {40, 41, 42, 43, 44, 45},
         };
+
+        // need 2 indices to access values
+        System.out.println("First item in array: " + grid[0][0]);
+        System.out.println("Last item in array: " + grid[3][5]);
     }
 }
 
