2a0fba50ccc7fc3745d66bfeb579bfd1438d8353
MichalGuz <michalguz@gmail.com>
2019-05-27 23:40:35 +0200

    Create class for multidimensional arrays and two arrays for comparision 1D to 2D array

---
+++ src/main/java/arrays/MultiDimensionalArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class MultiDimensionalArray {
+
+    public void multiDimensionalArray(){
+
+        // 1D array
+        int[] values = {10, 20, 30, 40, 50};
+
+        // 2D array
+        int[][] grid = {
+                {10, 11, 12, 13},
+                {20, 21, 22, 23, 24},
+                {30, 31, 32},
+                {40, 41, 42, 43},
+        };
+    }
+}
+
