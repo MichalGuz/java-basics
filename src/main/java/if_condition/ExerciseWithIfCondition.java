9985cb16608d380906b364c1b5059101732868b8
MichalGuz <michalguz@gmail.com>
2019-05-28 21:54:41 +0200

    Create method process with first if-statement in the body

--- src/main/java/if_condition/ExerciseWithIfCondition.java
+++ src/main/java/if_condition/ExerciseWithIfCondition.java
@@ -2,4 +2,11 @@ package if_condition;
 
 public class ExerciseWithIfCondition {
     int value = 15;
+
+    public void process(){
+        if(value>10){
+            System.out.println("The value is bigger than ten");
+        }
+    }
+
 }
