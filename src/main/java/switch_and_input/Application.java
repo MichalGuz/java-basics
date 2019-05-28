9365ba85446a56e98ba46b461150bfa1c376f3e1
MichalGuz <michalguz@gmail.com>
2019-05-28 23:17:23 +0200

    Create new object of class Scanner to make inputs and practice the switch

--- src/main/java/switch_and_input/Application.java
+++ src/main/java/switch_and_input/Application.java
@@ -1,7 +1,9 @@
 package switch_and_input;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
     }
 }
