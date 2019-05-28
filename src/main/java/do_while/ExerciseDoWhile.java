91c3a6ec36a441ee384a96b3f1ad57b97505a811
MichalGuz <michalguz@gmail.com>
2019-05-28 22:54:30 +0200

    Create new Scanner object for inputs and do-while

--- src/main/java/do_while/ExerciseDoWhile.java
+++ src/main/java/do_while/ExerciseDoWhile.java
@@ -1,8 +1,18 @@
 package do_while;
 
+import java.util.Scanner;
+
 public class ExerciseDoWhile {
     public static void main(String[] args) {
 
         int value = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+
+        } while(value!=5);
+        System.out.println("Bingo! You got 5!");
+
     }
 }
