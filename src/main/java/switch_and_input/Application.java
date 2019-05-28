b78fe74a269d268a80d9d30ddc5272b44f1fbb46
MichalGuz <michalguz@gmail.com>
2019-05-28 23:40:35 +0200

    Initialize variable text to make inputs

--- src/main/java/switch_and_input/Application.java
+++ src/main/java/switch_and_input/Application.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the name:");
+
+        String text = input.nextLine();
     }
 }
