d6baca079853f2e381071132e3587a39213d96b3
MichalGuz <michalguz@gmail.com>
2019-05-28 21:59:07 +0200

    Create second if-statement in the body of method

--- src/main/java/if_condition/ExerciseWithIfCondition.java
+++ src/main/java/if_condition/ExerciseWithIfCondition.java
@@ -4,8 +4,10 @@ public class ExerciseWithIfCondition {
     int value = 15;
 
     public void process(){
-        if(value>10){
+        if(value > 10){
             System.out.println("The value is bigger than ten");
+        } else if(value < 30){
+            System.out.println("The value is bigger than 10 and less than 30");
         }
     }
 
