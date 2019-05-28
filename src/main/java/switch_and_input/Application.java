1aaa0a91091d525755c4b0388249216d1449b76c
MichalGuz <michalguz@gmail.com>
2019-05-28 23:59:37 +0200

    Create body of switch

--- src/main/java/switch_and_input/Application.java
+++ src/main/java/switch_and_input/Application.java
@@ -5,12 +5,20 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the name:");
+        System.out.println("Please enter the command:");
 
         String text = input.nextLine();
 
         switch(text){
+            case "start":
+                System.out.println("Program just started");
+                break;
 
+            case "stop":
+                System.out.println("Program has stopped");
+
+            default:
+                System.out.println("Command not recognized.\nTry one more time!");
         }
     }
 }
