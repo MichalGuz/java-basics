d15ddffadd44f576bda25edcbf4d5aa8f2fdd4ed
MichalGuz <michalguz@gmail.com>
2019-08-15 23:59:46 +0200

    Add invocation of method createNewDog. Add methods to  print the  names both of dogs.

--- src/main/java/pass_by_value/DogMain.java
+++ src/main/java/pass_by_value/DogMain.java
@@ -3,5 +3,8 @@ package pass_by_value;
 public class DogMain {
     public static void main(String[] args) {
         Dog dog = new Dog("Burek");
+        System.out.println(dog.getName() + 1); // "Burek"
+        dog.createNewDog(dog);
+        System.out.println(dog.getName()+2); // "Burek"
     }
 }
