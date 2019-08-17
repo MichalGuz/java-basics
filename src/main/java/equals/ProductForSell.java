fa0d2da3dac06f20df9925412068918116505a6e
MichalGuz <michalguz@gmail.com>
2019-08-17 15:46:04 +0200

    Create fields and constructor of class 'ProductForSell'.

--- src/main/java/equals/ProductForSell.java
+++ src/main/java/equals/ProductForSell.java
@@ -1,4 +1,11 @@
 package equals;
 
 public class ProductForSell {
+    private String name;
+    private int price;
+
+    public ProductForSell(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
 }
