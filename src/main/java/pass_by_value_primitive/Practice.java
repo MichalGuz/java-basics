1dafaf4f978f3a478a4d58403867b5f9e73e3a3a
MichalGuz <michalguz@gmail.com>
2019-08-17 14:40:50 +0200

    Create two fields and static method 'change' in class Practice.

--- src/main/java/pass_by_value_primitive/Practice.java
+++ src/main/java/pass_by_value_primitive/Practice.java
@@ -4,4 +4,13 @@ public class Practice {
     private static int increase(int i){
         return i+i;
     }
+
+    int a;
+    int b;
+
+    private static void change(Practice p){
+        p.a++;
+        p.b++;
+    }
+
 }
