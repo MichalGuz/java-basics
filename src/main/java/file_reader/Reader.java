9ef3e8bf2969e1a02060cd3307fd7763a0b0faaf
MichalGuz <michalguz@gmail.com>
2019-08-19 23:20:02 +0200

    Create block 'try' in class Reader.

--- src/main/java/file_reader/Reader.java
+++ src/main/java/file_reader/Reader.java
@@ -2,10 +2,19 @@ package file_reader;
 
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Reader {
     public void readFile(){
         ClassLoader cl = getClass().getClassLoader();
         File file = new File(cl.getResource("file_reader/textForFileReader.txt").getFile());
+        Path path = Paths.get(file.getPath());
+        try{
+            Stream<String> linesOfTextFromFile = Files.lines(path);
+        }
     }
 }
