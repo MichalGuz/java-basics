662a57b2833f11617eff4b5989e647c2023a6f9e
MichalGuz <michalguz@gmail.com>
2019-08-25 23:27:56 +0200

    Start stream of elements from array of ints in class SecondHighest.

--- src/main/java/first_attempt/SecondHighest.java
+++ src/main/java/first_attempt/SecondHighest.java
@@ -1,5 +1,11 @@
 package first_attempt;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SecondHighest {
     int[] nbs = {23, 43, 34, 65,45,55,43,57,53,43, 35, 59, 60, 63,72,21,31,33,53};
+
+    List<Integer> secHigh = Arrays.stream(nbs)
+
 }
