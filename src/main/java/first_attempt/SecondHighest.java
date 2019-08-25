76356de047418821ff31dbb4b2707840f4a20052
MichalGuz <michalguz@gmail.com>
2019-08-25 23:34:42 +0200

    Map ints to object in stream.

--- src/main/java/first_attempt/SecondHighest.java
+++ src/main/java/first_attempt/SecondHighest.java
@@ -2,10 +2,12 @@ package first_attempt;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SecondHighest {
     int[] nbs = {23, 43, 34, 65,45,55,43,57,53,43, 35, 59, 60, 63,72,21,31,33,53};
 
     List<Integer> secHigh = Arrays.stream(nbs)
-
+            .mapToObj(o -> o)
+            .
 }
