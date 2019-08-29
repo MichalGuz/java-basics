0e14f37bcd576d607419833bf02c476e822bced5
MichalGuz <michalguz@gmail.com>
2019-08-30 00:04:07 +0200

    Invoke the countDiffs() method.

--- src/main/java/smallest_difference/DifferenceArrayMain.java
+++ src/main/java/smallest_difference/DifferenceArrayMain.java
@@ -3,5 +3,6 @@ package smallest_difference;
 public class DifferenceArrayMain {
     public static void main(String[] args) {
         DifferenceArray differenceArray = new DifferenceArray();
+        differenceArray.coutnDiffs();
     }
 }
