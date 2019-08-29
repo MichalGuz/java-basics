bf8a1f67290005ef6007708541d92c9e6efb338d
MichalGuz <michalguz@gmail.com>
2019-08-30 00:04:31 +0200

    Invoke the findTheSmallest() method.

--- src/main/java/smallest_difference/DifferenceArrayMain.java
+++ src/main/java/smallest_difference/DifferenceArrayMain.java
@@ -4,5 +4,6 @@ public class DifferenceArrayMain {
     public static void main(String[] args) {
         DifferenceArray differenceArray = new DifferenceArray();
         differenceArray.coutnDiffs();
+        differenceArray.findTheSmallest();
     }
 }
