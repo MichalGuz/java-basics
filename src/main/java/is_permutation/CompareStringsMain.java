6c3c75a753801a0fe33cb070130458cea19346cc
MichalGuz <michalguz@gmail.com>
2019-09-01 23:29:29 +0200

    Create main method in the class CompareStringsMain.

--- src/main/java/is_permutation/CompareStringsMain.java
+++ src/main/java/is_permutation/CompareStringsMain.java
@@ -1,4 +1,8 @@
 package is_permutation;
 
 public class CompareStringsMain {
+    public static void main(String[] args) {
+        CompareStrings compareString = new CompareStrings();
+        compareString.result("qwecdAbwzee", "wzeAbcdeqwe");
+    }
 }
