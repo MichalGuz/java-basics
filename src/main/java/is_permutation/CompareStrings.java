8c6fb91c8625b2c8cb0289fa3e6f29dc803e40f7
MichalGuz <michalguz@gmail.com>
2019-09-01 22:56:07 +0200

    Add two empty chars in the body of method isPermutation().

--- src/main/java/is_permutation/CompareStrings.java
+++ src/main/java/is_permutation/CompareStrings.java
@@ -2,6 +2,8 @@ package is_permutation;
 
 public class CompareStrings {
     public boolean isPermutation(String firstString, String secondString){
+        char [] one = new char[firstString.length()];
+        char [] two = new char[secondString.length()];
         return true;
     }
 }
