add0d2054c59ae8aa5f570b00affd95b16236a0a
MichalGuz <michalguz@gmail.com>
2019-09-08 23:56:51 +0200

    Create local variable 'generator' of Random class  in the body of for loop.

--- src/main/java/smallest_difference_array_list/Difference.java
+++ src/main/java/smallest_difference_array_list/Difference.java
@@ -1,12 +1,14 @@
 package smallest_difference_array_list;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Difference {
     ArrayList<Integer> ints = new ArrayList<>();
 
     ArrayList<Integer> fill() {
         for(int i =0; i<5; i++){
+            Random generator = new Random();
         }
         return ints;
     }
