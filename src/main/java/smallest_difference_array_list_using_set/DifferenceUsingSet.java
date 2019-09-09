349d42148b453ae43976bb6f2004fa37ee842e40
MichalGuz <michalguz@gmail.com>
2019-09-09 22:03:06 +0200

    Past code copied from prior exercise.

--- src/main/java/smallest_difference_array_list_using_set/DifferenceUsingSet.java
+++ src/main/java/smallest_difference_array_list_using_set/DifferenceUsingSet.java
@@ -1,4 +1,34 @@
 package smallest_difference_array_list_using_set;
 
+import smallest_difference_array_list.ListCreator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DifferenceUsingSet {
+    ListCreator creator = new ListCreator();
+    ArrayList<Integer> ints = creator.fill();
+
+
+    List<Integer> diffs = new ArrayList<>();
+
+    int findSmallestDifference() {
+        for (int i = 0; i < ints.size(); i++) {
+            int temp = ints.get(i);
+            for (int j = 0; j < ints.size(); j++) {
+                int diff = temp - ints.get(j);
+                if(diff >= 0) {
+                    diffs.add(diff);
+                }
+            }
+        }
+        System.out.println("Unsorted collection of non-negative differences:\n" + diffs);
+        Collections.sort(diffs);
+        System.out.println("The sorted collection of non-negative differences:\n" + diffs);
+
+        int foundSmallestDifference = diffs.get(0);
+        System.out.println("The smallest non-negative difference is:" + foundSmallestDifference);
+        return foundSmallestDifference;
+    }
 }
