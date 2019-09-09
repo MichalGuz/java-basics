bd0f687dbf82c1cf5a30b5a36c59c1a1638783c5
MichalGuz <michalguz@gmail.com>
2019-09-09 23:36:28 +0200

    Amend the code in DifferenceUsingSet class to introduce HashSet.

--- src/main/java/smallest_difference_array_list_using_set/DifferenceUsingSet.java
+++ src/main/java/smallest_difference_array_list_using_set/DifferenceUsingSet.java
@@ -1,18 +1,17 @@
 package smallest_difference_array_list_using_set;
 import smallest_difference_array_list.ListCreator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DifferenceUsingSet {
     ListCreator creator = new ListCreator();
     ArrayList<Integer> ints = creator.fill();
 
+    Set<Integer> diffs = new HashSet<>();
 
-    List<Integer> diffs = new ArrayList<>();
-
-    int findSmallestDifference() {
+    void findSmallestDifference() {
+        System.out.println(ints.size());
         for (int i = 0; i < ints.size(); i++) {
             int temp = ints.get(i);
             for (int j = 0; j < ints.size(); j++) {
@@ -22,12 +21,5 @@ public class DifferenceUsingSet {
                 }
             }
         }
-        System.out.println("Unsorted collection of non-negative differences:\n" + diffs);
-        Collections.sort(diffs);
-        System.out.println("The sorted collection of non-negative differences:\n" + diffs);
-
-        int foundSmallestDifference = diffs.get(0);
-        System.out.println("The smallest non-negative difference is:" + foundSmallestDifference);
-        return foundSmallestDifference;
     }
 }
---
+++ src/main/java/smallest_difference_array_list_using_set/DifferenceUsingSetMain.java
@@ -0,0 +1,9 @@
+package smallest_difference_array_list_using_set;
+
+public class DifferenceUsingSetMain {
+    public static void main(String[] args) {
+        DifferenceUsingSet differenceUsingSet = new DifferenceUsingSet();
+        differenceUsingSet.findSmallestDifference();
+    }
+
+}
