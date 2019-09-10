ed559e41d80c9aa856c200e47515fb717cabea27
MichalGuz <michalguz@gmail.com>
2019-09-10 23:54:19 +0200

    Add body of overriden method sendMessage in class 'MessengerService'.

--- src/main/java/dependency_injection/message/journaldev/MessengerService.java
+++ src/main/java/dependency_injection/message/journaldev/MessengerService.java
@@ -3,5 +3,6 @@ package dependency_injection.message.journaldev;
 public class MessengerService implements SocialMediaService{
     @Override
     public void sendMessage(String msg, String rec) {
+        System.out.println("[" + msg + "] sent by Messenger to: " + rec);
     }
 }
