ca386c453f7e03a1321ed31324bf0407433516c2
MichalGuz <michalguz@gmail.com>
2019-09-11 00:02:17 +0200

    Create a field of class Application of type SocialMediaService.

--- src/main/java/dependency_injection/message/journaldev/Application.java
+++ src/main/java/dependency_injection/message/journaldev/Application.java
@@ -1,6 +1,8 @@
 package dependency_injection.message.journaldev;
 
 public class Application implements Executor {
+    private SocialMediaService socialMediaService;
+
     @Override
     public void performMethodSend(String msg, String rec){
 
