569dfceb681981612b0924c2ee4dcad1d828052a
MichalGuz <michalguz@gmail.com>
2019-09-11 00:03:44 +0200

    Create a constructor of class Application.

--- src/main/java/dependency_injection/message/journaldev/Application.java
+++ src/main/java/dependency_injection/message/journaldev/Application.java
@@ -3,6 +3,9 @@ package dependency_injection.message.journaldev;
 public class Application implements Executor {
     private SocialMediaService socialMediaService;
 
+    public Application(SocialMediaService serv){
+        this.socialMediaService = serv;
+    }
     @Override
     public void performMethodSend(String msg, String rec){
 
