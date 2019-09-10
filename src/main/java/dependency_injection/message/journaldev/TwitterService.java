39766a346ee62114b67bb23880a746cc4089fdfb
MichalGuz <michalguz@gmail.com>
2019-09-10 23:56:30 +0200

    Add body of overriden method sendMessage in class 'TwitterService'.

--- src/main/java/dependency_injection/message/journaldev/TwitterService.java
+++ src/main/java/dependency_injection/message/journaldev/TwitterService.java
@@ -3,5 +3,6 @@ package dependency_injection.message.journaldev;
 public class TwitterService implements SocialMediaService{
     @Override
     public void sendMessage(String msg, String rec) {
+        System.out.println("[" + msg + "] sent by Twitter to: " + rec);
     }
 }
