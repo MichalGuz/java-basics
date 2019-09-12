21775233f194bd294401ff4d1e301c06a8ab86b7
MichalGuz <michalguz@gmail.com>
2019-09-12 23:58:21 +0200

    Create a class TwitterInjector

--- src/main/java/dependency_injection/message/journaldev/TwitterInjector.java
+++ src/main/java/dependency_injection/message/journaldev/TwitterInjector.java
@@ -1,4 +1,8 @@
 package dependency_injection.message.journaldev;
 
-public class TwitterInjector {
+public class TwitterInjector implements SocialMediaServiceInjector {
+        @Override
+        public Executor execute(){
+            return new Application(new TwitterService());
+        }
 }
