23924a5fa263a008343730472ed5889bd3b74715
MichalGuz <michalguz@gmail.com>
2019-09-12 23:54:48 +0200

    Impelement class MessenjerInjector.

--- src/main/java/dependency_injection/message/journaldev/MessengerInjector.java
+++ src/main/java/dependency_injection/message/journaldev/MessengerInjector.java
@@ -1,5 +1,8 @@
 package dependency_injection.message.journaldev;
 
-public class MessengerInjector
-{
+public class MessengerInjector implements SocialMediaServiceInjector {
+    @Override
+    public Executor execute() {
+        return new Application(new MessengerService());
+    }
 }
