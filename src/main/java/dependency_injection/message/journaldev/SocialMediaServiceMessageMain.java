de16176e9bfd7bf7146e795ad987246815aa45f8
MichalGuz <michalguz@gmail.com>
2019-09-13 00:02:04 +0200

    Create new objects of class nad Executor, invoke methods performMethodSend().

--- src/main/java/dependency_injection/message/journaldev/SocialMediaServiceMessageMain.java
+++ src/main/java/dependency_injection/message/journaldev/SocialMediaServiceMessageMain.java
@@ -2,6 +2,10 @@ package dependency_injection.message.journaldev;
 
 public class SocialMediaServiceMessageMain {
     public static void main(String[] args) {
+        String messengerRec = "Captain America";
+        String messengerMsg = "Sup bro?";
+        String twittRec = "molk99";
+        String twittMsg = "WYL grab some beer tonight?";
 
     }
 }
