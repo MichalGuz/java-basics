7e24dcf2e5c44548a7eff22c8fc5d6307a46f7ea
MichalGuz <michalguz@gmail.com>
2019-09-24 23:55:30 +0200

    Override send() method in class EmailMessage.

--- src/main/java/dependency_injection/message/my/EmailMessage.java
+++ src/main/java/dependency_injection/message/my/EmailMessage.java
@@ -1,4 +1,8 @@
 package dependency_injection.message.my;
 
 public class EmailMessage implements Message{
+    @Override
+    public void send(String receiver, String text){
+        System.out.println("Email " + text + " has been sent to " + receiver);
+    }
 }
