f22e375bd2e43235e9250203ec06c0fd390d7ab9
MichalGuz <michalguz@gmail.com>
2019-09-25 00:02:02 +0200

    Class EmailMessage implements interface Message.

--- src/main/java/dependency_injection/message/my/SmsMessage.java
+++ src/main/java/dependency_injection/message/my/SmsMessage.java
@@ -1,6 +1,6 @@
 package dependency_injection.message.my;
 
-public class SmsMessage implements EmailMessage{
+public class SmsMessage implements Message{
     @Override
     public void send(String receiver, String text){
         System.out.println("Sms message " + text + "has been sent to " + receiver);
