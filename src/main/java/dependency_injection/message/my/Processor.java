68b5847a71508c1381e1c72c1400217109d9fab5
MichalGuz <michalguz@gmail.com>
2019-09-26 22:45:34 +0200

    Add constructor of class Processor.

--- src/main/java/dependency_injection/message/my/Processor.java
+++ src/main/java/dependency_injection/message/my/Processor.java
@@ -2,4 +2,8 @@ package dependency_injection.message.my;
 
 public class Processor implements MessageProcessor{
    private Message message;
+
+   public Processor(Message mess) {
+       this.message = mess;
+   }
 }
