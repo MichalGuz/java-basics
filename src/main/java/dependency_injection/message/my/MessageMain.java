4e1d806cd0a5f76a530decb5e43a6f979ac3e3fe
MichalGuz <michalguz@gmail.com>
2019-09-27 21:26:13 +0200

    Create main method with four new variables.

---
+++ src/main/java/dependency_injection/message/my/MessageMain.java
@@ -0,0 +1,10 @@
+package dependency_injection.message.my;
+
+public class MessageMain {
+    public static void main(String[] args) {
+        String receiver = "Man@man.com";
+        String text = "Hi Man!";
+        String receiverSms = "696 679 777";
+        String message = "Hi M!";
+    }
+}
