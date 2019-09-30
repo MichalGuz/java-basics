a298063d7ba3a6f8c3d7236a205630b3cd531b5b
MichalGuz <michalguz@gmail.com>
2019-09-30 23:52:30 +0200

    Create constructor of class Product and get() methods.

--- src/main/java/equals_products/Product.java
+++ src/main/java/equals_products/Product.java
@@ -4,4 +4,16 @@ public class Product {
     String name;
     double price;
 
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
