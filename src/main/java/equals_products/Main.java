c8bd797cd9152c9c0ce0ab24872a97edfcb7bbb2
MichalGuz <michalguz@gmail.com>
2019-10-01 23:22:36 +0200

    Create new objects of class Food and Drink in the body of class  method main().

--- src/main/java/equals_products/Main.java
+++ src/main/java/equals_products/Main.java
@@ -2,6 +2,13 @@ package equals_products;
 
 public class Main {
     public static void main(String[] args) {
+        Product p1 = new Drink("chopin", 30);
+        Product p4 = new Drink("name", 10);
+        Product p2 = new Food("chopin", 30);
+        Product p3 = new Food("chopi", 3);
+        Drink d1 = new Drink("chopin", 30);
+        Food f1 = new Food("chopin", 30);
+        Food f2 = new Food("chopin", 30);
 
     }
 }
