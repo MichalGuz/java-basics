44e3eac1cad31c3d9d7b8e72989aeca870912006
MichalGuz <michalguz@gmail.com>
2019-10-01 23:27:31 +0200

    Add methods of println() in the body of method Main.

--- src/main/java/equals_products/Main.java
+++ src/main/java/equals_products/Main.java
@@ -10,5 +10,15 @@ public class Main {
         Food f1 = new Food("chopin", 30);
         Food f2 = new Food("chopin", 30);
 
+        Drink d1 = new Drink("chopin", 30);
+        Food f1 = new Food("chopin", 30);
+        Food f2 = new Food("chopin", 30);
+        System.out.println(p1.equals(p1) + " [ " + p1.name + " = " + p1.name + ", " + p1.price + " = " + p1.price + "]" + "comparing classes:  " + p1.getClass() + " : " + p1.getClass());
+        System.out.println(p1.equals(p2) + " [ " + p1.name + " = " + p2.name + ", " + p1.price + " = " + p2.price + "]" + "comparing classes:  " + p1.getClass() + " : " + p2.getClass());
+        System.out.println(p2.equals(p3) + " [ " + p2.name + " = " + p3.name + ", " + p2.price + " = " + p3.price + "]" + "comparing classes:  " + p2.getClass() + " : " + p3.getClass());
+        System.out.println(p1.equals(p4) + " [ " + p1.name + " = " + p4.name + ", " + p1.price + " = " + p4.price + "]" + "comparing classes:  " + p1.getClass() + " : " + p4.getClass());
+        System.out.println(d1.equals(f1) + " [ " + d1.name + " = " + f1.name + ", " + d1.price + " = " + f1.price + "]" + "comparing classes:  " + d1.getClass() + " : " + f1.getClass());
+        System.out.println(f1.equals(f2) + " [ " + f1.name + " = " + f2.name + ", " + f1.price + " = " + f2.price + "]" + "comparing classes:  " + f1.getClass() + " : " + f2.getClass());
+
     }
 }
