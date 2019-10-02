f38d00a68af05f434a33bd2f5b2d917378603d67
MichalGuz <michalguz@gmail.com>
2019-10-02 23:47:28 +0200

    Add fields in class BuiderBob

--- src/main/java/builder_bob_practice/BuilderBob.java
+++ src/main/java/builder_bob_practice/BuilderBob.java
@@ -1,5 +1,11 @@
 package builder_bob_practice;
 
+import java.util.List;
+
 public class BuilderBob {
+    private String name;
+    private int id;
+    private boolean busy;
+    private List<BuilderTool> tools;
 
 }
