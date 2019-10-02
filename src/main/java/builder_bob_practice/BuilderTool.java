5fcde0682723d13a604ec0bd1516c77887f85791
MichalGuz <michalguz@gmail.com>
2019-10-02 23:52:44 +0200

    Override hashCode() method in class BuiderTool.

--- src/main/java/builder_bob_practice/BuilderTool.java
+++ src/main/java/builder_bob_practice/BuilderTool.java
@@ -1,5 +1,7 @@
 package builder_bob_practice;
 
+import static java.util.Objects.hash;
+
 public class BuilderTool {
     private String tool;
 
@@ -22,5 +24,10 @@ public class BuilderTool {
         if (o == null || getClass() != o.getClass()) return false;
         BuilderTool that = (BuilderTool) o;
         return getTool().equals(that.getTool());
+    }
+
+    @Override
+    public int hashCode(){
+        return 31 * hash(tool);
     }
 }
