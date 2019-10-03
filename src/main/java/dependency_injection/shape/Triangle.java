72a7c1d07b3da02d6ccd17351d5d11369e40f1c0
MichalGuz <michalguz@gmail.com>
2019-10-03 23:11:07 +0200

    Amend Triangle class to implement the interface Shape.

--- src/main/java/dependency_injection/shape/Triangle.java
+++ src/main/java/dependency_injection/shape/Triangle.java
@@ -1,8 +1,9 @@
 package dependency_injection.shape;
 
-public class Triangle extends Figure {
+public class Triangle implements Shape {
     @Override
-    public void draw() {
+    public String draw() {
         System.out.println("This is a triangle");
+        return "This is a triangle";
     }
 }
