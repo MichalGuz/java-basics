0808a555af4c1a053c6ed8df637ebedb67922137
MichalGuz <michalguz@gmail.com>
2019-10-03 22:24:36 +0200

    Create class Circle with overriden draw() method.

---
+++ src/main/java/dependency_injection/message/shape/Circle.java
@@ -0,0 +1,8 @@
+package dependency_injection.message.shape;
+
+public class Circle extends Figure {
+    @Override
+    public void draw(){
+        System.out.println("This is a circle");
+    }
+}
