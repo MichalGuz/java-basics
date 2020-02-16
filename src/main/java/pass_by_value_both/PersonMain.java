08790e1ffa14713bfaeb3ab388bddddca10b1e29
MichalGuz <michalguz@gmail.com>
2020-02-16 23:59:54 +0100

    Call priintln() method to print in console name of both Person objectss.

--- src/main/java/pass_by_value_both/PersonMain.java
+++ src/main/java/pass_by_value_both/PersonMain.java
@@ -4,5 +4,9 @@ public class PersonMain {
     public static void main(String[] args) {
         Person adam = new Person();
         Person roman = new Person("Adam");
-    }
+
+        System.out.println(adam.getName());
+        System.out.println(roman.getName());
+
+        }
 }
