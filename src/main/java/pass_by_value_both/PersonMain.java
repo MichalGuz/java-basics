568d9601baa371feacbdfaafdf2987c8e935a2c2
MichalGuz <michalguz@gmail.com>
2020-02-16 23:59:28 +0100

    Create main() method and two new Person objects in PersonVerifier class.

--- src/main/java/pass_by_value_both/PersonMain.java
+++ src/main/java/pass_by_value_both/PersonMain.java
@@ -1,4 +1,8 @@
 package pass_by_value_both;
 
 public class PersonMain {
+    public static void main(String[] args) {
+        Person adam = new Person();
+        Person roman = new Person("Adam");
+    }
 }
