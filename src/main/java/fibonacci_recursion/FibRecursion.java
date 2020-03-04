353d0f2bd9665f84614cf1554754b01f4b3e8fe6
MichalGuz <michalguz@gmail.com>
2020-03-04 23:36:13 +0100

    Create and implement findFibonacciNumber(int n) method  iside class FibRecursion.

--- src/main/java/fibonacci_recursion/FibRecursion.java
+++ src/main/java/fibonacci_recursion/FibRecursion.java
@@ -1,4 +1,11 @@
 package fibonacci_recursion;
 
 public class FibRecursion {
+    public static int findFibonacciNumber(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return findFibonacciNumber(n - 1) + findFibonacciNumber((n));
+        }
+    }
 }
