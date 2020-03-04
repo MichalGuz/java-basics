d62b0d32f94bccc99236e4de65ffc7c51c564487
MichalGuz <michalguz@gmail.com>
2020-03-04 23:42:57 +0100

    Create new Scanner object (to get input from console) in main() method of class FibRecursion.

--- src/main/java/fibonacci_recursion/FibRecursion.java
+++ src/main/java/fibonacci_recursion/FibRecursion.java
@@ -1,5 +1,7 @@
 package fibonacci_recursion;
 
+import java.util.Scanner;
+
 public class FibRecursion {
     public static int findFibonacciNumber(int n) {
         if (n == 1 || n == 2) {
@@ -10,6 +12,7 @@ public class FibRecursion {
     }
 
     public static void main(String[] args) {
-
+        System.out.println("Which number from Fibonacci sequence do you want to see?");
+        Scanner input = new Scanner(System.in);
     }
 }
