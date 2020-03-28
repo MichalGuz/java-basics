79e75f3b1ab07b2a545b7c5a52395a33ccc691e9
MichalGuz <michalguz@gmail.com>
2020-03-28 18:31:36 +0100

    Create new empty ArrayList inside main() method of class BuilderBobMain.

--- src/main/java/builder_bob_practice/BuilderBobMain.java
+++ src/main/java/builder_bob_practice/BuilderBobMain.java
@@ -1,7 +1,10 @@
 package builder_bob_practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuilderBobMain {
     public static void main(String[] args) {
-
+        List<BuilderTool> listOfTools = new ArrayList<>();
     }
 }
