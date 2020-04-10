d8cf0296e1eced39ffc3b1ac2481f0205d8e7739
MichalGuz <michalguz@gmail.com>
2020-04-10 20:05:17 +0200

    Create 'digit' variable initialized by modulo of 'n' variable inside method squareDigitsCalc

--- src/main/java/squareDigitsCalculation/SquareDigitsCalculation.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalculation.java
@@ -4,9 +4,14 @@ public class SquareDigitsCalculation {
     private static int TEN  = 10;
     public int squareDigitsCalc(int n) {
         int result = 0;
+
         if (n < TEN) {
             result = n * n;
         }
+
+        int digit = n % TEN;
+        System.out.println("digit = " + digit);
+
         return result;
     }
 }
