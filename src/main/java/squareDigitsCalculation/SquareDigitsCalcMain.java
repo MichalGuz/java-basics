f2456ff46a00bbab6aa13da8fcefbc30adbeded3
MichalGuz <michalguz@gmail.com>
2020-04-10 21:33:39 +0200

    Call squareDigitsCalc() method inside main() method of class SquareDigitsCalcMain.

--- src/main/java/squareDigitsCalculation/SquareDigitsCalcMain.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalcMain.java
@@ -1,7 +1,9 @@
 package squareDigitsCalculation;
 
+import static squareDigitsCalculation.SquareDigitsCalculation.squareDigitsCalc;
+
 public class SquareDigitsCalcMain {
     public static void main(String[] args) {
-
+        int firstSquaredDigits = squareDigitsCalc(12345);
     }
 }
