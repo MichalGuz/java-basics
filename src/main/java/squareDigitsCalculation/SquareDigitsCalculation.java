f624199ea67544371fe7e47eee9c4aa827475d9f
MichalGuz <michalguz@gmail.com>
2020-04-10 20:02:08 +0200

    Create 'if' condition inside method squareDigitsCalc

--- src/main/java/squareDigitsCalculation/SquareDigitsCalculation.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalculation.java
@@ -4,6 +4,9 @@ public class SquareDigitsCalculation {
     private static int TEN  = 10;
     public int squareDigitsCalc(int n) {
         int result = 0;
+        if (n < TEN) {
+            result = n * n;
+        }
         return result;
     }
 }
