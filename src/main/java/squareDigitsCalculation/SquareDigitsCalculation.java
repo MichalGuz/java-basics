17cacafb783e8ddc1bd38b81d2705655d555f1d3
MichalGuz <michalguz@gmail.com>
2020-04-10 19:59:25 +0200

    Create private static int TEN inside class SquareDigitsCalculation

--- src/main/java/squareDigitsCalculation/SquareDigitsCalculation.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalculation.java
@@ -1,6 +1,7 @@
 package squareDigitsCalculation;
 
 public class SquareDigitsCalculation {
+    private static int TEN  = 10;
     public int squareDigitsCalc(int n) {
         int result = 0;
         return result;
