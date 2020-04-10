f29e271c5a0568e4c16c49d71d2de99e3b8c4683
MichalGuz <michalguz@gmail.com>
2020-04-10 21:55:11 +0200

    Call squareDigitsCalcConcisely() method inside SquareDigitsCalcMain class.

--- src/main/java/squareDigitsCalculation/SquareDigitsCalcMain.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalcMain.java
@@ -1,9 +1,11 @@
 package squareDigitsCalculation;
 
 import static squareDigitsCalculation.SquareDigitsCalculation.squareDigitsCalc;
+import static squareDigitsCalculation.SquareDigitsCalculationConcise.squareDigitsCalcConcisely;
 
 public class SquareDigitsCalcMain {
     public static void main(String[] args) {
         int firstSquaredDigits = squareDigitsCalc(12345);
+        int secondSquaredDigits = squareDigitsCalcConcisely(2035602);
     }
 }
