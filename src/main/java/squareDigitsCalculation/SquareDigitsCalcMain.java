a5d37f0eeb5fb718e0de269359d4956444882659
MichalGuz <michalguz@gmail.com>
2020-04-11 23:02:08 +0200

    Call the squareDigitsCalcWhileLoop(int n) method in SquareDigitsCalcMain class.

--- src/main/java/squareDigitsCalculation/SquareDigitsCalcMain.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalcMain.java
@@ -2,12 +2,13 @@ package squareDigitsCalculation;
 
 import static squareDigitsCalculation.SquareDigitsCalculation.squareDigitsCalc;
 import static squareDigitsCalculation.SquareDigitsCalculationConcise.squareDigitsCalcConcisely;
+import static squareDigitsCalculation.SquareDigitsCalculationWhileLoop.squareDigitsCalcWhileLoop;
 
 public class SquareDigitsCalcMain {
     public static void main(String[] args) {
         int firstSquaredDigits = squareDigitsCalc(12345);
         int secondSquaredDigits = squareDigitsCalcConcisely(2035602);
-
+        int thirdSquaredDigits = squareDigitsCalcWhileLoop(540213);
 
     }
 }
