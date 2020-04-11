a6bee1b14c7a36379b016dea046ad5af7f8796b7
MichalGuz <michalguz@gmail.com>
2020-04-11 22:36:57 +0200

    Println the statement about the runnig of loop with current value of 'digit' variable in squareDigitCalcWhileLoop(int n) method.

--- src/main/java/squareDigitsCalculation/SquareDigitsCalculationWhileLoop.java
+++ src/main/java/squareDigitsCalculation/SquareDigitsCalculationWhileLoop.java
@@ -1,11 +1,13 @@
-package squareDigitsCalculation;
+ package squareDigitsCalculation;
 
 public class SquareDigitsCalculationWhileLoop {
     public int squareDigitCalcWhileLoop(int n) {
         String result = "";
         while (n != 0) {
             System.out.println("Start the loop: n = " + n);
+
             int digit = n % 10;
+            System.out.println("Loop is running: digit = " + digit);
         }
     }
 }
