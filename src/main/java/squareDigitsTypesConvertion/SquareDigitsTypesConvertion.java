b3bbb7ce351c8d6f22134452ec1868f5502df469
MichalGuz <michalguz@gmail.com>
2020-04-12 20:18:22 +0200

    Add println() method [to display an array] inside squareDigitsWithTypesConversion( int n) method.

--- src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertion.java
+++ src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertion.java
@@ -5,6 +5,7 @@ public class SquareDigitsTypesConvertion {
         System.out.println("Number to square its digits: " + n);
         String number = String.valueOf(n);
         String[] arrayOfDigits = number.split("");
+        System.out.println(arrayOfDigits);
         int temporary = arrayOfDigits.length;
         return temporary;
     }
