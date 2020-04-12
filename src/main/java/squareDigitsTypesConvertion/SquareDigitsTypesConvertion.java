831874576646868c21fd001ad1e08d4e4b5f7137
MichalGuz <michalguz@gmail.com>
2020-04-12 20:10:31 +0200

    Create array of Strings by calling the String.split() method inside squareDigitsWithTypesConversion( int n) method.

--- src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertion.java
+++ src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertion.java
@@ -4,5 +4,6 @@ public class SquareDigitsTypesConvertion {
     public int squareDigitsWithTypesConvertion( int n){
         System.out.println("Number to square its digits: " + n);
         String number = String.valueOf(n);
+        String[] arrayOfDigits = number.split("");
     }
 }
