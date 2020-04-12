2c8f1098ae506ffe9c0d3019359da971658333fe
MichalGuz <michalguz@gmail.com>
2020-04-12 22:23:19 +0200

    Amend the body of squareDigitsWithTypesConversion() method: change the print methods.

--- src/main/java/squareDigitsTypesConversion/SquareDigitsTypesConversion.java
+++ src/main/java/squareDigitsTypesConversion/SquareDigitsTypesConversion.java
@@ -3,11 +3,16 @@ package squareDigitsTypesConversion;
 public class SquareDigitsTypesConversion {
     public int squareDigitsWithTypesConversion(int n){
         System.out.println("Number to square its digits: " + n);
+
         String number = String.valueOf(n);
+
         String[] arrayOfDigits = number.split("");
+
+        System.out.println("Array of digits contains: ");
         for(String a: arrayOfDigits){
-            System.out.println(a);
+            System.out.print(a);
         }
+
         int temporary = arrayOfDigits.length;
         return temporary;
     }
