54bcbfcac50bc3546088f54d84b8a8ee0dc40a41
MichalGuz <michalguz@gmail.com>
2020-04-12 20:08:04 +0200

    Initialize String variable by calling the valueOf(n) method inside squareDigitsWithTypesConversion( int n) method.

--- src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertion.java
+++ src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertion.java
@@ -3,5 +3,6 @@ package squareDigitsTypesConvertion;
 public class SquareDigitsTypesConvertion {
     public int squareDigitsWithTypesConvertion( int n){
         System.out.println("Number to square its digits: " + n);
+        String number = String.valueOf(n);
     }
 }
