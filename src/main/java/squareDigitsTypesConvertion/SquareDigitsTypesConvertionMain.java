afa6fce9a4038cec2555a4b68fec93cd4c9eb366
MichalGuz <michalguz@gmail.com>
2020-04-12 20:20:23 +0200

    Create main() method and new object of SquareDigitsTypesConversion class inside SquareDigitsTypesConversionMain class.

--- src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertionMain.java
+++ src/main/java/squareDigitsTypesConvertion/SquareDigitsTypesConvertionMain.java
@@ -1,4 +1,7 @@
 package squareDigitsTypesConvertion;
 
 public class SquareDigitsTypesConvertionMain {
+    public static void main(String[] args) {
+        SquareDigitsTypesConvertion temp = new SquareDigitsTypesConvertion();
+    }
 }
