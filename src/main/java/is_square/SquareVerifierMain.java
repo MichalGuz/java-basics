98115a7b30391d367f09a5b146de9e08ef9b6fd9
MichalGuz <michalguz@gmail.com>
2020-04-14 22:22:10 +0200

    Call Arrays.fill() method inside loop 'for' to fill an array with numbers from 0 to 121.

--- src/main/java/is_square/SquareVerifierMain.java
+++ src/main/java/is_square/SquareVerifierMain.java
@@ -1,9 +1,12 @@
 package is_square;
 
+import java.util.Arrays;
+
 public class SquareVerifierMain {
     public static void main(String[] args) {
         int[] numbers = new int[122];
         for (int i = 0; i < numbers.length; i++) {
+            Arrays.fill(numbers, i, i+1, i);
         }
     }
 }
