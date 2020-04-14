68dc017c98e58f66bc5483c1d62743921ef025b8
MichalGuz <michalguz@gmail.com>
2020-04-14 22:07:52 +0200

    Add 'else if' condition in isSquare(int p) method.

--- src/main/java/is_square/SquareVerifier.java
+++ src/main/java/is_square/SquareVerifier.java
@@ -4,6 +4,9 @@ public class SquareVerifier {
     public static boolean isSquare(int p) {
         double n = Math.sqrt(p);
         if (p <= 0) {
+            return false;
+        } else if (p / n == n && p % n == 0) {
+            return true;
         }
     }
 }
