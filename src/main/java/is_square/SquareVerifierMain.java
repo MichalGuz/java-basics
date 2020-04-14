2427f74869a64635194f5b279d202c31bfde770c
MichalGuz <michalguz@gmail.com>
2020-04-14 22:19:40 +0200

    Create loop 'for' inside main() method inside SquareVerifierMain class.

--- src/main/java/is_square/SquareVerifierMain.java
+++ src/main/java/is_square/SquareVerifierMain.java
@@ -3,5 +3,7 @@ package is_square;
 public class SquareVerifierMain {
     public static void main(String[] args) {
         int[] numbers = new int[122];
+        for (int i = 0; i < numbers.length; i++) {
+        }
     }
 }
