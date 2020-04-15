233fc1520fb131fcee3a88ff58b0e042f686fabe
MichalGuz <michalguz@gmail.com>
2020-04-15 22:47:05 +0200

    Add return statement inside 'if' condition.

--- src/main/java/is_square/SquareVerifierPositive.java
+++ src/main/java/is_square/SquareVerifierPositive.java
@@ -3,6 +3,7 @@ package is_square;
 public class SquareVerifierPositive {
     public static boolean isSquareP(int n) {
         if (n < 0 || Math.sqrt(n) % 1 != 0 ) {
+            return false;
         }
     }
 }
