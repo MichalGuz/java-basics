8e97ba5e4b1b35617c17a5813282177403ffb789
MichalGuz <michalguz@gmail.com>
2020-04-15 22:21:23 +0200

    Change conditions in 'if' and 'if else' blocks of method isSquare(int n).

--- src/main/java/is_square/SquareVerifier.java
+++ src/main/java/is_square/SquareVerifier.java
@@ -3,9 +3,9 @@ package is_square;
 public class SquareVerifier {
     public static boolean isSquare(int p) {
         double n = Math.sqrt(p);
-        if (p <= 0) {
+        if (p < 0) {
             return false;
-        } else if ( p % n == 0) {
+        } else if (p == 0 || p % n == 0) {
             return true;
         }
         return false;
