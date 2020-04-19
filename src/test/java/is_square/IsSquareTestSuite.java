3569afeb51df84b3457c1de86502bc06db5421b5
MichalGuz <michalguz@gmail.com>
2020-04-19 21:42:44 +0200

    Add 'given' section with examples of initialized int variables inside testIsSquareConciseWithExamples() method.

--- src/test/java/is_square/IsSquareTestSuite.java
+++ src/test/java/is_square/IsSquareTestSuite.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 public class IsSquareTestSuite {
     @Test
     public void testIsSquareConciseWithExamples() {
+        // given
+        int negative = -1;
+        int zero = 0;
+        int one = 1;
+        int three = 3;
+        int nine = 9;
+        int sixteen = 16;
+        int seventeen = 17;
 
     }
 }
