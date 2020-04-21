b888cd6643882cfe762826a2c68c733d4f99033f
MichalGuz <michalguz@gmail.com>
2020-04-21 22:00:33 +0200

    Create 'for each' loop in createPhoneNumber(int[]n) method.

--- src/main/java/array_phone_number/PhoneNumberCreator.java
+++ src/main/java/array_phone_number/PhoneNumberCreator.java
@@ -3,6 +3,9 @@ package array_phone_number;
 public class PhoneNumberCreator {
     String number = "";
     public String createPhoneNumber(int[] n) {
+        for(int num: n) {
+
+        }
 
     }
 }
