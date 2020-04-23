967563906a03bc667f6cadaff9f428d7ed719b45
MichalGuz <michalguz@gmail.com>
2020-04-23 21:20:53 +0200

    Create a few different String objects inside 'given' section of testcompareAmountOfXAndOLetters() method.

--- src/test/java/boolean_letters_counter/BooleanLettersCounterTestSuite.java
+++ src/test/java/boolean_letters_counter/BooleanLettersCounterTestSuite.java
@@ -7,6 +7,13 @@ public class BooleanLettersCounterTestSuite {
     public void testcompareAmountOfXAndOLetters() {
         // given
         BooleanLettersCounter b = new BooleanLettersCounter();
+        String s1 = "xo";
+        String s2 = "xxo";
+        String s3 = "zqq";
+        String s4 = "abcdoqweoazsdxplerx";
+        String s5 = "ooooooooooooo";
+        String s6 = "xxxxxxx";
+        String s7 = "xpoxoppoxopxpoxxopxopxopxpoxpoopxopx";
 
     }
 }
