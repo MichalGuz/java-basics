0621abffe84cfb9478c4a8f349cb508944f2efe1
MichalGuz <michalguz@gmail.com>
2020-04-23 21:04:17 +0200

    Initialize two int variables - both letters' counters - in compareAmountOfXAndOLetters(String n).

--- src/main/java/boolean_letters_counter/BooleanLettersCounter.java
+++ src/main/java/boolean_letters_counter/BooleanLettersCounter.java
@@ -1,7 +1,9 @@
 package boolean_letters_counter;
 
 public class BooleanLettersCounter {
-    public boolean check(String n) {
+    public boolean compareAmountOfXAndOLetters(String n) {
+        int xCounter = 0;
+        int oCounter = 0;
         return false;
     }
 
