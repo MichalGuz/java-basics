4c208e3485089129d285eee60cbe9ba35c45643d
MichalGuz <michalguz@gmail.com>
2020-05-01 23:09:47 +0200

    Add a comment with task descrition in MumbledLetters class..

--- src/main/java/mumbledLetters/MumbledLetters.java
+++ src/main/java/mumbledLetters/MumbledLetters.java
@@ -1,4 +1,13 @@
 package mumbledLetters;
 
 public class MumbledLetters {
+
+    //  Write function accum
+    //  Examples:
+    //      accum("abcd") -> "A-Bb-Ccc-Dddd"
+    //      accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+    //      accum("cwAt") -> "C-Ww-Aaa-Tttt"
+    //  The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
 }
