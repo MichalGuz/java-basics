6d913dbd05ecb61647a267ca52d836e6cad96f63
MichalGuz <michalguz@gmail.com>
2020-05-02 19:04:08 +0200

    Amend the code by calling toUpperCase() method in second loop 'for' in the body of accum() method.

--- src/main/java/mumbled_letters/MumbledLetters.java
+++ src/main/java/mumbled_letters/MumbledLetters.java
@@ -20,7 +20,7 @@ public class MumbledLetters {
         for (int i = 0; i < letters.length; i++ ){
             String retrievedLetter = Character.toString((char) Array.get(letters, i));
             for (int j = 1; j <= counter; j++) {
-                temporaryString = retrievedLetter.toLowerCase();
+                temporaryString = retrievedLetter.toUpperCase();
                 if ( j > 1) {
                     for (int k = 1; k < counter; k++) {
                         temporaryString = temporaryString + retrievedLetter;
