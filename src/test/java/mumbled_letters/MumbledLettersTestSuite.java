6ea2e02c5d590ab123fefa300d5ebfb382a45394
MichalGuz <michalguz@gmail.com>
2020-05-02 18:55:41 +0200

    Import static method accum() and create 'when' section in testAccum() method. Call the accum() method.

--- src/test/java/mumbled_letters/MumbledLettersTestSuite.java
+++ src/test/java/mumbled_letters/MumbledLettersTestSuite.java
@@ -2,10 +2,15 @@ package mumbled_letters;
 
 import org.junit.Test;
 
+import static mumbled_letters.MumbledLetters.accum;
+
 public class MumbledLettersTestSuite {
     @Test
     public void testAccum() {
         // given
         String n = "AbCd";
+
+        // when
+        accum(n);
     }
 }
