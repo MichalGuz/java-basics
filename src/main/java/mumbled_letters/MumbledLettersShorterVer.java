ef9eb2afe917f834a2798ddb4798442a1c8ca89f
MichalGuz <michalguz@gmail.com>
2020-05-05 22:32:36 +0200

    Initialize String variable in acuumShorterVer() method.

--- src/main/java/mumbled_letters/MumbledLettersShorterVer.java
+++ src/main/java/mumbled_letters/MumbledLettersShorterVer.java
@@ -3,5 +3,6 @@ package mumbled_letters;
 public class MumbledLettersShorterVer {
     public static String acuumShorterVer(String s) {
         char[] charArray = s.toLowerCase().toCharArray();
+        String str = "";
     }
 }
