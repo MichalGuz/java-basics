2b2a87ca2bd2fc5d9b2a9634d1cec39cedda9c05
MichalGuz <michalguz@gmail.com>
2020-05-08 23:18:30 +0200

    Initialize int variable in accumStringBuild() method.

--- src/main/java/mumbled_letters/MumbledLettersWithStringBuilder.java
+++ src/main/java/mumbled_letters/MumbledLettersWithStringBuilder.java
@@ -3,5 +3,6 @@ package mumbled_letters;
 public class MumbledLettersWithStringBuilder {
     public static String accumStringBuild(String s) {
         StringBuilder bldr = new StringBuilder();
+        int i = 0;
     }
 }
