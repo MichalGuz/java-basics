4ade4d09914dd34c1199baa50e21b2cd8bba8730
MichalGuz <michalguz@gmail.com>
2020-05-08 23:19:21 +0200

    Create 'for each' loop (with call of toCharArray() method) in accumStringBuild() method.

--- src/main/java/mumbled_letters/MumbledLettersWithStringBuilder.java
+++ src/main/java/mumbled_letters/MumbledLettersWithStringBuilder.java
@@ -4,5 +4,8 @@ public class MumbledLettersWithStringBuilder {
     public static String accumStringBuild(String s) {
         StringBuilder bldr = new StringBuilder();
         int i = 0;
+        for(char c : s.toCharArray()) {
+
+        }
     }
 }
