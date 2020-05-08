a6cfcf6964d59eae0bf102a503d6b2e41ef11faa
MichalGuz <michalguz@gmail.com>
2020-05-08 23:17:33 +0200

    Create new StringBuilder object in accumStringBuild() method.

--- src/main/java/mumbled_letters/MumbledLettersWithStringBuilder.java
+++ src/main/java/mumbled_letters/MumbledLettersWithStringBuilder.java
@@ -2,6 +2,6 @@ package mumbled_letters;
 
 public class MumbledLettersWithStringBuilder {
     public static String accumStringBuild(String s) {
-
+        StringBuilder bldr = new StringBuilder();
     }
 }
