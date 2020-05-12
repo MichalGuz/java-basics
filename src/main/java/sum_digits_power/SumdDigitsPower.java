91ecee22735d5d6bbfa71d44bfa8cc0f2cc4a25d
MichalGuz <michalguz@gmail.com>
2020-05-12 23:05:48 +0200

    Create two empty ArrayLists in sumDigitsPower() method.

--- src/main/java/sum_digits_power/SumdDigitsPower.java
+++ src/main/java/sum_digits_power/SumdDigitsPower.java
@@ -1,6 +1,11 @@
 package sum_digits_power;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumdDigitsPower {
     public static Long[] sumDigitsPower(Long a, Long b) {
+        List<Long> inputNums = new ArrayList<>();
+        List<Long> outputInts = new ArrayList<>();
     }
 }
