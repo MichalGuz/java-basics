bed8e677509c3c795094474eeb0787ebc5c56174
MichalGuz <michalguz@gmail.com>
2020-05-15 13:02:42 +0200

    Delete unusefull 'if' condition in 'for' loop and extend another 'if' condition (to eliminate 0 from output) in last 'for' loop in sumDigitsPower() method.

--- src/main/java/sum_digits_power/SumdDigitsPower.java
+++ src/main/java/sum_digits_power/SumdDigitsPower.java
@@ -8,32 +8,28 @@ public class SumdDigitsPower {
     public static Long[] sumDigitsPower(Long a, Long b) {
         List<Long> inputNums = new ArrayList<>();
         List<Long> outputInts = new ArrayList<>();
-        List<Long>  powered = new ArrayList<>();
+        List<Long> powered = new ArrayList<>();
         Long[] inputArray = new Long[inputNums.size()];
         Long[] output = new Long[outputInts.size()];
         Long[] poweredInts = new Long[powered.size()];
 
-        for (long i = a; i < b+1; i++) {
+        for (long i = a; i < b + 1; i++) {
             inputNums.add(i);
         }
-        for (Long x: inputNums) {
-            if (x < 10 && x > 0) {
-                powered.add(x);
-            } else {
-                long temp = 0;
-                double poweredTemp = 0;
-                long sum = 0;
-                do {
-                    temp = x % 10;
-                    poweredTemp = Math.pow(temp, x.toString().length());
-                    sum += poweredTemp;
-                    x /= 10;
-                } while (x >= 1);
-                powered.add(sum);
-            }
+        for (Long x : inputNums) {
+            long temp = 0;
+            double poweredTemp = 0;
+            long sum = 0;
+            do {
+                temp = x % 10;
+                poweredTemp = Math.pow(temp, x.toString().length());
+                sum += poweredTemp;
+                x /= 10;
+            } while (x >= 1);
+            powered.add(sum);
         }
         for (int y = 0; y < inputNums.size(); y++) {
-            if (inputNums.get(y).equals(powered.get(y))) {
+            if (inputNums.get(y).equals(powered.get(y)) && inputNums.get(y) != 0) {
                 Long q = inputNums.get(y);
                 outputInts.add(q);
             }
@@ -44,5 +40,9 @@ public class SumdDigitsPower {
         System.out.println("outputInts ArrayList = " + Arrays.toString(outputInts.toArray(output)));
 
         return output;
+    }
+
+    public static void main(String[] args) {
+        sumDigitsPower(-1L, 135L);
     }
 }
