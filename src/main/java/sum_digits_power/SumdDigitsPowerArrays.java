1f81e43acfe4a22b38581022bed610ffbc667e93
MichalGuz <michalguz@gmail.com>
2020-05-16 21:44:59 +0200

    Create three new arrays in sumDigitsPower() method in SumdDigitsPowerArrays class.

--- src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
+++ src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
@@ -4,5 +4,8 @@ import java.util.List;
 
 public class SumdDigitsPowerArrays {
     public static List<Long> sumDigitsPower(long a, long b) {
+        long[] inputArray = new long[(int)((b-a)+1L)];
+        long[] output = new long[(int)((b-a)+1L)];
+        long[] poweredInts = new long[(int)((b-a)+1L)];;
     }
 }
