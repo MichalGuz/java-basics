bd33a06e37e83db1e16d4e9248153024fc869aec
MichalGuz <michalguz@gmail.com>
2020-05-16 21:43:45 +0200

    Create sumDigitsPower() method in SumdDigitsPowerArrays class.

--- src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
+++ src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
@@ -1,5 +1,8 @@
 package sum_digits_power;
 
-public class SumdDigitsPowerArrays {
+import java.util.List;
 
+public class SumdDigitsPowerArrays {
+    public static List<Long> sumDigitsPower(long a, long b) {
+    }
 }
