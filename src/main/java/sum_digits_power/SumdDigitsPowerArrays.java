a6ff7404d50e5fca4709433ad28a3ead03a423f2
MichalGuz <michalguz@gmail.com>
2020-05-16 21:50:03 +0200

    Increament value of 'index1' in 'for' loop in sumDigitsPower() method.

--- src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
+++ src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
@@ -13,6 +13,7 @@ public class SumdDigitsPowerArrays {
         int index1 = 0;
         for (long i = a; i < b + 1; i++) {
             fill(inputArray, index1, index1+1, i);
+            index1++;
         }
     }
 }
