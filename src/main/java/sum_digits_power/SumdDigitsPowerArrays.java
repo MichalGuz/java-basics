e1c6830383ec4fc4b54c9fc19da7fa4338abad87
MichalGuz <michalguz@gmail.com>
2020-05-16 21:48:29 +0200

    Initialize variable 'index1' and inside 'for' loop call Arrays.fill() method to fill the first array in sumDigitsPower() method.

--- src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
+++ src/main/java/sum_digits_power/SumdDigitsPowerArrays.java
@@ -2,13 +2,17 @@ package sum_digits_power;
 
 import java.util.List;
 
+import static java.util.Arrays.fill;
+
 public class SumdDigitsPowerArrays {
     public static List<Long> sumDigitsPower(long a, long b) {
         long[] inputArray = new long[(int)((b-a)+1L)];
         long[] output = new long[(int)((b-a)+1L)];
         long[] poweredInts = new long[(int)((b-a)+1L)];;
 
+        int index1 = 0;
         for (long i = a; i < b + 1; i++) {
+            fill(inputArray, index1, index1+1, i);
         }
     }
 }
