fe6d93ae14a57026d1ea2c4ef72d8657748f5176
MichalGuz <michalguz@gmail.com>
2020-05-18 22:19:28 +0200

    Initilize int 'temp' value in loop 'for' in reverseArray(int[] n) method in ReverseArray class: 'temp' = first element of array.

--- src/main/java/reverse_sequence_of_data/ReverseArray.java
+++ src/main/java/reverse_sequence_of_data/ReverseArray.java
@@ -3,7 +3,7 @@ package reverse_sequence_of_data;
 public class ReverseArray {
     public static int[] reverseArray(int[] n) {
         for (int i = 0; i < n.length/2; i++ ) {
-
+            int temp = n[i];
         }
     }
 }
