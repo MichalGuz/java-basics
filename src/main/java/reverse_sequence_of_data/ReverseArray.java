ae04629a576d88b3e0615db9186f6e377ff55a3f
MichalGuz <michalguz@gmail.com>
2020-05-18 22:32:19 +0200

    Call the reverseArray() method and println() method two times in main() method in ReverseArray class.

--- src/main/java/reverse_sequence_of_data/ReverseArray.java
+++ src/main/java/reverse_sequence_of_data/ReverseArray.java
@@ -1,5 +1,7 @@
 package reverse_sequence_of_data;
 
+import java.util.Arrays;
+
 public class ReverseArray {
     public static int[] reverseArray(int[] n) {
         for (int i = 0; i < n.length/2; i++ ) {
@@ -15,5 +17,10 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] exmpl = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 
+        System.out.println(Arrays.toString(exmpl));
+
+        reverseArray(exmpl);
+
+        System.out.println(Arrays.toString(exmpl));
     }
 }
