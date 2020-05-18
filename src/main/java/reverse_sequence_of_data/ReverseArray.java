40423a728f5f7b26809a77000ec016a39bfec656
MichalGuz <michalguz@gmail.com>
2020-05-18 22:24:30 +0200

    Swap elements of array in loop 'for' in reverseArray(int[] n) method: first element has value of the last.

--- src/main/java/reverse_sequence_of_data/ReverseArray.java
+++ src/main/java/reverse_sequence_of_data/ReverseArray.java
@@ -4,6 +4,8 @@ public class ReverseArray {
     public static int[] reverseArray(int[] n) {
         for (int i = 0; i < n.length/2; i++ ) {
             int temp = n[i];
+            // swap elements
+            n[i] = n[n.length - 1 - i];
         }
     }
 }
