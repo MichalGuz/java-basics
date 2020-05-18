4b9cff6ad45ce7613916f96d463d98fabbca9b5d
MichalGuz <michalguz@gmail.com>
2020-05-18 21:58:29 +0200

    Create loop 'for' in reverseArray(int[] n) method in ReverseArray class. Range of loop is less than lenght of int[] n divided by 2.

--- src/main/java/reverse_sequence_of_data/ReverseArray.java
+++ src/main/java/reverse_sequence_of_data/ReverseArray.java
@@ -2,6 +2,8 @@ package reverse_sequence_of_data;
 
 public class ReverseArray {
     public static int[] reverseArray(int[] n) {
+        for (int i = 0; i < n.length/2; i++ ) {
 
+        }
     }
 }
