7fc1e3c76b093dd1061e89eebd0b6d1ef525070d
MichalGuz <michalguz@gmail.com>
2020-05-19 15:46:30 +0200

    Call println() method in the body of method printNumber() in class PassByValueNumberProcessor.

--- src/main/java/pass_by_value_primitive/PassByValueProcessor.java
+++ src/main/java/pass_by_value_primitive/PassByValueProcessor.java
@@ -3,5 +3,6 @@ package pass_by_value_primitive;
 public class PassByValueProcessor {
     public static void printNumber(PassByValueNumber number) {
         number.setValue(number.getValue() + 1);
+        System.out.println("Value after call the printNumber method = " + number.getValue());
     }
 }
