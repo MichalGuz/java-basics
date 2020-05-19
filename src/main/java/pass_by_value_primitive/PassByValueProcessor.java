88dd528dcde3355f3036fae8af8c73b787f8152b
MichalGuz <michalguz@gmail.com>
2020-05-19 15:48:49 +0200

    Create printInt() method in class PassByValueNumberProcessor.

--- src/main/java/pass_by_value_primitive/PassByValueProcessor.java
+++ src/main/java/pass_by_value_primitive/PassByValueProcessor.java
@@ -1,8 +1,13 @@
 package pass_by_value_primitive;
 
 public class PassByValueProcessor {
-    public static void printNumber(PassByValueNumber number) {
+    public void printNumber(PassByValueNumber number) {
         number.setValue(number.getValue() + 1);
         System.out.println("Value after call the printNumber method = " + number.getValue());
+    }
+
+    public void printInt( int n) {
+        n++;
+        System.out.println("n after the call of printInt method = " + n);
     }
 }
