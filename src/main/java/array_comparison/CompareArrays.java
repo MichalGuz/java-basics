d92252cbe4483ab9b3ea6a982a90e6b8f9432cf6
MichalGuz <michalguz@gmail.com>
2020-05-19 19:33:10 +0200

    Remove useless  Object[] arrays from compareArraysInt() method in CompareArrays class.

--- src/main/java/array_comparison/CompareArrays.java
+++ src/main/java/array_comparison/CompareArrays.java
@@ -6,9 +6,7 @@ public class CompareArrays {
     public static void compareArraysInt(int[] a, int[] b) {
         Object[] arr1 = {a};
         Object[] arr2 = {b};
-        Object[] inArr1 = {arr1};
-        Object[] inArr2 = {arr2};
-        if(Arrays.deepEquals(inArr1, inArr2)) {
+        if(Arrays.deepEquals(arr1, arr2)) {
             System.out.println("Arrays are equal.");
         } else {
             System.out.println("Arrays are different.");
