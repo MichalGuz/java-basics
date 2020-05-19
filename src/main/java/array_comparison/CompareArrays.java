d366992be19751af96cda88409f72473ceab3cde
MichalGuz <michalguz@gmail.com>
2020-05-19 19:12:30 +0200

    Create Object[] arrays for deep comparison of arrays in compareArraysInt() method in CompareArrays class.

--- src/main/java/array_comparison/CompareArrays.java
+++ src/main/java/array_comparison/CompareArrays.java
@@ -2,6 +2,10 @@ package array_comparison;
 
 public class CompareArrays {
     public static void compareArraysInt(int[] a, int[] b) {
+        Object[] arr1 = {a};
+        Object[] arr2 = {b};
+        Object[] inArr1 = {arr1};
+        Object[] inArr2 = {arr2};
 
     }
 }
