27ac263f5630d316fdddd31700ea9336a5782f73
MichalGuz <michalguz@gmail.com>
2020-05-19 15:44:20 +0200

    Create void method printNumber() in class PassByValueNumberProcessor. Set value of number in this method.

--- src/main/java/pass_by_value_primitive/PassByValueProcessor.java
+++ src/main/java/pass_by_value_primitive/PassByValueProcessor.java
@@ -1,5 +1,7 @@
 package pass_by_value_primitive;
 
 public class PassByValueProcessor {
-    
+    public static void printNumber(PassByValueNumber number) {
+        number.setValue(number.getValue() + 1);
+    }
 }
