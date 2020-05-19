f953816032fdc87be2b6c8bd21a736e6befdb86a
MichalGuz <michalguz@gmail.com>
2020-05-19 17:14:54 +0200

    Create numberObjectModifier() method inside PassByValueProcessor class and call it method in main() method.

--- src/main/java/pass_by_value_primitive/PassByValueProcessor.java
+++ src/main/java/pass_by_value_primitive/PassByValueProcessor.java
@@ -3,14 +3,21 @@ package pass_by_value_primitive;
 public class PassByValueProcessor {
     public void printNumber(PassByValueNumber number) {
         number.setValue(number.getValue() + 1);
-        System.out.println("int processed by call of printNumber method = " + number.getValue());
+        System.out.println("[method printNumber] int processed by call of printNumber method = " + number.getValue());
     }
 
     public void printInt( int n) {
         n++;
-        System.out.println("int processed the call of printInt method = " + n);
+        System.out.println("[method printInt] int processed the call of printInt method = " + n);
     }
 
+    public int numberObjectModifier(PassByValueNumber number, int i) {
+        System.out.println("[1 step of method numberObjectModifier] int at beginning of numberObjectModifier method " + number.getValue());
+        int temp = number.getValue() * i;
+        number.setValue(temp);
+        System.out.println("[2 step of method numberObjectModifier] int during of numberObjectModifier method " + number.getValue());
+        return number.getValue();
+    }
 
     public static void main(String[] args) {
         PassByValueNumber pbvn = new PassByValueNumber(5,"name");
@@ -19,8 +26,11 @@ public class PassByValueProcessor {
         int i = 2;
         System.out.println("int before the call of printInt method = " + i);
         PassByValueProcessor processor = new PassByValueProcessor();
+
         processor.printInt(i);
         System.out.println("int after the call of printInt method = " + i);
+
+        processor.numberObjectModifier(pbvn,4);
 
         System.out.println("int before the call of printNumber method = " + val);
         processor.printNumber(pbvn);
@@ -35,6 +45,6 @@ public class PassByValueProcessor {
 
         processor.printInt(check);
 
-
+        processor.numberObjectModifier(pbvn,4);
     }
 }
