6f4f4deefe2a1563436f69901182ce1bcb001adb
MichalGuz <michalguz@gmail.com>
2020-05-19 15:29:31 +0200

    Create two fields, constructor and getters in class PassByValueNumber.

--- src/main/java/pass_by_value_primitive/PassByValueNumber.java
+++ src/main/java/pass_by_value_primitive/PassByValueNumber.java
@@ -1,4 +1,19 @@
 package pass_by_value_primitive;
 
 public class PassByValueNumber {
+    int value;
+    String name;
+
+    public PassByValueNumber(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
