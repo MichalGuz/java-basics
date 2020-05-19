f0039c0398ac90e22cf2047b66e96605c0a8c7b0
MichalGuz <michalguz@gmail.com>
2020-05-19 19:16:54 +0200

    Call Arrays.deepEquals() and create if-else block in compareArraysInt() method in CompareArrays class.

--- src/main/java/array_comparison/CompareArrays.java
+++ src/main/java/array_comparison/CompareArrays.java
@@ -1,11 +1,18 @@
 package array_comparison;
 
+import java.util.Arrays;
+
 public class CompareArrays {
     public static void compareArraysInt(int[] a, int[] b) {
         Object[] arr1 = {a};
         Object[] arr2 = {b};
         Object[] inArr1 = {arr1};
         Object[] inArr2 = {arr2};
+        if(Arrays.deepEquals(inArr1, inArr2)) {
+            System.out.println("arrays are equal.");
+        } else {
+            System.out.println("arrays are different");
+        }
 
     }
 }
