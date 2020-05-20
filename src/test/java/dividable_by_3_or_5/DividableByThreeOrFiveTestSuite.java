2956daa5b091c279371d66c0e1b4d37a439b0531
MichalGuz <michalguz@gmail.com>
2020-05-20 19:21:19 +0200

    Create 'given' section of testDividableNumbersFromRange() method.

--- src/test/java/dividable_by_3_or_5/DividableByThreeOrFiveTestSuite.java
+++ src/test/java/dividable_by_3_or_5/DividableByThreeOrFiveTestSuite.java
@@ -2,11 +2,16 @@ package dividable_by_3_or_5;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DividableByThreeOrFiveTestSuite {
     @Test
     public void testDividableNumbersFromRange() {
+        // given
+        List<Integer> testList = new ArrayList<>();
+        int a = 1;
+        int b= 10;
 
     }
 }
