697c53fc169a02ce83afa3264cbc90e9f9479cd2
MichalGuz <michalguz@gmail.com>
2020-05-20 17:29:00 +0200

    Create dividableNumbersFromRange() method in DividableByThreeOrFive class.

--- src/main/java/dividable_by_3_or_5/DividableByThreeOrFive.java
+++ src/main/java/dividable_by_3_or_5/DividableByThreeOrFive.java
@@ -1,11 +1,17 @@
 package dividable_by_3_or_5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DividableByThreeOrFive {
 //   Instructions:
 //   If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //   Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 //   Note: If the number is a multiple of both 3 and 5, only count it once.
 
-
+    public static List<Integer> dividableNumbersFromRange(int a, int b) {
+        List<Integer> numbers = new ArrayList<>();
+        return numbers;
+    }
 }
 
