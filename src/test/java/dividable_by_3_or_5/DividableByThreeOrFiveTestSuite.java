39b96ee6b3f8e8503ac70d02a9daadd9c6eba973
MichalGuz <michalguz@gmail.com>
2020-05-20 19:13:50 +0200

    Create testDividableNumbersFromRange() method in DividableByThreeOrFiveTestSuite class.

--- src/test/java/dividable_by_3_or_5/DividableByThreeOrFiveTestSuite.java
+++ src/test/java/dividable_by_3_or_5/DividableByThreeOrFiveTestSuite.java
@@ -1,4 +1,12 @@
 package dividable_by_3_or_5;
 
+import org.junit.Test;
+
+import java.util.List;
+
 public class DividableByThreeOrFiveTestSuite {
+    @Test
+    public void testDividableNumbersFromRange() {
+
+    }
 }
