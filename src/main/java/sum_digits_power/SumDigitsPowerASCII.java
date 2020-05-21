0b95f0e7c5c98fd8358166619f205066848f59cc
MichalGuz <michalguz@gmail.com>
2020-05-21 14:33:08 +0200

    Create sumDigPowASCII() method in SumDigitsPowerASCII class and create new ArrayList()

--- src/main/java/sum_digits_power/SumDigitsPowerASCII.java
+++ src/main/java/sum_digits_power/SumDigitsPowerASCII.java
@@ -1,5 +1,11 @@
 package sum_digits_power;
 
-public class SumDigitsPowerASCII {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SumDigitsPowerASCII {
+    public static List<Long> sumDigPowASCII(long a, long b) {
+        List<Long> nums = new ArrayList<Long>();
+        return nums;
+    }
 }
