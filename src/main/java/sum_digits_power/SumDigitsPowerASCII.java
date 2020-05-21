1141e1ba5f4d37c4897a138deec7b8374efbf3ab
MichalGuz <michalguz@gmail.com>
2020-05-21 14:35:23 +0200

    Create 'for' loop with two variables sumDigPowASCII() method in SumDigitsPowerASCII.

--- src/main/java/sum_digits_power/SumDigitsPowerASCII.java
+++ src/main/java/sum_digits_power/SumDigitsPowerASCII.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class SumDigitsPowerASCII {
     public static List<Long> sumDigPowASCII(long a, long b) {
         List<Long> nums = new ArrayList<Long>();
+        for(long i = a; i<=b ; i++) {
+            int y = 1;
+            long sum = 0;
+
+        }        }
         return nums;
     }
 }
