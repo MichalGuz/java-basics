568debc5c66fc51eb65cb36c54ace7bafd0bbbd2
MichalGuz <michalguz@gmail.com>
2020-05-22 12:42:05 +0200

    Create String object by call Long.toString() method in boolean isEqualToPower(long number) method in SumDigitsPowerStream class.

--- src/main/java/sum_digits_power/SumDigitsPowerStream.java
+++ src/main/java/sum_digits_power/SumDigitsPowerStream.java
@@ -2,6 +2,7 @@ package sum_digits_power;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SumDigitsPowerStream {
     public static List<Long> sumDigitsPowerStream(long a, long b) {
@@ -9,6 +10,7 @@ public class SumDigitsPowerStream {
     }
 
     private static boolean isEqualToPower(long number) {
+        String value = Long.toString(number);
 
     }
 }
