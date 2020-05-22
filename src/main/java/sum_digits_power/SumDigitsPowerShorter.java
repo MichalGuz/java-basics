171256ffaea63aa8b54d0763b2cead1bfa34826a
MichalGuz <michalguz@gmail.com>
2020-05-22 11:04:40 +0200

    Initialize int variable by call length() method inside isEqual(long number) method in SumDigitsPowerShorter class.

--- src/main/java/sum_digits_power/SumDigitsPowerShorter.java
+++ src/main/java/sum_digits_power/SumDigitsPowerShorter.java
@@ -17,6 +17,9 @@ public class SumDigitsPowerShorter {
     private static boolean isEqual(long number){
         long temp = 0;
         long sum = 0;
+        int power = lenght(number);
+
+        }
     }
 
     private static int lenght(long n) {
