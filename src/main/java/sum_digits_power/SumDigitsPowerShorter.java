9a4a76d887d242bd198fd0534fe912a7fd38a3a2
MichalGuz <michalguz@gmail.com>
2020-05-22 10:17:34 +0200

    Add 'if' condition in the beggining of sumDigitsPowerShorter() method to check if given numbers are positive integers.

--- src/main/java/sum_digits_power/SumDigitsPowerShorter.java
+++ src/main/java/sum_digits_power/SumDigitsPowerShorter.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class SumDigitsPowerShorter {
     public static List<Long> sumDigitsPowerShorter(Long a, Long b) {
-
+        if ((a < 1) || (b < 1)) {
+            System.out.println("numbers must be non-zero positive integers");
+            throw new IllegalArgumentException();
+        }
     }
 }
