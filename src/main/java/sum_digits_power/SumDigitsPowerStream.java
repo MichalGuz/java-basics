e5658a96f9c78262ae2241f77c1b27404c790c5e
MichalGuz <michalguz@gmail.com>
2020-05-22 12:39:13 +0200

    Create boolean isEqualToPower(long number) method in SumDigitsPowerStream class.

--- src/main/java/sum_digits_power/SumDigitsPowerStream.java
+++ src/main/java/sum_digits_power/SumDigitsPowerStream.java
@@ -1,9 +1,14 @@
 package sum_digits_power;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SumDigitsPowerStream {
     public static List<Long> sumDigitsPowerStream(long a, long b) {
+        List<Long> result = new ArrayList<>();
+    }
+
+    private static boolean isEqualToPower(long number) {
 
     }
 }
