787ce539d6185eb69c4aafd26f30500c452ad0a2
MichalGuz <michalguz@gmail.com>
2020-05-25 22:47:34 +0200

    Create 'if' condition in loop 'for' in sumDividableNumbersBelowWithLoop() method.

--- src/main/java/dividable_by_3_or_5/DividableByThreeOrFive.java
+++ src/main/java/dividable_by_3_or_5/DividableByThreeOrFive.java
@@ -42,7 +42,9 @@ public class DividableByThreeOrFive {
     public static int sumDividableNumbersBelowWithLoop(int number) {
         int sum = 0;
         for (int i = 0; i < number; i++ ){
+            if (i % 3 == 0 || i % 5 == 0) {
 
+            }
         }
 
     }
