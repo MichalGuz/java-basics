04e05071a318aa9201cb9f9ce09419c9c6913dd0
MichalGuz <michalguz@gmail.com>
2020-06-08 22:44:10 +0200

    Create a loop 'for each' to fill the ArrayList with Strings form array in method toRoman

--- src/main/java/roman_numerals/RomanNumerals.java
+++ src/main/java/roman_numerals/RomanNumerals.java
@@ -7,5 +7,11 @@ public class RomanNumerals {
     public static String toRoman(int number) {
         String[] rom = {"","I", "II","III","IV","V","VI","VII","VIII","IX","","X","XX","XXX","XL","L","LX", "LXX", "LXXX", "XC", "", "C","CC","CCC","CD","D","DC","DCC","DCCC","CM","","M","MM","MMM"};
         List<String> roman = new ArrayList<>();
+
+        for (String r: rom) {
+            roman.add(r);
+            System.out.print(r + ", ");
+        }
+
     }
 }
