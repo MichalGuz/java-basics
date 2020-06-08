e2fc9cf261f05006f3f3a9b80f5c424ccde4593d
MichalGuz <michalguz@gmail.com>
2020-06-08 22:43:16 +0200

    Create an empty ArrayList in method toRoman

--- src/main/java/roman_numerals/RomanNumerals.java
+++ src/main/java/roman_numerals/RomanNumerals.java
@@ -1,7 +1,11 @@
 package roman_numerals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RomanNumerals {
     public static String toRoman(int number) {
         String[] rom = {"","I", "II","III","IV","V","VI","VII","VIII","IX","","X","XX","XXX","XL","L","LX", "LXX", "LXXX", "XC", "", "C","CC","CCC","CD","D","DC","DCC","DCCC","CM","","M","MM","MMM"};
+        List<String> roman = new ArrayList<>();
     }
 }
