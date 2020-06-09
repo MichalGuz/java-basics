1dbfd0f9e03dd382aaeec060b651e8c7288827de
MichalGuz <michalguz@gmail.com>
2020-06-09 22:11:26 +0200

    Create 'if-else' block 'in RomanNumerals class with throwing of new IllegalArgumentException if value of 'number' isn't less than 4000.

--- src/main/java/roman_numerals/RomanNumerals.java
+++ src/main/java/roman_numerals/RomanNumerals.java
@@ -5,29 +5,34 @@ import java.util.List;
 
 public class RomanNumerals {
     public static String toRoman(int number) {
-        String[] rom = {"","I", "II","III","IV","V","VI","VII","VIII","IX","","X","XX","XXX","XL","L","LX", "LXX", "LXXX", "XC", "", "C","CC","CCC","CD","D","DC","DCC","DCCC","CM","","M","MM","MMM"};
-        List<String> roman = new ArrayList<>();
+        if (number < 4000) {
+            String[] rom = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "", "M", "MM", "MMM"};
+            List<String> roman = new ArrayList<>();
 
-        for (String r: rom) {
-            roman.add(r);
-            System.out.print(r + ", ");
-        }
+            for (String r : rom) {
+                roman.add(r);
+                System.out.print(r + ", ");
+            }
 
-        List<Integer> numbers = new ArrayList<>();
-        while (number > 0) {
-            int digit = number % 10;
-            numbers.add(digit);
-            number /= 10;
-        }
+            List<Integer> numbers = new ArrayList<>();
+            while (number > 0) {
+                int digit = number % 10;
+                numbers.add(digit);
+                number /= 10;
+            }
 
-        String result = "";
-        int temp = 0;
-        for (Integer i: numbers ) {
-            i = i + temp;
-            result = new StringBuilder().append(roman.get(i)).append(result).toString();
-            temp += 10;
+            String result = "";
+            int temp = 0;
+            for (Integer i : numbers) {
+                i = i + temp;
+                result = new StringBuilder().append(roman.get(i)).append(result).toString();
+                temp += 10;
+            }
+            System.out.println(result);
+            return result;
+        } else {
+            throw new IllegalArgumentException("Value of number must be less than 4000.");
         }
-        System.out.println(result);
-        return result;
     }
+
 }
