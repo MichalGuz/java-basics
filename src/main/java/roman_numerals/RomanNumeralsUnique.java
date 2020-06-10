fbbc1821a8f243b753c562f77db51f49abf8c5e3
MichalGuz <michalguz@gmail.com>
2020-06-10 22:41:25 +0200

    Create final static array of 'arab' ints in RomanNumeralsUnique class.

--- src/main/java/roman_numerals/RomanNumeralsUnique.java
+++ src/main/java/roman_numerals/RomanNumeralsUnique.java
@@ -1,6 +1,7 @@
 package roman_numerals;
 
 public class RomanNumeralsUnique {
+    private final static int[] arab = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     public static String toRomanNumeral(int n) {
 
     }
