dc46bc279514cec05e0563cd36d094ce3e24b781
MichalGuz <michalguz@gmail.com>
2020-06-10 22:49:18 +0200

    Create new StrinBuilder object 'result' in method toRomanNumeral(int n) in RomanNumeralsUnique class.

--- src/main/java/roman_numerals/RomanNumeralsUnique.java
+++ src/main/java/roman_numerals/RomanNumeralsUnique.java
@@ -3,7 +3,9 @@ package roman_numerals;
 public class RomanNumeralsUnique {
     private static final int[] arab = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     private static final String[] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
     public static String toRomanNumeral(int n) {
+        StringBuilder result = new StringBuilder();
 
     }
 }
