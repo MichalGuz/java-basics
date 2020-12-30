1cfd95c0a65a40a99e13120c72296780e68ec36c
MichalGuz <michalguz@gmail.com>
2020-12-30 21:05:46 +0100

    Count the hours in getDuration method.

--- src/main/java/moduloPractise/Duration.java
+++ src/main/java/moduloPractise/Duration.java
@@ -5,6 +5,17 @@ public class Duration {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
             return "Invalid value.";
         }
-        return "duration";
+        int hours = minutes / 60;
+        minutes %= 60;
+        String h;
+        String m;
+        String s;
+        if (hours < 10) {
+            h = "0" + hours;
+        } else {
+            h = "" + hours;
+        }
+
+        return h;
     }
 }
