1f6858697e4d2aee9b33c03f23966532d66ebe96
MichalGuz <michalguz@gmail.com>
2020-12-30 20:50:05 +0100

    Add initial condition in getDuration method.

--- src/main/java/moduloPractise/Duration.java
+++ src/main/java/moduloPractise/Duration.java
@@ -1,4 +1,10 @@
 package moduloPractise;
 
 public class Duration {
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0 || seconds < 0 || seconds > 59) {
+            return "Invalid value.";
+        }
+        return "duration";
+    }
 }
