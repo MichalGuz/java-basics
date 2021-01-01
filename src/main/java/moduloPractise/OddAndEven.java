790224040c756e5b02cfb0f9620a371547882832
MichalGuz <michalguz@gmail.com>
2021-01-01 16:17:10 +0100

    Rename printOdd(int firtsNumber, int lastNumber) method in OddAndEven class.

--- src/main/java/moduloPractise/OddAndEven.java
+++ src/main/java/moduloPractise/OddAndEven.java
@@ -43,7 +43,7 @@ public class OddAndEven {
         return false;
     }
 
-    public static void oddPrint(int firstNumber, int lastNumber) {
+    public static void printOdd(int firstNumber, int lastNumber) {
 
     }
 }
