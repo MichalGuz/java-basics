6788e29ef669e43d6b95de12ad4f38a05486409f
MichalGuz <michalguz@gmail.com>
2021-01-01 15:39:50 +0100

    Add if condition in the body of isEven method.

--- src/main/java/moduloPractise/OddAndEven.java
+++ src/main/java/moduloPractise/OddAndEven.java
@@ -1,4 +1,12 @@
 package moduloPractise;
 
 public class OddAndEven {
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
