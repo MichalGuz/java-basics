2d71c2e06020209ef8feb85ddff002266f884e0c
MichalGuz <michalguz@gmail.com>
2021-01-02 15:19:51 +0100

    Create int sumOfDigits(int number) method in class SumOfDigits

--- src/main/java/moduloPractise/SumOfDigits.java
+++ src/main/java/moduloPractise/SumOfDigits.java
@@ -1,4 +1,26 @@
 package moduloPractise;
 
 public class SumOfDigits {
+    public static int sumOfDigits(int number) {
+        int sum = 0;
+        int digit;
+        if(number >= 10) {
+            while(number > 0) {
+                digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+            System.out.println("Sum = " + sum);
+            return sum;
+        } else {
+            if (number >= 0) {
+                System.out.println("Sum = " + number);
+                return number;
+            } else {
+                return -1;
+            }
+        }
+    }
 }
+
+
