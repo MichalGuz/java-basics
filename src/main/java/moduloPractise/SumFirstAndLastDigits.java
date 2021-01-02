1a8867d2f8cca2e24ac3b7d7e5eecc5cde505d8b
MichalGuz <michalguz@gmail.com>
2021-01-02 16:36:11 +0100

    Amend if conditions in sumFirstAndLastDigits method in order to correct processing single digit numbers

--- src/main/java/moduloPractise/SumFirstAndLastDigits.java
+++ src/main/java/moduloPractise/SumFirstAndLastDigits.java
@@ -8,19 +8,32 @@ public class SumFirstAndLastDigits {
             System.out.println("It is a negative number");
             return -1;
         } else {
-            int firstDigit = 0;
-            int lastDigit = processedNumber % 10;
-            do {
-                firstDigit = processedNumber % 10;
-                processedNumber /= 10;
-            } while (processedNumber > 0);
-            sum = firstDigit + lastDigit;
-            System.out.println("First digit of number " + number + " is " + firstDigit);
-            System.out.println("Last digit of number " + number + " is " + lastDigit);
-            System.out.println("Sum of first and last digits is " + sum);
+            if (processedNumber < 10) {
+                System.out.println("The first and last digits is " + processedNumber + " because the number: " + processedNumber +  " is a single digit number.");
+                sum = processedNumber;
+            } else {
+                int firstDigit = 0;
+                int lastDigit = processedNumber % 10;
+                do {
+                    firstDigit = processedNumber % 10;
+                    processedNumber /= 10;
+                } while (processedNumber > 0);
+                sum = firstDigit + lastDigit;
+                System.out.println("First digit of number " + number + " is " + firstDigit);
+                System.out.println("Last digit of number " + number + " is " + lastDigit);
+            }
         }
-    return sum;
+        System.out.println("Sum of first and last digits is " + sum);
+        return sum;
     }
 
+    public static void main(String[] args) {
+        sumFirstAndLastDigits(0);
+        sumFirstAndLastDigits(7);
+        sumFirstAndLastDigits(17);
+        sumFirstAndLastDigits(303);
+        sumFirstAndLastDigits(-4321);
+        sumFirstAndLastDigits(1200);
+    }
 }
 
