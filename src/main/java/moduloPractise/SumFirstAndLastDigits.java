a9544710590eb010bf1388da6ca05b6b692d06f9
MichalGuz <michalguz@gmail.com>
2021-01-02 16:21:51 +0100

    Create sumFirstAndLastDigits method with if condition inside class SumFirstAndLastDigits

--- src/main/java/moduloPractise/SumFirstAndLastDigits.java
+++ src/main/java/moduloPractise/SumFirstAndLastDigits.java
@@ -1,4 +1,15 @@
 package moduloPractise;
 
 public class SumFirstAndLastDigits {
+    public static int sumFirstAndLastDigits(int number) {
+        int processedNumber = number;
+        int sum = 0;
+        if (processedNumber < 0) {
+            System.out.println("It is a negative number");
+            return -1;
+        }
+    return sum;
+    }
+
 }
+
