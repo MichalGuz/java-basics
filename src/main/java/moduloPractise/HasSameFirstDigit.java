066746c902bec73048ac3c640c8025b2dca1a838
MichalGuz <michalguz@gmail.com>
2021-01-03 17:12:20 +0100

    Remove redundant calls of println method inside body of isValid method

--- src/main/java/moduloPractise/HasSameFirstDigit.java
+++ src/main/java/moduloPractise/HasSameFirstDigit.java
@@ -37,10 +37,8 @@ public class HasSameFirstDigit {
 
     public static boolean isValid ( int number){
         if ((number >= 10) && (number <= 1000)) {
-            System.out.println("true");
             return true;
         } else {
-            System.out.println("false");
             return false;
         }
     }
