dec4b4b246f572a94e40c800afba8a64993a8dea
MichalGuz <michalguz@gmail.com>
2021-01-03 16:27:42 +0100

    Create if condition to limit the arguments of method hasSharedDigits

--- src/main/java/moduloPractise/HasSharedDigits.java
+++ src/main/java/moduloPractise/HasSharedDigits.java
@@ -1,4 +1,13 @@
 package moduloPractise;
 
 public class HasSharedDigits {
+    public static boolean hasSharedDigits(int firstNumber, int secondNumber) {
+        int f = firstNumber;
+        int s = secondNumber;
+        if ((firstNumber < 10) || (firstNumber > 99 ) || (secondNumber < 10) || (secondNumber > 99)) {
+            System.out.println("false");
+            return false;
+        }
+        return false;
+    }
 }
