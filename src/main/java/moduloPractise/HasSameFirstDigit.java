b1ffdd8e21ecff89bcd317d2904ff26efd051d00
MichalGuz <michalguz@gmail.com>
2021-01-03 16:53:57 +0100

    Create boolean isValid(int number) method in HasSameFirstDigits class

--- src/main/java/moduloPractise/HasSameFirstDigit.java
+++ src/main/java/moduloPractise/HasSameFirstDigit.java
@@ -4,4 +4,14 @@ public class HasSameFirstDigit {
     public static boolean hasSameFirstDigit(int firstNumber, int secondNumber) {
         return false;
     }
+
+    public static boolean isValid ( int number){
+        if ((number >= 10) && (number <= 1000)) {
+            System.out.println("true");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
 }
