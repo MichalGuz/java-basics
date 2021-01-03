b2fdcafd9f4a253793c01057f8b0b428af9d7266
MichalGuz <michalguz@gmail.com>
2021-01-03 16:35:20 +0100

    Create else block in method hasSharedDigits

--- src/main/java/moduloPractise/HasSharedDigits.java
+++ src/main/java/moduloPractise/HasSharedDigits.java
@@ -7,6 +7,11 @@ public class HasSharedDigits {
         if ((firstNumber < 10) || (firstNumber > 99 ) || (secondNumber < 10) || (secondNumber > 99)) {
             System.out.println("false");
             return false;
+        } else {
+            int digitOne = firstNumber % 10;
+            int digitTwo = (firstNumber / 10) % 10;
+            int digitTree = secondNumber % 10;
+            int digitFour = (secondNumber / 10) % 10;
         }
         return false;
     }
