3d5b53b45b0ec0f918b024da8c5bb7b7d1b18d68
MichalGuz <michalguz@gmail.com>
2021-01-04 21:03:24 +0100

    Create if condition to eliminate negative numbers in sumOfEvenDigit method

--- src/main/java/moduloPractise/SumOfEvenDigit.java
+++ src/main/java/moduloPractise/SumOfEvenDigit.java
@@ -2,6 +2,14 @@ package moduloPractise;
 
 public class SumOfEvenDigit {
     public static int sumOfEvenDigit(int number) {
+        int sum= 0;
+        int processedNumber = 0;
+        if (number < 0) {
+            System.out.println("It's a negative number");
+            return -1;
 
+
+        }
+        return sum;
     }
 }
