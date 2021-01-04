75fd7621805c07d63bed71491bad945c46f7528b
MichalGuz <michalguz@gmail.com>
2021-01-04 20:06:49 +0100

    Create if-else block in hasSameLastDigit method

--- src/main/java/moduloPractise/HasSameLastDigit.java
+++ src/main/java/moduloPractise/HasSameLastDigit.java
@@ -6,8 +6,17 @@ public class HasSameLastDigit {
             int firstLastDigit = firstNumber % 10;
             int secondLastDigit = secondNumber% 10;
             int thirdLastDigit = thirdNumber% 10;
+            if(firstLastDigit == secondLastDigit && secondLastDigit == thirdLastDigit) {
+                System.out.println("The" + firstLastDigit + " the same last digit in numbers: " + firstLastDigit + ", " + secondLastDigit + ", " + thirdLastDigit);
+                return true;
+            } else {
+                System.out.println("False");
+                return false;
+            }
+        } else {
+            System.out.println("Inappropriate numbers");
+            return false;
         }
-        return false;
     }
 
     public static boolean isValidNumber(int number) {
