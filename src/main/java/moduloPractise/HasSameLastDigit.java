0b3d473d38e0c041b80ea7e5cae6355fab063e4a
MichalGuz <michalguz@gmail.com>
2021-01-04 19:50:12 +0100

    Create isValid method in HasSameLastDigit class

--- src/main/java/moduloPractise/HasSameLastDigit.java
+++ src/main/java/moduloPractise/HasSameLastDigit.java
@@ -4,4 +4,12 @@ public class HasSameLastDigit {
     public static boolean hasSameLastDigit(int firstNumber, int secondNumber, int thirdNumber) {
         return false;
     }
+
+    public static boolean isValid(int number) {
+        if(number >= 10 || number <=1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
