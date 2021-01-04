41b73a02e4b19bcc982ac3d3eeffaa61d9c21210
MichalGuz <michalguz@gmail.com>
2021-01-04 19:57:27 +0100

    Create if condition and assigned values of last digits to int variables in hasSameLastDigit method

--- src/main/java/moduloPractise/HasSameLastDigit.java
+++ src/main/java/moduloPractise/HasSameLastDigit.java
@@ -2,11 +2,16 @@ package moduloPractise;
 
 public class HasSameLastDigit {
     public static boolean hasSameLastDigit(int firstNumber, int secondNumber, int thirdNumber) {
+        if (isValidNumber(firstNumber) && isValidNumber(secondNumber) && isValidNumber(thirdNumber)) {
+            int firstLastDigit = firstNumber % 10;
+            int secondLastDigit = secondNumber% 10;
+            int thirdLastDigit = thirdNumber% 10;
+        }
         return false;
     }
 
-    public static boolean isValid(int number) {
-        if(number >= 10 || number <=1000) {
+    public static boolean isValidNumber(int number) {
+        if(number >= 10 && number <=1000) {
             return true;
         } else {
             return false;
