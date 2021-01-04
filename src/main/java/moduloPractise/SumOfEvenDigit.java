be870d8080193d3ea6d32bd6a0872c2ee8813bed
MichalGuz <michalguz@gmail.com>
2021-01-04 21:04:59 +0100

    Create else block with while loop in sumOfEvenDigit method

--- src/main/java/moduloPractise/SumOfEvenDigit.java
+++ src/main/java/moduloPractise/SumOfEvenDigit.java
@@ -2,11 +2,16 @@ package moduloPractise;
 
 public class SumOfEvenDigit {
     public static int sumOfEvenDigit(int number) {
-        int sum= 0;
+        int sum = 0;
         int processedNumber = 0;
         if (number < 0) {
             System.out.println("It's a negative number");
             return -1;
+        } else {
+            while(processedNumber > 0) {
+                processedNumber /= 10;
+
+            }
 
 
         }
