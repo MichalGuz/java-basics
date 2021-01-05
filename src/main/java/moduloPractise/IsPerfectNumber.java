de6a1c29d54e1d80e407cb44d55d31db5d79ebf9
MichalGuz <michalguz@gmail.com>
2021-01-05 18:50:31 +0100

    Add disclaimer for IsPerfectNumber class to explain what is the perfect number

--- src/main/java/moduloPractise/IsPerfectNumber.java
+++ src/main/java/moduloPractise/IsPerfectNumber.java
@@ -1,5 +1,6 @@
 package moduloPractise;
 
+// perfect number is when the sum of all it's factors equals value of this number
 public class IsPerfectNumber {
     public static boolean isPerfectNumber(int number) {
         return false;
