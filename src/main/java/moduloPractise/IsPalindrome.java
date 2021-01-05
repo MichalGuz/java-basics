78748076644db249e4f44689084c7adcab31fbfd
MichalGuz <michalguz@gmail.com>
2021-01-05 18:09:17 +0100

    Create if condition to avoid negative numbers in isPalindrom method

--- src/main/java/moduloPractise/IsPalindrome.java
+++ src/main/java/moduloPractise/IsPalindrome.java
@@ -2,6 +2,12 @@ package moduloPractise;
 
 public class IsPalindrome {
     public static boolean isPalindrom(int number) {
+        int temp = number;
+        int reversed = 0;
+        if(number < 0) {
+            System.out.println("False: " + number + " is a negative number.");
+            return false;
+        }
         return false;
     }
 }
