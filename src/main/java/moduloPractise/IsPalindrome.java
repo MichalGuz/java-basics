893a83deef7105b37daab711fc7ef142cd4c491f
MichalGuz <michalguz@gmail.com>
2021-01-05 18:16:34 +0100

    Create else block in isPalindrom method with loop while inside it

--- src/main/java/moduloPractise/IsPalindrome.java
+++ src/main/java/moduloPractise/IsPalindrome.java
@@ -7,7 +7,13 @@ public class IsPalindrome {
         if(number < 0) {
             System.out.println("False: " + number + " is a negative number.");
             return false;
+        } else {
+            while (temp > 0) {
+                int mod = temp % 10;
+                reversed = (reversed * 10) + mod;
+                temp /= 10;
+            }
+            return false;
         }
-        return false;
     }
 }
