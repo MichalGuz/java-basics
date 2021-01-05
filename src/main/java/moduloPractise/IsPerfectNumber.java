07844fcc03d0978cbca79ab9c528b2eee6575fb0
MichalGuz <michalguz@gmail.com>
2021-01-05 18:56:44 +0100

    Create if condition to eliminate invalid values isPerfectNumber method

--- src/main/java/moduloPractise/IsPerfectNumber.java
+++ src/main/java/moduloPractise/IsPerfectNumber.java
@@ -3,6 +3,10 @@ package moduloPractise;
 // perfect number is when the sum of all it's factors equals value of this number
 public class IsPerfectNumber {
     public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            System.out.println("Invalid value.");
+            return false;
+        }
         return false;
     }
 }
