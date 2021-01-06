8be65caf212394030c0f51f8841b842bc6688b1d
MichalGuz <michalguz@gmail.com>
2021-01-06 15:46:59 +0100

    Amend int variables and create if condition to filter out the negative numbers in findGreatestCommonDivisor method

--- src/main/java/moduloPractise/Divisors.java
+++ src/main/java/moduloPractise/Divisors.java
@@ -2,8 +2,14 @@ package moduloPractise;
 
 public class Divisors {
     public static int findGreatestCommonDivisor(int firstNumber, int secondNumber) {
-        int divisor = 0;
+        int divisor = 1;
+        int theGreatestDivisor = 0;
 
-        return divisor;
+        if(firstNumber < 0 || secondNumber < 0) {
+            System.out.println("Invalid values - numbers must be positive");
+            return -1;
+        }
+
+        return theGreatestDivisor;
     }
 }
