3000e491d21a3fa3ad9b67d0e6e29e1430caffbe
MichalGuz <michalguz@gmail.com>
2021-01-06 15:10:10 +0100

    Create int findGreatestCommonDivisor(int firstNumber, int secondNumber) method in Divisors class

---
+++ src/main/java/moduloPractise/Divisors.java
@@ -0,0 +1,9 @@
+package moduloPractise;
+
+public class Divisors {
+    public static int findGreatestCommonDivisor(int firstNumber, int secondNumber) {
+        int divisor = 0;
+
+        return divisor;
+    }
+}
