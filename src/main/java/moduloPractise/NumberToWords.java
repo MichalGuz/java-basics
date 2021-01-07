cda00a6a0ababc12f77886e4a79e0404d2d10556
MichalGuz <michalguz@gmail.com>
2021-01-07 21:50:45 +0100

    Create if condition numberToWords method

--- src/main/java/moduloPractise/NumberToWords.java
+++ src/main/java/moduloPractise/NumberToWords.java
@@ -2,6 +2,9 @@ package moduloPractise;
 
 public class NumberToWords {
     public static String numberToWords(int number) {
+        if(number < 0) {
+            return "Invalid value - number can't be negative";
+        }
         return "The number = ";
     }
 }
