00c7a44d71d7e30885d1e99142f135580a4a095b
MichalGuz <michalguz@gmail.com>
2021-01-07 22:20:46 +0100

    Fill the switch block with cases in numberToWords method

--- src/main/java/moduloPractise/NumberToWords.java
+++ src/main/java/moduloPractise/NumberToWords.java
@@ -14,7 +14,36 @@ public class NumberToWords {
                 digit = reversed % 10;
                 reversed /= 10;
                 switch (digit) {
-
+                    case 0:
+                        System.out.println("Zero");
+                        break;
+                    case 1:
+                        System.out.println("One");
+                        break;
+                    case 2:
+                        System.out.println("Two");
+                        break;
+                    case 3:
+                        System.out.print("Three ");
+                        break;
+                    case 4:
+                        System.out.print("Four ");
+                        break;
+                    case 5:
+                        System.out.print("Five ");
+                        break;
+                    case 6:
+                        System.out.print("Six ");
+                        break;
+                    case 7:
+                        System.out.print("Seven ");
+                        break;
+                    case 8:
+                        System.out.print("Eight ");
+                        break;
+                    case 9:
+                        System.out.print("Nine ");
+                        break;
                 }
             }
         }
