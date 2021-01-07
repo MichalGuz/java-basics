fbe5cb90ccc9ef06a5357bbc14aae5a395332730
MichalGuz <michalguz@gmail.com>
2021-01-07 21:55:35 +0100

    Create else block in numberToWords method

--- src/main/java/moduloPractise/NumberToWords.java
+++ src/main/java/moduloPractise/NumberToWords.java
@@ -4,6 +4,12 @@ public class NumberToWords {
     public static void numberToWords(int number) {
         if(number < 0) {
             System.out.println("Invalid value - number can't be negative");
+        } else {
+            System.out.println(number + " = ");
+            int digit = 0;
+            int reversed;
         }
+
+
     }
 }
