584610840dd530740c71a08421f3c0c55cf36e04
MichalGuz <michalguz@gmail.com>
2021-01-07 22:43:39 +0100

    Amend the numberToWords method

--- src/main/java/moduloPractise/NumberToWords.java
+++ src/main/java/moduloPractise/NumberToWords.java
@@ -5,7 +5,7 @@ public class NumberToWords {
         if(number < 0) {
             System.out.println("Invalid value - number can't be negative");
         } else {
-            System.out.println(number + " = ");
+            System.out.print(number + " = ");
             int digit = 0;
             int reversed = reverse(number);
             int numberOfDigits = countDigits(number);
@@ -14,40 +14,41 @@ public class NumberToWords {
 
             for (int i = 0; i < numberOfDigits; i++) {
                 digit = reversed % 10;
-                reversed /= 10;
+
                 switch (digit) {
                     case 0:
-                        System.out.println("Zero");
+                        System.out.print("Zero");
                         break;
                     case 1:
-                        System.out.println("One");
+                        System.out.print("One");
                         break;
                     case 2:
-                        System.out.println("Two");
+                        System.out.print("Two");
                         break;
                     case 3:
-                        System.out.print("Three ");
+                        System.out.print("Three");
                         break;
                     case 4:
-                        System.out.print("Four ");
+                        System.out.print("Four");
                         break;
                     case 5:
-                        System.out.print("Five ");
+                        System.out.print("Five");
                         break;
                     case 6:
-                        System.out.print("Six ");
+                        System.out.print("Six");
                         break;
                     case 7:
-                        System.out.print("Seven ");
+                        System.out.print("Seven");
                         break;
                     case 8:
-                        System.out.print("Eight ");
+                        System.out.print("Eight");
                         break;
                     case 9:
-                        System.out.print("Nine ");
+                        System.out.print("Nine");
                         break;
                 }
-                System.out.println(" ");
+                reversed /= 10;
+                System.out.print(" ");
             }
 
             if(difference > 0) {
@@ -55,6 +56,8 @@ public class NumberToWords {
                     System.out.println("Zero ");
                 }
             }
+
+            System.out.println("");
         }
     }
 
