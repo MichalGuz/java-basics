0f58adabb9bacbfb49115cf5f73ceb246f578aa4
MichalGuz <michalguz@gmail.com>
2021-01-08 22:16:52 +0100

    Create if condition in printEqual method

--- src/main/java/simplest/Simplest.java
+++ src/main/java/simplest/Simplest.java
@@ -2,6 +2,8 @@ package simplest;
 
 public class Simplest {
     public static void printEqual(int a, int b, int c) {
-
+        if ( (a < 0) || (b < 0) || (c < 0)) {
+            System.out.println("Invalid Value");
+        }
     }
 }
