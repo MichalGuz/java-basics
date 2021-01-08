be9cfcfa3cf4acd505c5107c7f378a82fc4030c6
MichalGuz <michalguz@gmail.com>
2021-01-08 21:58:56 +0100

    Create if condition in printYearsAndDays method

--- src/main/java/moduloPractise/PrintYearsAndDays.java
+++ src/main/java/moduloPractise/PrintYearsAndDays.java
@@ -1,5 +1,9 @@
 package moduloPractise;
 
 public class PrintYearsAndDays {
-    public static void printYearsAndDays(long minutes) {}
+    public static void printYearsAndDays(long minutes) {
+        if(minutes < 0) {
+            System.out.println("Invalid value");
+        }
+    }
 }
