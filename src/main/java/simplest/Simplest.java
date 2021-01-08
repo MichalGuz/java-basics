f12a679104507564ac2e1546a47b737999273eb0
MichalGuz <michalguz@gmail.com>
2021-01-08 22:17:43 +0100

    Create first else if block in printEqual method

--- src/main/java/simplest/Simplest.java
+++ src/main/java/simplest/Simplest.java
@@ -4,6 +4,8 @@ public class Simplest {
     public static void printEqual(int a, int b, int c) {
         if ( (a < 0) || (b < 0) || (c < 0)) {
             System.out.println("Invalid Value");
+        } else if ( (a!= b) && (a!= c) && (b!=c)){
+            System.out.println("All numbers are different");
         }
     }
 }
