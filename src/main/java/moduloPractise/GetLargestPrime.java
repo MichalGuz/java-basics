deac4fbfce04e17184114efef9aa5d36b5aba88e
MichalGuz <michalguz@gmail.com>
2021-01-09 22:21:01 +0100

    Create if econdition in getLargestPrime method

--- src/main/java/moduloPractise/GetLargestPrime.java
+++ src/main/java/moduloPractise/GetLargestPrime.java
@@ -3,9 +3,19 @@ package moduloPractise;
 public class GetLargestPrime {
     public static int getLargestPrime(int number) {
         int result = 0;
+        int factor = 0;
         if(number <= 1) {
             System.out.println("Invalid value: " + number);
             return -1;
+        } else if (number == 2){
+            System.out.println("The largest prime factor of number" + number + " is: 2");
+            return 2;
+        } else {
+            for(int i=2; i<number; i++) {
+                if(number % i == 0) {
+                    factor = i;
+                }
+            }
         }
         return result;
     }
