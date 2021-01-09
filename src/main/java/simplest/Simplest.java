b5fe4740cd81af339100da0d2778c8bbf0354b9f
MichalGuz <michalguz@gmail.com>
2021-01-09 19:05:10 +0100

    Create void printVariousFloats() method in Simplest class

--- src/main/java/simplest/Simplest.java
+++ src/main/java/simplest/Simplest.java
@@ -2,11 +2,11 @@ package simplest;
 
 public class Simplest {
     public static void printEqual(int a, int b, int c) {
-        if ( (a < 0) || (b < 0) || (c < 0)) {
+        if ((a < 0) || (b < 0) || (c < 0)) {
             System.out.println("Invalid Value");
-        } else if ( (a!= b) && (a!= c) && (b!=c)){
+        } else if ((a != b) && (a != c) && (b != c)) {
             System.out.println("All numbers are different");
-        } else if ( (a == b) && (a == c) && (b ==c)){
+        } else if ((a == b) && (a == c) && (b == c)) {
             System.out.println("All numbers are equal");
         } else {
             System.out.println("Neither all are equal or different");
@@ -25,11 +25,14 @@ public class Simplest {
 
     public static double area(double x, double y) {
         double area = 0;
-        if (x < 0 || y < 0){
+        if (x < 0 || y < 0) {
             return -1;
         } else {
             area = x * y;
         }
         return area;
+    }
+
+    public static void printVariousFloats() {
     }
 }
