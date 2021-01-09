724c5dcb9a1e852d52f489227d08d4eb5fc1765a
MichalGuz <michalguz@gmail.com>
2021-01-09 22:36:21 +0100

    Create for loop to find all divisors of factor

--- src/main/java/moduloPractise/GetLargestPrime.java
+++ src/main/java/moduloPractise/GetLargestPrime.java
@@ -4,16 +4,22 @@ public class GetLargestPrime {
     public static int getLargestPrime(int number) {
         int result = 0;
         int factor = 0;
+        int counter = 0;
         if(number <= 1) {
             System.out.println("Invalid value: " + number);
             return -1;
-        } else if (number == 2){
+        } else if (number == 2) {
             System.out.println("The largest prime factor of number" + number + " is: 2");
             return 2;
         } else {
             for(int i=2; i<number; i++) {
                 if (number % i == 0) {
                     factor = i;
+                    for(int p=2; p<factor; p++) {
+                        if(factor % p == 0) {
+                            counter++;
+                        }
+                    }
                 }
             }
             return result;
