3f03e324f12a5785e21f3079578aff3992357e25
MichalGuz <michalguz@gmail.com>
2021-01-09 22:11:29 +0100

    Create if condition in getLargestPrime method

--- src/main/java/moduloPractise/GetLargestPrime.java
+++ src/main/java/moduloPractise/GetLargestPrime.java
@@ -3,6 +3,10 @@ package moduloPractise;
 public class GetLargestPrime {
     public static int getLargestPrime(int number) {
         int result = 0;
+        if(number <= 1) {
+            System.out.println("Invalid value: " + number);
+            return -1;
+        }
         return result;
     }
 }
