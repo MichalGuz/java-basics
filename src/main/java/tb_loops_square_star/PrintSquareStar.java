00f4dd1019c71209a69c94791f8ae53ea7e409ca
MichalGuz <michalguz@gmail.com>
2021-01-11 21:34:07 +0100

    Create two int variables and if condition in printSquareStar method

--- src/main/java/tb_loops_square_star/PrintSquareStar.java
+++ src/main/java/tb_loops_square_star/PrintSquareStar.java
@@ -2,7 +2,12 @@ package tb_loops_square_star;
 
 public class PrintSquareStar {
     public static void printSquareStar(int number) {
+        int rowCount = 0;
+        int colCount = 0;
 
+        if(number < 5) {
+            System.out.println("Invalid value");
+        }
     }
 
 }
