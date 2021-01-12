8974f29b53ccf88aeb980fb7301e2d9b575953bd
MichalGuz <michalguz@gmail.com>
2021-01-12 20:00:54 +0100

    Initialize Scanner variable in getIntegers method

--- src/main/java/tb_arrays/SortedArray.java
+++ src/main/java/tb_arrays/SortedArray.java
@@ -1,8 +1,10 @@
 package tb_arrays;
 
+import java.util.Scanner;
+
 public class SortedArray {
     public static int[] getIntegers(int number) {
         int unsortedArray[] = new int[number];
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
