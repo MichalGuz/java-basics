c9f345088525e57b2258996d829918c82e33857b
MichalGuz <michalguz@gmail.com>
2021-01-13 21:19:32 +0100

    Create getBucketCount in RoundNumbers class.

--- src/main/java/tb_round_numbers/RoundNumbers.java
+++ src/main/java/tb_round_numbers/RoundNumbers.java
@@ -1,4 +1,8 @@
 package tb_round_numbers;
 
 public class RoundNumbers {
+    public static int getBucketCount(double width, double hight, double areaPerBucket, int extraBuckets){
+        int result = 0;
+        return result;
+    }
 }
