e7a2a1a81c6fb26dbc44b0f01103e04efaab215b
MichalGuz <michalguz@gmail.com>
2021-01-13 20:32:43 +0100

    Create if condition sumThreeAndFiveDividable ThreeAndFiveDividable class.

--- src/main/java/moduloPractise/ThreeAndFiveDividable.java
+++ src/main/java/moduloPractise/ThreeAndFiveDividable.java
@@ -3,6 +3,10 @@ package moduloPractise;
 public class ThreeAndFiveDividable {
     public static int sumThreeAndFiveDividable(int number) {
         int sum = 0;
+        if(number < 0) {
+            System.out.println("Invalid value");
+            return -1;
+        }
         return sum;
     }
 }
