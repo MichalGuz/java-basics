a159919f40add5d539edc97358ac46a7b57d8d66
MichalGuz <michalguz@gmail.com>
2021-01-13 20:38:04 +0100

    Create else if block in sumThreeAndFiveDividable method.

--- src/main/java/moduloPractise/ThreeAndFiveDividable.java
+++ src/main/java/moduloPractise/ThreeAndFiveDividable.java
@@ -6,6 +6,8 @@ public class ThreeAndFiveDividable {
         if(number < 0) {
             System.out.println("Invalid value");
             sum = -1;
+        } else if (number == 0){
+            sum = 0;
         }
         for (int i=0; i<number; i++) {
             if((i % 3 == 0) && (i % 5 == 0)) {
