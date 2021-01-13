97f0565718538cbb60f5f4c972b22b5017e2c71a
MichalGuz <michalguz@gmail.com>
2021-01-13 21:33:30 +0100

    Create if else block inside method getBucketCount in RoundNumbers class.

--- src/main/java/tb_round_numbers/RoundNumbers.java
+++ src/main/java/tb_round_numbers/RoundNumbers.java
@@ -3,7 +3,7 @@ package tb_round_numbers;
 public class RoundNumbers {
     public static int getBucketCount(double width, double hight, double areaPerBucket, int extraBuckets) {
         int result = 0;
-        if((width <= 0) || (hight <= 0) || (areaPerBucket <= 0) || (extraBuckets < 0)) {
+        if ((width <= 0) || (hight <= 0) || (areaPerBucket <= 0) || (extraBuckets < 0)) {
             result = -1;
         } else {
             double bucket = Math.ceil((width * hight) / areaPerBucket);
@@ -15,17 +15,24 @@ public class RoundNumbers {
     // #1 overloaded getBucketCount()
     public static int getBucketCount(double width, double hight, double areaPerBucket) {
         int result = 0;
-        if((width <= 0) || (hight <= 0) || (areaPerBucket <= 0)) {
+        if ((width <= 0) || (hight <= 0) || (areaPerBucket <= 0)) {
             result = -1;
         } else {
             double bucket = Math.ceil((width * hight) / areaPerBucket);
-            result = (int)bucket;
+            result = (int) bucket;
         }
         return result;
     }
 
     // #2 overloaded getBucketCount()
-    public static int getBucketCount(double area, double areaPerBucket){
+    public static int getBucketCount(double area, double areaPerBucket) {
         int result = 0;
+        if((area <= 0) || (areaPerBucket <= 0)) {
+            result = -1;
+        } else {
+            double bucket = Math.ceil(area / areaPerBucket);
+            result = (int)bucket;
+        }
         return result;
     }
+}
