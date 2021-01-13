4217c9d4a10fb2fbd84dc9d023c8874c8263bc54
MichalGuz <michalguz@gmail.com>
2021-01-13 20:40:20 +0100

    Add two calls of println method in sumThreeAndFiveDividable method.

--- src/main/java/moduloPractise/ThreeAndFiveDividable.java
+++ src/main/java/moduloPractise/ThreeAndFiveDividable.java
@@ -11,10 +11,13 @@ public class ThreeAndFiveDividable {
         } else {
             for (int i = 0; i < number; i++) {
                 if ((i % 3 == 0) && (i % 5 == 0)) {
+                    System.out.println("number dividable by 3 and 5: " + i);
                     sum += i;
                 }
             }
         }
+        System.out.println("sum of those numbers = " + sum);
         return sum;
     }
+
 }
