9c4b509ac41b310b058402e907bf563010b56fbf
MichalGuz <michalguz@gmail.com>
2021-01-13 21:24:24 +0100

    Create if else block int getBucketCount(double width, double hight, double areaPerBucket, int extraBuckets) method in RoundNumbers class.

--- src/main/java/tb_round_numbers/RoundNumbers.java
+++ src/main/java/tb_round_numbers/RoundNumbers.java
@@ -1,8 +1,12 @@
 package tb_round_numbers;
 
 public class RoundNumbers {
-    public static int getBucketCount(double width, double hight, double areaPerBucket, int extraBuckets){
+    public static int getBucketCount(double width, double hight, double areaPerBucket, int extraBuckets) {
         int result = 0;
+        if((width <= 0) || (hight <= 0) || (areaPerBucket <= 0) || (extraBuckets < 0)) {
+            result = -1;
+        } else {
+        }
         return result;
     }
 }
