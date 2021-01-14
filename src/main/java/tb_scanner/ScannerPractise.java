44fbdbdb4fa0bbd64643fa35f18898710ec1bf79
MichalGuz <michalguz@gmail.com>
2021-01-14 21:30:30 +0100

    Create  void scanInput() method in ScannerPractise class

--- src/main/java/tb_scanner/ScannerPractise.java
+++ src/main/java/tb_scanner/ScannerPractise.java
@@ -1,4 +1,10 @@
 package tb_scanner;
 
+import java.util.Scanner;
+
 public class ScannerPractise {
+    public static void scanInput() {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
