e8c85d63d092c6fe1bd0496945e4fec2cc86654f
MichalGuz <michalguz@gmail.com>
2021-01-14 21:56:25 +0100

    Initialize int variables and instantiate Scanner class in  SumUsersInput class

--- src/main/java/tb_scanner/SumUsersInput.java
+++ src/main/java/tb_scanner/SumUsersInput.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class SumUsersInput {
     public static int sumUsersInput(int number) {
-        int sum = 0;
         Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
         int counter = 1;
 
         return sum;
