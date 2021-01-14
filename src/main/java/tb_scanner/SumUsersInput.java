04b95364a51aeebd9262dd0276497df632c3ad56
MichalGuz <michalguz@gmail.com>
2021-01-14 22:03:52 +0100

    Create if condition and amend while loop in sumUsersInput method

--- src/main/java/tb_scanner/SumUsersInput.java
+++ src/main/java/tb_scanner/SumUsersInput.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class SumUsersInput {
     public static int sumUsersInput(int number) {
+        if(number < 1) {
+            System.out.println("Invalid value of sumUsersInput method's argument");
+            return -1;
+        }
         Scanner scanner = new Scanner(System.in);
 
         int sum = 0;
-        int counter = 1;
+        int counter = 0;
 
-        while (counter < 11) {
+        while (counter < number) {
             System.out.println("Enter number #" + counter);
             boolean hasNextInt = scanner.hasNextInt();
             if(hasNextInt) {
