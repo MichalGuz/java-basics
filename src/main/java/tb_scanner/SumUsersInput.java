ff0b047596a7a1b229fd2b357e648877142308b7
MichalGuz <michalguz@gmail.com>
2021-01-14 21:50:18 +0100

    Assign varial SumUsersInput class

--- src/main/java/tb_scanner/SumUsersInput.java
+++ src/main/java/tb_scanner/SumUsersInput.java
@@ -1,8 +1,13 @@
 package tb_scanner;
 
+import java.util.Scanner;
+
 public class SumUsersInput {
-    public static int sumUsersInput() {
-        int sum =0;
+    public static int sumUsersInput(int number) {
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        int counter = 1;
+
         return sum;
     }
 }
