070b040b62656f26cae3f0d711e325f8965427bd
MichalGuz <michalguz@gmail.com>
2021-01-15 21:54:10 +0100

    Call println and hasNextInt methods in minAndMaximumInput method.

--- src/main/java/tb_scanner/MinAndMaximumInput.java
+++ src/main/java/tb_scanner/MinAndMaximumInput.java
@@ -8,5 +8,8 @@ public class MinAndMaximumInput {
         int max = 0;
         int number = 0;
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        boolean hasNextInt = scanner.hasNextInt();
     }
 }
