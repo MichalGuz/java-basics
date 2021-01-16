f339f01175b39c5c1c64d9e0b27d0e935d66dee1
MichalGuz <michalguz@gmail.com>
2021-01-16 19:18:34 +0100

    Create void roundAndFormatFloats() method and instantiate Random and ArrayList classes

--- src/main/java/tb_round_numbers/RoundAndFormatFloats.java
+++ src/main/java/tb_round_numbers/RoundAndFormatFloats.java
@@ -1,4 +1,13 @@
 package tb_round_numbers;
 
+import tb_add_arrays_temp.AverageFromScanner;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class RoundAndFormatFloats {
+    public static void roundAndFormatFloats() {
+        Random random = new Random();
+        ArrayList<Double> numbers = new ArrayList<>();
+    }
 }
