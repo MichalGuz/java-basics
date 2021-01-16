959a0d704f3934eac67bffbba981add32f268e6c
MichalGuz <michalguz@gmail.com>
2021-01-16 17:46:16 +0100

    Create if condition inside while loop in sumAndAverageInput method

--- src/main/java/tb_scanner/SumAndAverageInput.java
+++ src/main/java/tb_scanner/SumAndAverageInput.java
@@ -6,12 +6,17 @@ public class SumAndAverageInput {
     public void sumAndAverageInput() {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
+        int enteredInteger = 0;
 
-        System.out.println("Start   ");
+        System.out.println("Start");
         while(true) {
-            System.out.println("Enter the integer");
+            System.out.println("Enter an integer");
             boolean hasNextInt = scanner.hasNextInt();
-
+            if(hasNextInt) {
+                enteredInteger = scanner.nextInt();
+                scanner.nextLine();
+                sum += enteredInteger;
+            }
         }
 
     }
