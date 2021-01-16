a96b8b92e91758ccd7789500b54c224057c58295
MichalGuz <michalguz@gmail.com>
2021-01-16 17:27:57 +0100

    Create void sumAndAverageInput() method in SumAndAverageInput class

--- src/main/java/tb_scanner/SumAndAverageInput.java
+++ src/main/java/tb_scanner/SumAndAverageInput.java
@@ -1,4 +1,12 @@
 package tb_scanner;
 
+import java.util.Scanner;
+
 public class SumAndAverageInput {
+    public void sumAndAverageInput() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+
+    }
 }
