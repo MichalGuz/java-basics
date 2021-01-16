2f598552d358c4e675dc96c5050ce1364a487d3b
MichalGuz <michalguz@gmail.com>
2021-01-16 20:02:43 +0100

    Create if else block in roundAndFormatFloats method

--- src/main/java/tb_round_numbers/RoundAndFormatFloats.java
+++ src/main/java/tb_round_numbers/RoundAndFormatFloats.java
@@ -7,12 +7,16 @@ public class RoundAndFormatFloats {
     public static void roundAndFormatFloats() {
         Random random = new Random();
         ArrayList<Double> numbers = new ArrayList<>();
-
+        double res = 0;
 
         for(double i=0; i<10; i++) {
             double d = random.nextInt(10);
             System.out.println("i = " + i + " d = "+ d );
-
+            if (d <= 1) {
+                res = (i / 1) / 4;
+            } else {
+                res = (i / d) / 4;
+            }
         }
     }
 }
