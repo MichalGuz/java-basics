92a0fef6ab094a5f6e4e00f0851e1e771eba9c92
MichalGuz <michalguz@gmail.com>
2021-01-17 16:58:18 +0100

    Instantiate Scanner object then call nextInt and nextLine methods in  printDayOfTheWeek() method

--- src/main/java/switch_and_input/SwitchWeek.java
+++ src/main/java/switch_and_input/SwitchWeek.java
@@ -1,7 +1,12 @@
 package switch_and_input;
 
+import java.util.Scanner;
+
 public class SwitchWeek {
     public static void printDayOfTheWeek() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number form range 1-7");
+        int day = scanner.nextInt();
+        scanner.nextLine();
     }
 }
