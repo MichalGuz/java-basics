96ac05ff9737ed4e072ccf8b13e46ef4ebcd9f18
MichalGuz <michalguz@gmail.com>
2021-01-17 17:04:39 +0100

    Create switch block in printDayOfTheWeek method

--- src/main/java/switch_and_input/SwitchWeek.java
+++ src/main/java/switch_and_input/SwitchWeek.java
@@ -8,5 +8,33 @@ public class SwitchWeek {
         System.out.println("Enter the number form range 1-7");
         int day = scanner.nextInt();
         scanner.nextLine();
+
+        switch (day) {
+            case 0:
+                System.out.println("End");
+                scanner.close();
+                break;
+            case 1:
+                System.out.println("Monday" + "\n[to quit enter 0]");
+                break;
+            case 2:
+                System.out.println("Tuesday" + "\n[to quit enter 0]");
+                break;
+            case 3:
+                System.out.println("Wednesday" + "\n[to quit enter 0]");
+                break;
+            case 4:
+                System.out.println("Thursday" + "\n[to quit enter 0]");
+                break;
+            case 5:
+                System.out.println("Friday" + "\n[to quit enter 0]");
+                break;
+            case 6:
+                System.out.println("Saturday" + "\n[to quit enter 0]");
+                break;
+            case 7:
+                System.out.println("Sunday" + "\n[to quit enter 0]");
+                break;
+        }
     }
 }
