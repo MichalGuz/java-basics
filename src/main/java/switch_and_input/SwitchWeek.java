8097cc931d04f19833efdb50ead72d907c928edd
MichalGuz <michalguz@gmail.com>
2021-01-17 17:21:19 +0100

    Create while loop in printDayOfTheWeek method

--- src/main/java/switch_and_input/SwitchWeek.java
+++ src/main/java/switch_and_input/SwitchWeek.java
@@ -6,39 +6,42 @@ public class SwitchWeek {
     public static void printDayOfTheWeek() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number form range 1-7");
-        int day = scanner.nextInt();
-        scanner.nextLine();
+        boolean quit = true;
 
-        if(day < 0 || day > 7) {
-            System.out.println("Invalid value, Enter the number form range 1-7");
-        }
-
-        switch (day) {
-            case 0:
-                System.out.println("End");
-                scanner.close();
-                break;
-            case 1:
-                System.out.println("Monday" + "\n[to quit enter 0]");
-                break;
-            case 2:
-                System.out.println("Tuesday" + "\n[to quit enter 0]");
-                break;
-            case 3:
-                System.out.println("Wednesday" + "\n[to quit enter 0]");
-                break;
-            case 4:
-                System.out.println("Thursday" + "\n[to quit enter 0]");
-                break;
-            case 5:
-                System.out.println("Friday" + "\n[to quit enter 0]");
-                break;
-            case 6:
-                System.out.println("Saturday" + "\n[to quit enter 0]");
-                break;
-            case 7:
-                System.out.println("Sunday" + "\n[to quit enter 0]");
-                break;
+        while (quit) {
+            int day = scanner.nextInt();
+            scanner.nextLine();
+            if(day < 0 || day > 7) {
+                System.out.println("Invalid value, Enter the number form range 1-7");
+            }
+            switch (day) {
+                case 0:
+                    System.out.println("End");
+                    scanner.close();
+                    quit = false;
+                    break;
+                case 1:
+                    System.out.println("Monday" + "\n[to quit enter 0]");
+                    break;
+                case 2:
+                    System.out.println("Tuesday" + "\n[to quit enter 0]");
+                    break;
+                case 3:
+                    System.out.println("Wednesday" + "\n[to quit enter 0]");
+                    break;
+                case 4:
+                    System.out.println("Thursday" + "\n[to quit enter 0]");
+                    break;
+                case 5:
+                    System.out.println("Friday" + "\n[to quit enter 0]");
+                    break;
+                case 6:
+                    System.out.println("Saturday" + "\n[to quit enter 0]");
+                    break;
+                case 7:
+                    System.out.println("Sunday" + "\n[to quit enter 0]");
+                    break;
+            }
         }
     }
 }
