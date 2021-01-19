d493dd82ce2dfc7f87c1ad5bb1f774d3b65c75a4
MichalGuz <michalguz@gmail.com>
2021-01-19 21:18:47 +0100

    Create if condition in getDaysInMonth method.

--- src/main/java/switch_and_input/SwitchDaysInMonths.java
+++ src/main/java/switch_and_input/SwitchDaysInMonths.java
@@ -2,6 +2,10 @@ package switch_and_input;
 
 public class SwitchDaysInMonths {
     public static int getDaysInMonth(int month, int year) {
+        int days = 0;
+        if ((month < 1) || (month > 12) || (year < 1) || (year > 9999)) {
+            return -1;
+        }
         return -1;
     }
 }
