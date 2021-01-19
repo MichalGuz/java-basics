69025bab607c8f84347a8e97125ddc8b1dc5c9df
MichalGuz <michalguz@gmail.com>
2021-01-19 22:17:32 +0100

    Create switch block in  else if block in getDaysInMonth method.

--- src/main/java/switch_and_input/SwitchDaysInMonths.java
+++ src/main/java/switch_and_input/SwitchDaysInMonths.java
@@ -18,6 +18,17 @@ public class SwitchDaysInMonths {
                     break;
             }
         } else if (isLeapYear(year) == false) {
+            switch (month) {
+                case 2:
+                    days = 28;
+                    break;
+                case 4: case 6: case 9: case 11:
+                    days = 30;
+                    break;
+                default:
+                    days = 31;
+                    break;
+            }
 
         }
         return days;
