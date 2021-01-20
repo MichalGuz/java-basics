61f1f25eff4f215ca7c4e72468281a2d58812147
MichalGuz <michalguz@gmail.com>
2021-01-20 20:54:42 +0100

    Create if condition in isPrime method

--- src/main/java/isPrime/IsPrime.java
+++ src/main/java/isPrime/IsPrime.java
@@ -2,6 +2,9 @@ package isPrime;
 
 public class IsPrime {
     public static boolean isPrime(int number) {
+        if(number < 2) {
+            return false;
+        }
         return false;
     }
 }
