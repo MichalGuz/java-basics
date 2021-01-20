99c7316e32118a86f5460d4ed53f83f0a2c73ad1
MichalGuz <michalguz@gmail.com>
2021-01-20 21:04:37 +0100

    Call println method in isPrime method

--- src/main/java/isPrime/IsPrime.java
+++ src/main/java/isPrime/IsPrime.java
@@ -3,14 +3,17 @@ package isPrime;
 public class IsPrime {
     public static boolean isPrime(int number) {
         if(number < 2) {
+            System.out.println(number + " is not a prime number");
             return false;
         }
 
         for(int i=2; i<=(long)Math.sqrt(i); i++) {
             if(number % i == 0) {
+                System.out.println(number + " is not a prime number");
                 return false;
             }
         }
+        System.out.println(number + " is a prime number");
         return true;
     }
 }
