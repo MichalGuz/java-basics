7c9eb81f1f9b514df20d7a07cf7dc2f6dfc254f3
MichalGuz <michalguz@gmail.com>
2021-01-20 21:21:12 +0100

    Renove useless calls of println method in isPrime method

--- src/main/java/isPrime/IsPrime.java
+++ src/main/java/isPrime/IsPrime.java
@@ -3,13 +3,11 @@ package isPrime;
 public class IsPrime {
     public static boolean isPrime(int number) {
         if(number < 2) {
-            System.out.println(number + " is not a prime number");
             return false;
         }
 
         for(int i=2; i<=(long)Math.sqrt(number); i++) {
             if(number % i == 0) {
-                System.out.println(number + " is not a prime number");
                 return false;
             }
         }
