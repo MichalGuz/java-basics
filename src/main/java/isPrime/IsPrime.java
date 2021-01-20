1bea9cfa7203b26a757cedd7f47101682b97de9b
MichalGuz <michalguz@gmail.com>
2021-01-20 21:02:19 +0100

    Create for loop in isPrime method

--- src/main/java/isPrime/IsPrime.java
+++ src/main/java/isPrime/IsPrime.java
@@ -5,6 +5,10 @@ public class IsPrime {
         if(number < 2) {
             return false;
         }
+
+        for(int i=2; i<=(long)Math.sqrt(i); i++) {
+
+        }
         return false;
     }
 }
