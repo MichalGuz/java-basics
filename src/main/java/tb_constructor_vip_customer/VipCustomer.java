edaa661dffc7b34bd85a4fd386d22cc00d7f6dda
MichalGuz <michalguz@gmail.com>
2021-01-21 21:44:46 +0100

    Create properties of VipCustomer class in tb_constructor_vip_customer package

--- src/main/java/tb_constructor_vip_customer/VipCustomer.java
+++ src/main/java/tb_constructor_vip_customer/VipCustomer.java
@@ -3,6 +3,11 @@ package tb_constructor_vip_customer;
 public class VipCustomer {
     private String name;
     private double creditLimit;
-    private String emailAdrees;
+    private String emailAddress;
 
+    public VipCustomer(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
 }
