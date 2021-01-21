84bcb9d6490c1429f5b348d3dc00efeadea3bd9b
MichalGuz <michalguz@gmail.com>
2021-01-21 20:43:50 +0100

    Create property and constructor in Cylinder class which extends Circle class

--- src/main/java/tb_inheritance_cylinder/Cylinder.java
+++ src/main/java/tb_inheritance_cylinder/Cylinder.java
@@ -1,4 +1,10 @@
 package tb_inheritance_cylinder;
 
 public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
 }
