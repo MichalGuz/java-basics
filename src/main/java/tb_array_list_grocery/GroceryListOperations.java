76ab94001538c5d83a65451c6ec76ac627589b76
MichalGuz <michalguz@gmail.com>
2021-01-22 22:33:54 +0100

    Create printInstructions method in GroceryListOperations class

--- src/main/java/tb_array_list_grocery/GroceryListOperations.java
+++ src/main/java/tb_array_list_grocery/GroceryListOperations.java
@@ -1,4 +1,17 @@
 package tb_array_list_grocery;
 
 public class GroceryListOperations {
+
+
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
 }
