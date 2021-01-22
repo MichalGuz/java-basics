5fc3dc5438797491b96ab947851875b9f8128ea9
MichalGuz <michalguz@gmail.com>
2021-01-22 22:25:26 +0100

    Create printGroceryList method in GroceryList class

--- src/main/java/tb_array_list_grocery/GroceryList.java
+++ src/main/java/tb_array_list_grocery/GroceryList.java
@@ -12,4 +12,14 @@ public class GroceryList {
     public void addGroceryItem(String item){
         grocery.add(item);
     }
+
+    public void printGroceryList(){
+        if(grocery.size() > 0) {
+            for (int i = 0; i < grocery.size(); i++) {
+                System.out.println("Item " + (i + 1) + " = " + grocery.get(i));
+            }
+        } else {
+            System.out.println("You have 0 item in grocery list");
+        }
+    }
 }
