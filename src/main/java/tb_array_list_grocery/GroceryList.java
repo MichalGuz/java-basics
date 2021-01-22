a239a90aedc1cd1d50a07a9048ae290fe9966fb2
MichalGuz <michalguz@gmail.com>
2021-01-22 22:23:20 +0100

    Create property in GroceryList class with getter

--- src/main/java/tb_array_list_grocery/GroceryList.java
+++ src/main/java/tb_array_list_grocery/GroceryList.java
@@ -1,4 +1,11 @@
 package tb_array_list_grocery;
 
+import java.util.ArrayList;
+
 public class GroceryList {
+    private ArrayList<String> grocery = new ArrayList<>();
+
+    public ArrayList<String> getGrocery() {
+        return grocery;
+    }
 }
