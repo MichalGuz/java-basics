7c0f7628410b81c3071e4b3d7bb5d03b93b0df56
MichalGuz <michalguz@gmail.com>
2021-01-24 21:36:19 +0100

    Modify constructor in Chandelier class

--- src/main/java/tb_composition_room/Chandelier.java
+++ src/main/java/tb_composition_room/Chandelier.java
@@ -2,7 +2,7 @@ package tb_composition_room;
 
 public class Chandelier extends Lamp {
     public Chandelier(String name, String style, String color, int numberOfLights, String typeOfLights, double width, double high) {
-        super(name, style, color, numberOfLights, typeOfLights, width, high);
+        super("chandelier", style, color, numberOfLights, typeOfLights, width, high);
     }
 
     @Override
