00ca8459e7ca7c3bdb46c864393836ab1a2fd11d
MichalGuz <michalguz@gmail.com>
2021-01-24 20:48:05 +0100

    Create properties of Room class

--- src/main/java/tb_composition_room/Room.java
+++ src/main/java/tb_composition_room/Room.java
@@ -1,4 +1,11 @@
 package tb_composition_room;
 
 public class Room {
+    private Ceiling ceiling;
+    private Furniture furniture;
+    private Lights lights;
+    private Floor floor;
+    private Walls walls;
+
+
 }
