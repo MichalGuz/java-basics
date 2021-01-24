2168c9923cce829af7fee64fdcceba7eef23a382
MichalGuz <michalguz@gmail.com>
2021-01-24 21:21:36 +0100

    Create getters for all properties of Lights class

--- src/main/java/tb_composition_room/Lights.java
+++ src/main/java/tb_composition_room/Lights.java
@@ -7,4 +7,11 @@ public class Lights {
     private int numberOfTableLamps;
     private int numberOfFloorLamps;
 
+    public Lights(Chandelier chandelier, TableLamp tableLamp, FloorLamp floorLamp, int numberOfTableLamps, int numberOfFloorLamps) {
+        this.chandelier = chandelier;
+        this.tableLamp = tableLamp;
+        this.floorLamp = floorLamp;
+        this.numberOfTableLamps = numberOfTableLamps;
+        this.numberOfFloorLamps = numberOfFloorLamps;
+    }
 }
