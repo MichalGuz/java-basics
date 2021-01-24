a50060d88637dbc807c472fcec47c54d486f861f
MichalGuz <michalguz@gmail.com>
2021-01-24 21:20:56 +0100

    Create properties of Lights class

--- src/main/java/tb_composition_room/Lights.java
+++ src/main/java/tb_composition_room/Lights.java
@@ -1,4 +1,10 @@
 package tb_composition_room;
 
 public class Lights {
+    private Chandelier chandelier;
+    private TableLamp tableLamp;
+    private FloorLamp floorLamp;
+    private int numberOfTableLamps;
+    private int numberOfFloorLamps;
+
 }
