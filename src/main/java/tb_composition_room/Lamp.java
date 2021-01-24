879a34d2042101ca2a4d0c24aec58fd9f1710dbf
MichalGuz <michalguz@gmail.com>
2021-01-24 21:27:09 +0100

    Create properties of Lamp class

--- src/main/java/tb_composition_room/Lamp.java
+++ src/main/java/tb_composition_room/Lamp.java
@@ -1,4 +1,11 @@
 package tb_composition_room;
 
 public class Lamp {
+    private String name;
+    private String style;
+    private String color;
+    private int numberOfLights;
+    private String typeOfLights;
+    private double width;
+    private double high;
 }
