6bf1efbf88cb5eb5d18518d5f081ae91fe365753
MichalGuz <michalguz@gmail.com>
2021-01-25 21:20:44 +0100

    Create constructor in TableLamp class

--- src/main/java/tb_composition_room/TableLamp.java
+++ src/main/java/tb_composition_room/TableLamp.java
@@ -1,6 +1,7 @@
 package tb_composition_room;
 
 public class TableLamp extends Lamp {
-
-
+    public TableLamp(String style, String color, int numberOfLights, String typeOfLights, double width, double high) {
+        super("table lamp", style, color, numberOfLights, typeOfLights, width, high);
+    }
 }
