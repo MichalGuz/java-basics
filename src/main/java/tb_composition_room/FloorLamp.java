59512ba59bcaf5c37eca4060067ac49fb47ee891
MichalGuz <michalguz@gmail.com>
2021-01-25 21:36:10 +0100

    Create constructor in FloorLamp class

--- src/main/java/tb_composition_room/FloorLamp.java
+++ src/main/java/tb_composition_room/FloorLamp.java
@@ -1,4 +1,7 @@
 package tb_composition_room;
 
 public class FloorLamp extends Lamp {
+    public FloorLamp(String name, String style, String color, int numberOfLights, String typeOfLights, double width, double high) {
+        super(name, style, color, numberOfLights, typeOfLights, width, high);
+    }
 }
