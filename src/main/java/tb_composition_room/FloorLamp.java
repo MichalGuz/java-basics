3b46f86c16e32effcd6b559fcebe4e7cc927141d
MichalGuz <michalguz@gmail.com>
2021-01-25 21:40:19 +0100

    Override methods of Lamp class in FloorLamp class

--- src/main/java/tb_composition_room/FloorLamp.java
+++ src/main/java/tb_composition_room/FloorLamp.java
@@ -4,4 +4,49 @@ public class FloorLamp extends Lamp {
     public FloorLamp(String name, String style, String color, int numberOfLights, String typeOfLights, double width, double high) {
         super("floor lamp", style, color, numberOfLights, typeOfLights, width, high);
     }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getStyle() {
+        return super.getStyle();
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public int getNumberOfLights() {
+        return super.getNumberOfLights();
+    }
+
+    @Override
+    public String getTypeOfLights() {
+        return super.getTypeOfLights();
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getHigh() {
+        return super.getHigh();
+    }
+
+    @Override
+    public void turnOnTheLamp() {
+        super.turnOnTheLamp();
+    }
+
+    @Override
+    public void turnOffTheLamp() {
+        super.turnOffTheLamp();
+    }
 }
