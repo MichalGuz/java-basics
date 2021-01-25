ec8c59c4ae3199fb164e920308a151e46a3ef842
MichalGuz <michalguz@gmail.com>
2021-01-25 21:53:29 +0100

    Create constructor and getters in Hoover class

--- src/main/java/tb_composition_room/Hoover.java
+++ src/main/java/tb_composition_room/Hoover.java
@@ -3,4 +3,17 @@ package tb_composition_room;
 public class Hoover {
     private String name;
     private int power;
+
+    public Hoover(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
 }
