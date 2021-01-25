adf53d409d7dcf1e8d9abd4e3ab08a6df01b2831
MichalGuz <michalguz@gmail.com>
2021-01-25 21:36:41 +0100

    Amend constructor in FloorLamp class

--- src/main/java/tb_composition_room/FloorLamp.java
+++ src/main/java/tb_composition_room/FloorLamp.java
@@ -2,6 +2,6 @@ package tb_composition_room;
 
 public class FloorLamp extends Lamp {
     public FloorLamp(String name, String style, String color, int numberOfLights, String typeOfLights, double width, double high) {
-        super(name, style, color, numberOfLights, typeOfLights, width, high);
+        super("floor lamp", style, color, numberOfLights, typeOfLights, width, high);
     }
 }
