49a0d3adde1daf3b92c4f48d64f30edf9c85a7f8
MichalGuz <michalguz@gmail.com>
2021-01-25 21:57:38 +0100

    Create constructor and getters in Floor class

--- src/main/java/tb_composition_room/Floor.java
+++ src/main/java/tb_composition_room/Floor.java
@@ -6,4 +6,26 @@ public class Floor {
     private String carpet;
     private Hoover hoover;
 
+    public Floor(double width, double length, String carpet, Hoover hoover) {
+        this.width = width;
+        this.length = length;
+        this.carpet = carpet;
+        this.hoover = hoover;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String getCarpet() {
+        return carpet;
+    }
+
+    public Hoover getHoover() {
+        return hoover;
+    }
 }
