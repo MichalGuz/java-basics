b7f5188f4cefc6c315fb3a5d3db8461f81074269
MichalGuz <michalguz@gmail.com>
2021-01-26 23:34:15 +0100

    Create constructor in Sofa class

--- src/main/java/tb_composition_room/Sofa.java
+++ src/main/java/tb_composition_room/Sofa.java
@@ -1,5 +1,7 @@
 package tb_composition_room;
 
 public class Sofa extends FurnitureItem {
-
+    public Sofa(String name, double height, double width, double length, String color, String material) {
+        super(name, height, width, length, color, material);
+    }
 }
