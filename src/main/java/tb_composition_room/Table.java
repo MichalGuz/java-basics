48440848b4654465235e4c4da37b554ac2e1d217
MichalGuz <michalguz@gmail.com>
2021-01-26 23:24:24 +0100

    Add a property and amnend constructor in Table class

--- src/main/java/tb_composition_room/Table.java
+++ src/main/java/tb_composition_room/Table.java
@@ -1,8 +1,11 @@
 package tb_composition_room;
 
 public class Table extends FurnitureItem{
-    public Table(String name, double height, double width, double length, String color, String material) {
-        super(name, height, width, length, color, material);
+    private int numberOfLegs;
+
+    public Table(String name, double height, double width, double length, String color, String material,  int numberOfLegs) {
+        super("table", height, width, length, color, material);
+        this.numberOfLegs = numberOfLegs;
     }
 }
 
