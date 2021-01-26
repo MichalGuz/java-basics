c59c6e85ba37cda7f5badf9fbfcf244bff5a6740
MichalGuz <michalguz@gmail.com>
2021-01-26 23:14:02 +0100

    Create properties od FurnitureItem class

--- src/main/java/tb_composition_room/FurnitureItem.java
+++ src/main/java/tb_composition_room/FurnitureItem.java
@@ -1,4 +1,11 @@
 package tb_composition_room;
 
 public class FurnitureItem {
+    private String name;
+    private double height;
+    private double width;
+    private double length;
+    private String color;
+    private String material;
+
 }
