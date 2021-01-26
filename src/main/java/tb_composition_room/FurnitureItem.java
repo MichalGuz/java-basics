dcc70042cd924507c3557ad3bfec3f3e5a935b48
MichalGuz <michalguz@gmail.com>
2021-01-26 23:15:26 +0100

    Create constructor in FurnitureItem class

--- src/main/java/tb_composition_room/FurnitureItem.java
+++ src/main/java/tb_composition_room/FurnitureItem.java
@@ -8,4 +8,12 @@ public class FurnitureItem {
     private String color;
     private String material;
 
+    public FurnitureItem(String name, double height, double width, double length, String color, String material) {
+        this.name = name;
+        this.height = height;
+        this.width = width;
+        this.length = length;
+        this.color = color;
+        this.material = material;
+    }
 }
