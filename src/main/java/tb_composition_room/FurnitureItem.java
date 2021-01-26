2f303e811a056bd9ee1a9c5a5bf0a7289e59f7c9
MichalGuz <michalguz@gmail.com>
2021-01-26 23:16:33 +0100

    Create getters in FurnitureItem class

--- src/main/java/tb_composition_room/FurnitureItem.java
+++ src/main/java/tb_composition_room/FurnitureItem.java
@@ -16,4 +16,29 @@ public class FurnitureItem {
         this.color = color;
         this.material = material;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
 }
