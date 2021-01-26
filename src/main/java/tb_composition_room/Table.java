4fd456631c4a67d0b81cba470906b648a63d1c78
MichalGuz <michalguz@gmail.com>
2021-01-26 23:22:33 +0100

    Create constructor in Table class

--- src/main/java/tb_composition_room/Table.java
+++ src/main/java/tb_composition_room/Table.java
@@ -1,4 +1,8 @@
 package tb_composition_room;
 
 public class Table extends FurnitureItem{
+    public Table(String name, double height, double width, double length, String color, String material) {
+        super(name, height, width, length, color, material);
+    }
 }
+
