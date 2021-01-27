7ccf8e42c5276a66c731cc0493fd3fc1150dd6ef
MichalGuz <michalguz@gmail.com>
2021-01-27 21:03:13 +0100

    Amend constructor in Table class

--- src/main/java/tb_composition_room/Table.java
+++ src/main/java/tb_composition_room/Table.java
@@ -3,7 +3,7 @@ package tb_composition_room;
 public class Table extends FurnitureItem{
     private int numberOfLegs;
 
-    public Table(String name, double height, double width, double length, String color, String material,  int numberOfLegs) {
+    public Table(double height, double width, double length, String color, String material,  int numberOfLegs) {
         super("table", height, width, length, color, material);
         this.numberOfLegs = numberOfLegs;
     }
