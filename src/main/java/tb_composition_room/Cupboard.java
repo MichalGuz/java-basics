46ea14a5e81cb648948b1209fcdfdae540cda793
MichalGuz <michalguz@gmail.com>
2021-01-27 20:53:47 +0100

    Create constructor in Cupboard class with default value of name variable

--- src/main/java/tb_composition_room/Cupboard.java
+++ src/main/java/tb_composition_room/Cupboard.java
@@ -1,4 +1,7 @@
 package tb_composition_room;
 
 public class Cupboard extends FurnitureItem {
+    public Cupboard(String name, double height, double width, double length, String color, String material) {
+        super("cupboard", height, width, length, color, material);
+    }
 }
