7e70d3da4051a82612cd9bdd777fc84c10991676
MichalGuz <michalguz@gmail.com>
2021-01-27 20:47:59 +0100

    Amend constructor in Chair class

--- src/main/java/tb_composition_room/Chair.java
+++ src/main/java/tb_composition_room/Chair.java
@@ -2,6 +2,6 @@ package tb_composition_room;
 
 public class Chair extends FurnitureItem {
     public Chair(String name, double height, double width, double length, String color, String material) {
-        super(name, height, width, length, color, material);
+        super("chair", height, width, length, color, material);
     }
 }
