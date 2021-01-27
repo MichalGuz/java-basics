aa25d94fa396e72adebe43b5efe623409db04131
MichalGuz <michalguz@gmail.com>
2021-01-27 20:48:55 +0100

    Override methods of FurnitureItem class in Chair class

--- src/main/java/tb_composition_room/Chair.java
+++ src/main/java/tb_composition_room/Chair.java
@@ -4,4 +4,34 @@ public class Chair extends FurnitureItem {
     public Chair(String name, double height, double width, double length, String color, String material) {
         super("chair", height, width, length, color, material);
     }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getLength() {
+        return super.getLength();
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public String getMaterial() {
+        return super.getMaterial();
+    }
 }
