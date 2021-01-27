c9639e85941d10c7494a301d60e88fe1f5db3d43
MichalGuz <michalguz@gmail.com>
2021-01-27 20:41:02 +0100

    Amend constructor in Bed class

--- src/main/java/tb_composition_room/Bed.java
+++ src/main/java/tb_composition_room/Bed.java
@@ -2,6 +2,6 @@ package tb_composition_room;
 
 public class Bed extends FurnitureItem {
     public Bed(String name, double height, double width, double length, String color, String material) {
-        super(name, height, width, length, color, material);
+        super("bed", height, width, length, color, material);
     }
 }
