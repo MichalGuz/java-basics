69e096ca7010b79b78d4cb3d7a7156999a3db413
MichalGuz <michalguz@gmail.com>
2021-01-27 20:55:30 +0100

    Override methods of FurnitureItem class in Cupboard class

--- src/main/java/tb_composition_room/Cupboard.java
+++ src/main/java/tb_composition_room/Cupboard.java
@@ -1,7 +1,37 @@
 package tb_composition_room;
 
 public class Cupboard extends FurnitureItem {
-    public Cupboard(String name, double height, double width, double length, String color, String material) {
+    public Cupboard(double height, double width, double length, String color, String material) {
         super("cupboard", height, width, length, color, material);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getLength() {
+        return super.getLength();
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public String getMaterial() {
+        return super.getMaterial();
     }
 }
