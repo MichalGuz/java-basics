892a44256d0556a939f068ffed51959023c7bb78
MichalGuz <michalguz@gmail.com>
2021-01-27 21:02:36 +0100

    Amend constructor in Sofa class

--- src/main/java/tb_composition_room/Sofa.java
+++ src/main/java/tb_composition_room/Sofa.java
@@ -1,7 +1,7 @@
 package tb_composition_room;
 
 public class Sofa extends FurnitureItem {
-    public Sofa(String name, double height, double width, double length, String color, String material) {
+    public Sofa(double height, double width, double length, String color, String material) {
         super("sofa", height, width, length, color, material);
     }
 
