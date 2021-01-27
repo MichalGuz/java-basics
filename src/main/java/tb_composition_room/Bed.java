1e738fde27ff60632fba43a5d779d24c7060f2b8
MichalGuz <michalguz@gmail.com>
2021-01-27 20:39:12 +0100

    Create constructor Bed class

--- src/main/java/tb_composition_room/Bed.java
+++ src/main/java/tb_composition_room/Bed.java
@@ -1,4 +1,7 @@
 package tb_composition_room;
 
 public class Bed extends FurnitureItem {
+    public Bed(String name, double height, double width, double length, String color, String material) {
+        super(name, height, width, length, color, material);
+    }
 }
