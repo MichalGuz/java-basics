d46cb1774fe107df7f7ebca4ec404b4ee8ae9e27
MichalGuz <michalguz@gmail.com>
2021-01-27 20:43:02 +0100

    Override methods of FurnitureItem class in Bed class

--- src/main/java/tb_composition_room/Bed.java
+++ src/main/java/tb_composition_room/Bed.java
@@ -4,4 +4,34 @@ public class Bed extends FurnitureItem {
     public Bed(String name, double height, double width, double length, String color, String material) {
         super("bed", height, width, length, color, material);
     }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getLength() {
+        return super.getLength();
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public String getMaterial() {
+        return super.getMaterial();
+    }
 }
