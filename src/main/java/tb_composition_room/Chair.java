77bca79255b95d63249d45e8d51f8d10e5c91f6e
MichalGuz <michalguz@gmail.com>
2021-01-27 20:47:21 +0100

    Create constructor in Chair class

--- src/main/java/tb_composition_room/Chair.java
+++ src/main/java/tb_composition_room/Chair.java
@@ -1,4 +1,7 @@
 package tb_composition_room;
 
 public class Chair extends FurnitureItem {
+    public Chair(String name, double height, double width, double length, String color, String material) {
+        super(name, height, width, length, color, material);
+    }
 }
