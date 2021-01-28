26e32fa57d0b2494582bf90ea5fee44bad38b5f4
MichalGuz <michalguz@gmail.com>
2021-01-28 18:58:55 +0100

    Create constructor and getters in Doors class

--- src/main/java/tb_composition_room/Doors.java
+++ src/main/java/tb_composition_room/Doors.java
@@ -4,4 +4,22 @@ public class Doors {
     private int numberOfDoors;
     private double width;
     private double heigth;
+
+    public Doors(int numberOfDoors, double width, double heigth) {
+        this.numberOfDoors = numberOfDoors;
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeigth() {
+        return heigth;
+    }
 }
