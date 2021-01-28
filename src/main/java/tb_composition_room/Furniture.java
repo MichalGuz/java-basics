7dc2147cb0d6bfa416a41a0d1525fe12b4c6b1aa
MichalGuz <michalguz@gmail.com>
2021-01-28 18:53:20 +0100

    Create properties of Furniture class

--- src/main/java/tb_composition_room/Furniture.java
+++ src/main/java/tb_composition_room/Furniture.java
@@ -1,4 +1,10 @@
 package tb_composition_room;
 
 public class Furniture {
+    private Bed bed;
+    private Sofa sofa;
+    private Cupboard cupboard;
+    private Table table;
+    private Chair chair;
+
 }
