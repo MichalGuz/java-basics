75f46bc8a268eabc08dd5ab1d5915c10b78361ca
MichalGuz <michalguz@gmail.com>
2021-01-28 19:02:35 +0100

    Create constructor and getters in WindowInRoom class

--- src/main/java/tb_composition_room/WindowInRoom.java
+++ src/main/java/tb_composition_room/WindowInRoom.java
@@ -4,4 +4,22 @@ public class WindowInRoom {
     private int numberOfWindows;
     private double width;
     private double heigth;
+
+    public WindowInRoom(int numberOfWindows, double width, double heigth) {
+        this.numberOfWindows = numberOfWindows;
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    public int getNumberOfWindows() {
+        return numberOfWindows;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeigth() {
+        return heigth;
+    }
 }
