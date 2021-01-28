82491e41f9bf29e1c401e8027c886e50c340e26d
MichalGuz <michalguz@gmail.com>
2021-01-28 19:08:57 +0100

    Create properties in Walls class

--- src/main/java/tb_composition_room/Walls.java
+++ src/main/java/tb_composition_room/Walls.java
@@ -1,4 +1,11 @@
 package tb_composition_room;
 
 public class Walls {
+    private double length;
+    private double width;
+    private double heigth;
+    private double numberOfWalls;
+    private String colour;
+    private WindowInRoom windowInRoom;
+    private Doors doors;
 }
