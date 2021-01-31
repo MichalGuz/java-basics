0482397397cc3475adbe4350db3ade79b27b7086
MichalGuz <michalguz@gmail.com>
2021-01-31 19:44:16 +0100

    Create constructor in Printer class

--- src/main/java/tb_encapsulation/Printer.java
+++ src/main/java/tb_encapsulation/Printer.java
@@ -4,4 +4,10 @@ public class Printer {
     private double tonerLevel;
     private int printedPages;
     private boolean duplex;
+
+    public Printer(double tonerLevel, int printedPages, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.printedPages = printedPages;
+        this.duplex = duplex;
+    }
 }
