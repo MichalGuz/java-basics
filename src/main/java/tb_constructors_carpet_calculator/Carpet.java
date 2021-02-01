fc0f7cc14837f29bb07212f565dc2ecbd5b70478
MichalGuz <michalguz@gmail.com>
2021-02-01 22:53:57 +0100

    Create if else block to validate cost variable in constructor in Carpet class

--- src/main/java/tb_constructors_carpet_calculator/Carpet.java
+++ src/main/java/tb_constructors_carpet_calculator/Carpet.java
@@ -4,6 +4,10 @@ public class Carpet {
     private double cost;
 
     public Carpet(double cost) {
-        this.cost = cost;
+        if (cost < 0) {
+            this.cost = 0;
+        } else {
+            this.cost = cost;
+        }
     }
 }
