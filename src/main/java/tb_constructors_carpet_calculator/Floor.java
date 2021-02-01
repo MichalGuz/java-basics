f812a289765a8cc231213f62fa1d975393ecf31d
MichalGuz <michalguz@gmail.com>
2021-02-01 22:48:57 +0100

    Create constructor in Floor class

--- src/main/java/tb_constructors_carpet_calculator/Floor.java
+++ src/main/java/tb_constructors_carpet_calculator/Floor.java
@@ -3,4 +3,9 @@ package tb_constructors_carpet_calculator;
 public class Floor {
     private double width;
     private double length;
+
+    public Floor(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
 }
