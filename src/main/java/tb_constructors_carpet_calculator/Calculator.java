539000d75d8f2eedb7963ed9bc904a4d443d4e80
MichalGuz <michalguz@gmail.com>
2021-02-01 23:02:06 +0100

    Create main method in Calculator class

--- src/main/java/tb_constructors_carpet_calculator/Calculator.java
+++ src/main/java/tb_constructors_carpet_calculator/Calculator.java
@@ -12,4 +12,12 @@ public class Calculator {
     public double getTotalCost() {
         return floor.getArea()* carpet.getCost();
     }
+
+    public static void main(String[] args) {
+        Floor floor = new Floor(2.55, 5.42);
+        Carpet carpet = new Carpet(3.5);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total cost = " + calculator.getTotalCost());
+    }
 }
+
