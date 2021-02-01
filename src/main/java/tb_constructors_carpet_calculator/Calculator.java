639ee8cc47c04263b8b1d970bfd6c09c9ae58be8
MichalGuz <michalguz@gmail.com>
2021-02-01 22:56:34 +0100

    Create constructor in Calculator class

--- src/main/java/tb_constructors_carpet_calculator/Calculator.java
+++ src/main/java/tb_constructors_carpet_calculator/Calculator.java
@@ -3,4 +3,11 @@ package tb_constructors_carpet_calculator;
 public class Calculator {
     private Floor floor;
     private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+
 }
