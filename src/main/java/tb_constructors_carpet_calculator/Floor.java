bc636fd82d0bbf4c0d3c066b46510f80b586f02f
MichalGuz <michalguz@gmail.com>
2021-02-01 22:50:28 +0100

    Create if else block to validate windth variable in constructor in Floor class

--- src/main/java/tb_constructors_carpet_calculator/Floor.java
+++ src/main/java/tb_constructors_carpet_calculator/Floor.java
@@ -5,7 +5,11 @@ public class Floor {
     private double length;
 
     public Floor(double width, double length) {
-        this.width = width;
+        if(width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
         if (length < 0) {
             this.length = 0;
         } else {
