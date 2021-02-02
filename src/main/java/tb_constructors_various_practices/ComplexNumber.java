2acee8a25f0f1035c1d48b1342d3761051ea978e
MichalGuz <michalguz@gmail.com>
2021-02-02 19:26:04 +0100

    Create constructor in ComplexNumber class

--- src/main/java/tb_constructors_various_practices/ComplexNumber.java
+++ src/main/java/tb_constructors_various_practices/ComplexNumber.java
@@ -3,4 +3,9 @@ package tb_constructors_various_practices;
 public class ComplexNumber {
     private double real;
     private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
 }
