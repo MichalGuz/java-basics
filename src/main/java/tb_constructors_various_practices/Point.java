33f08c5935b2db83d56807b3d854a81c8dfce358
MichalGuz <michalguz@gmail.com>
2021-02-03 20:58:01 +0100

    Create double distance(int x, int y)  method

--- src/main/java/tb_constructors_various_practices/Point.java
+++ src/main/java/tb_constructors_various_practices/Point.java
@@ -19,4 +19,16 @@ public class Point {
     public int getY() {
         return y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(int x, int y){
+        return 0;
+    }
 }
