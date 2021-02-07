5a6ae2138ebf84878ba345f4f915483c1be5eaad
MichalGuz <michalguz@gmail.com>
2021-02-07 18:09:23 +0100

    Create getters and setters in Wall class

--- src/main/java/tb_constructors_various_practices/Wall.java
+++ src/main/java/tb_constructors_various_practices/Wall.java
@@ -12,5 +12,19 @@ public class Wall {
         this.height = height;
     }
 
+    public double getWidth() {
+        return width;
+    }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
