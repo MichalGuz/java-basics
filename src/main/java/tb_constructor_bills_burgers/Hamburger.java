890737da9193986fa096237cfb8670d1612ab28c
MichalGuz <michalguz@gmail.com>
2021-02-07 18:36:20 +0100

    Create properties in Hamburger class

--- src/main/java/tb_constructor_bills_burgers/Hamburger.java
+++ src/main/java/tb_constructor_bills_burgers/Hamburger.java
@@ -1,5 +1,12 @@
 package tb_constructor_bills_burgers;
 
 public class Hamburger {
-
+    private String name;
+    private String roll;
+    private String meat;
+    private double price;
+    private String extra1;
+    private String extra2;
+    private String extra3;
+    private String extra4;
 }
