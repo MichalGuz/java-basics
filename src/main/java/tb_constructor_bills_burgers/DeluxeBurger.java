b84068a46e1906367597f205462e4175f3eb15af
MichalGuz <michalguz@gmail.com>
2021-02-08 22:32:36 +0100

    Create two constructors: one with call super method and one with call this method in DeluxeBurger class

--- src/main/java/tb_constructor_bills_burgers/DeluxeBurger.java
+++ src/main/java/tb_constructor_bills_burgers/DeluxeBurger.java
@@ -3,4 +3,14 @@ package tb_constructor_bills_burgers;
 public class DeluxeBurger extends Hamburger {
     private String chips;
     private String drinks;
+
+    public DeluxeBurger(String name, String roll, String meat, double price, String extra1, String extra2, String extra3, String extra4, String chips, String drinks) {
+        super(name, roll, meat, price, extra1, extra2, extra3, extra4);
+        this.chips = chips;
+        this.drinks = drinks;
+    }
+
+    public DeluxeBurger() {
+        this("deluxe burger", "white", "beef", 26.99, "lettuce", "mayo", "tomato", "red onion", "chips", "cola");
+    }
 }
