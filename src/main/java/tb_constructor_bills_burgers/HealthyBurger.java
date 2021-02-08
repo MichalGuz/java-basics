a9023924d1ee3a140de80278ebcd7a40607ba774
MichalGuz <michalguz@gmail.com>
2021-02-08 22:13:56 +0100

    Create constructor with call super() method  in HealthyBurger class

--- src/main/java/tb_constructor_bills_burgers/HealthyBurger.java
+++ src/main/java/tb_constructor_bills_burgers/HealthyBurger.java
@@ -3,4 +3,10 @@ package tb_constructor_bills_burgers;
 public class HealthyBurger extends Hamburger {
     private String extra5;
     private String extra6;
+
+    public HealthyBurger(String name, String roll, String meat, double price, String extra1, String extra2, String extra3, String extra4, String extra5, String extra6) {
+        super(name, roll, meat, price, extra1, extra2, extra3, extra4);
+        this.extra5 = extra5;
+        this.extra6 = extra6;
+    }
 }
