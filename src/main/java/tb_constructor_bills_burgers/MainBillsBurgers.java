35e226f93027014886a450bb43d0acceb43e52a8
MichalGuz <michalguz@gmail.com>
2021-02-09 19:59:10 +0100

    Add another instance of HealthyBurger class and call methods of that class in main method in MainBillsBurgers class

--- src/main/java/tb_constructor_bills_burgers/MainBillsBurgers.java
+++ src/main/java/tb_constructor_bills_burgers/MainBillsBurgers.java
@@ -10,5 +10,12 @@ public class MainBillsBurgers {
 
         HealthyBurger healthyBurger = new HealthyBurger("cucumber");
         System.out.println(healthyBurger.getTotalPrice());
+
+        HealthyBurger secondHealthyBurger = new HealthyBurger("pepper", "black seed");
+        System.out.println("Basic healthy burger costs " + secondHamburger.getPrice());
+        System.out.println("Price of 5th extra [" + secondHealthyBurger.getExtra5() + "] costs " + secondHealthyBurger.getExtra5Price() );
+        System.out.println("Price of 6th extra [" + secondHealthyBurger.getExtra6() + "] costs " + secondHealthyBurger.getExtra6Price() );
+        System.out.println("The " + secondHealthyBurger.getName() + " costs " + secondHealthyBurger.getTotalPrice());
+
     }
 }
