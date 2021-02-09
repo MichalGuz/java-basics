69247bb7baf275e10c24991c2ccb0f5eeb54531a
MichalGuz <michalguz@gmail.com>
2021-02-09 19:25:11 +0100

    Create main method in MainBillsBurgers class, instantiate Hamburgr class and call getTotalPrice method

--- src/main/java/tb_constructor_bills_burgers/MainBillsBurgers.java
+++ src/main/java/tb_constructor_bills_burgers/MainBillsBurgers.java
@@ -1,4 +1,9 @@
 package tb_constructor_bills_burgers;
 
 public class MainBillsBurgers {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger();
+        System.out.println(hamburger.getTotalPrice());
+    }
+
 }
