b8e15daa8cecf14e0cb2c6362522b0ea797a7569
MichalGuz <michalguz@gmail.com>
2021-02-09 20:27:29 +0100

    Amend calls of println method in main method in MainBillsBurgers class

--- src/main/java/tb_constructor_bills_burgers/MainBillsBurgers.java
+++ src/main/java/tb_constructor_bills_burgers/MainBillsBurgers.java
@@ -12,10 +12,10 @@ public class MainBillsBurgers {
         System.out.println("Healthy burger with one extra addition costs " + healthyBurger.getTotalPrice());
 
         HealthyBurger secondHealthyBurger = new HealthyBurger("pepper", "black seed");
-        System.out.println("Basic healthy burger costs " + secondHamburger.getPrice());
+        System.out.println("Basic healthy burger costs " + secondHealthyBurger.getPrice());
         System.out.println("Price of 5th extra [" + secondHealthyBurger.getExtra5() + "] costs " + secondHealthyBurger.getExtra5Price() );
         System.out.println("Price of 6th extra [" + secondHealthyBurger.getExtra6() + "] costs " + secondHealthyBurger.getExtra6Price() );
-        System.out.println("The " + secondHealthyBurger.getName() + " costs " + secondHealthyBurger.getTotalPrice());
+        System.out.println("The second " + secondHealthyBurger.getName() + " costs " + secondHealthyBurger.getTotalPrice());
 
         DeluxeBurger deluxeBurger = new DeluxeBurger();
         System.out.println("The " + deluxeBurger.getName() + " costs " + deluxeBurger.getTotalPrice() + " [" + deluxeBurger.getChips() + " and " + deluxeBurger.getDrinks() + " included]");
