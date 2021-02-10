406bbdfde3957c079eb1154bb30dcef429460c3a
MichalGuz <michalguz@gmail.com>
2021-02-10 21:57:56 +0100

    Create properties of Vehicle class

--- src/main/java/tb_inheritance_vehicle/Vehicle.java
+++ src/main/java/tb_inheritance_vehicle/Vehicle.java
@@ -1,4 +1,10 @@
 package tb_inheritance_vehicle;
 
 public class Vehicle {
+    private String name;
+    private String size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
 }
