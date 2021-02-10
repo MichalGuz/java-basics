4182e44e1fdd169b9f0f8f1616ce31b93c198027
MichalGuz <michalguz@gmail.com>
2021-02-10 22:08:17 +0100

    Create constructor in Car class

--- src/main/java/tb_inheritance_vehicle/Car.java
+++ src/main/java/tb_inheritance_vehicle/Car.java
@@ -1,4 +1,18 @@
 package tb_inheritance_vehicle;
 
 public class Car extends Vehicle {
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = currentGear;
+    }
 }
