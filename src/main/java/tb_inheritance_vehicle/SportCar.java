486539dad8234f3a1b8de40a22258f46d0c0f163
MichalGuz <michalguz@gmail.com>
2021-02-12 22:32:07 +0100

    Amend constructor in SportCar class

--- src/main/java/tb_inheritance_vehicle/SportCar.java
+++ src/main/java/tb_inheritance_vehicle/SportCar.java
@@ -3,8 +3,8 @@ package tb_inheritance_vehicle;
 public class SportCar extends Car {
     private int roadServiceMonth;
 
-    public SportCar(String name, String size, int wheels, int doors, int gears, boolean isManual, int roadServiceMonth) {
-        super(name, size, wheels, doors, gears, isManual);
+    public SportCar(String name, String size, int doors, int gears, boolean isManual, int roadServiceMonth) {
+        super(name, size, 4, doors, gears, isManual);
         this.roadServiceMonth = roadServiceMonth;
     }
 }
