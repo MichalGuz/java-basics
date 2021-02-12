7eafe73001f734876a1dbe472a67556e3601beef
MichalGuz <michalguz@gmail.com>
2021-02-12 22:35:52 +0100

    Create constructor in Bugatti class

--- src/main/java/tb_inheritance_vehicle/Bugatti.java
+++ src/main/java/tb_inheritance_vehicle/Bugatti.java
@@ -2,4 +2,9 @@ package tb_inheritance_vehicle;
 
 public class Bugatti extends SportCar {
     private String type;
+
+    public Bugatti(String name, String size, int doors, int gears, boolean isManual, int roadServiceMonth, String type) {
+        super(name, size, doors, gears, isManual, roadServiceMonth);
+        this.type = type;
+    }
 }
