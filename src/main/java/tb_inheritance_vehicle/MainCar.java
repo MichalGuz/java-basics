dc021762b00dd11fe170b672670f8fb6a7323be0
MichalGuz <michalguz@gmail.com>
2021-02-12 22:42:00 +0100

    Call accelerate method inside main method in MainCar class

--- src/main/java/tb_inheritance_vehicle/MainCar.java
+++ src/main/java/tb_inheritance_vehicle/MainCar.java
@@ -3,5 +3,14 @@ package tb_inheritance_vehicle;
 public class MainCar {
     public static void main(String[] args) {
         Bugatti bugatti = new Bugatti("roadster");
+        bugatti.accelerate(10);
+        bugatti.accelerate(10);
+        bugatti.accelerate(90);
+        bugatti.accelerate(100);
+        bugatti.accelerate(-140);
+        bugatti.accelerate(-70);
+        bugatti.accelerate(10);
+        bugatti.accelerate(-10);
     }
 }
+
