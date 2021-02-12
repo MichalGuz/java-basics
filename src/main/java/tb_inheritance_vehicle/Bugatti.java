275090fe6b476d74dede712eb82f65c3c5571a97
MichalGuz <michalguz@gmail.com>
2021-02-12 22:36:44 +0100

    Amend constructor in Bugatti class

--- src/main/java/tb_inheritance_vehicle/Bugatti.java
+++ src/main/java/tb_inheritance_vehicle/Bugatti.java
@@ -3,8 +3,8 @@ package tb_inheritance_vehicle;
 public class Bugatti extends SportCar {
     private String type;
 
-    public Bugatti(String name, String size, int doors, int gears, boolean isManual, int roadServiceMonth, String type) {
-        super(name, size, doors, gears, isManual, roadServiceMonth);
+    public Bugatti(String type) {
+        super("Veyron", "W16", 3, 7, false, 36);
         this.type = type;
     }
 }
