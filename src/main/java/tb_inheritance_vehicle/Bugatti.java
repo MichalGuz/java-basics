a9e0427096234f3d41555752199299e6e7dd9bff
MichalGuz <michalguz@gmail.com>
2021-02-12 22:38:41 +0100

    Amend accelerate method in Bugatti class

--- src/main/java/tb_inheritance_vehicle/Bugatti.java
+++ src/main/java/tb_inheritance_vehicle/Bugatti.java
@@ -10,6 +10,23 @@ public class Bugatti extends SportCar {
 
     @Override
     public void accelerate(int rate) {
-        super.accelerate(rate);
+        int newVelocity = getCurrentVelocity() + rate;
+        if (newVelocity <= 0) {
+            stop();
+        } else if (newVelocity > 0 && newVelocity <= 10){
+            changeGear(1);
+        } else if (newVelocity > 10 && newVelocity <= 20) {
+            changeGear(2);
+        } else if (newVelocity > 20 && newVelocity <= 30) {
+            changeGear(3);
+        } else if (newVelocity > 30 && newVelocity <= 50) {
+            changeGear(4);
+        } else if (newVelocity > 50 && newVelocity <= 70) {
+            changeGear(5);
+        } else if (newVelocity > 70 && newVelocity <= 100) {
+            changeGear(6);
+        } else if (newVelocity > 100){
+            changeGear(7);
+        }
     }
 }
