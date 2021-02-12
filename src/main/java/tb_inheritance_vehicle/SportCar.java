f2b501d0811ec95e7c8fc1aaecd5af71d61bc4f7
MichalGuz <michalguz@gmail.com>
2021-02-12 22:31:16 +0100

    Create constructor in SportCar class

--- src/main/java/tb_inheritance_vehicle/SportCar.java
+++ src/main/java/tb_inheritance_vehicle/SportCar.java
@@ -3,4 +3,8 @@ package tb_inheritance_vehicle;
 public class SportCar extends Car {
     private int roadServiceMonth;
 
+    public SportCar(String name, String size, int wheels, int doors, int gears, boolean isManual, int roadServiceMonth) {
+        super(name, size, wheels, doors, gears, isManual);
+        this.roadServiceMonth = roadServiceMonth;
+    }
 }
