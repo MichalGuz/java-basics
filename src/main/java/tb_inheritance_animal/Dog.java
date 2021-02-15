64734cd6a7aae1ffa54e0981c32ea67589cd5285
MichalGuz <michalguz@gmail.com>
2021-02-15 21:56:25 +0100

    Create constructor in Dog class

--- src/main/java/tb_inheritance_animal/Dog.java
+++ src/main/java/tb_inheritance_animal/Dog.java
@@ -4,4 +4,11 @@ public class Dog extends Animal {
     private int tail;
     private int eyes;
     private String coat;
+
+    public Dog(String name, int brain, int body, int tail, int eyes, String coat) {
+        super(name, brain, body);
+        this.tail = tail;
+        this.eyes = eyes;
+        this.coat = coat;
+    }
 }
