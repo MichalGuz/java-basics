a37923501c5283fe524f0cb6cbeab59c977474d3
MichalGuz <michalguz@gmail.com>
2021-02-15 21:47:57 +0100

    Create constructor in Animal class

--- src/main/java/tb_inheritance_animal/Animal.java
+++ src/main/java/tb_inheritance_animal/Animal.java
@@ -4,4 +4,10 @@ public class Animal {
     private String name;
     private int brain;
     private int body;
+
+    public Animal(String name, int brain, int body) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+    }
 }
