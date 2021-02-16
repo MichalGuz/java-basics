cb3d3f8d7580c39bbcdbddbd51079f51a8e731d2
MichalGuz <michalguz@gmail.com>
2021-02-16 22:56:04 +0100

    Create constructor in Fish class

--- src/main/java/tb_inheritance_animal/Fish.java
+++ src/main/java/tb_inheritance_animal/Fish.java
@@ -4,4 +4,11 @@ public class Fish extends Animal {
     private int gills;
     private int eyes;
     private int fins;
+
+    public Fish(String name, int brain, int body, int gills, int eyes, int fins) {
+        super(name, brain, body);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
 }
