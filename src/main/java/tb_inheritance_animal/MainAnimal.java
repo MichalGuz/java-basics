dddc289fc4367e34de0fd8773b37c11515e6f785
MichalGuz <michalguz@gmail.com>
2021-02-16 23:07:00 +0100

    Cell moveLegs method in main method in MainAnimal class

--- src/main/java/tb_inheritance_animal/MainAnimal.java
+++ src/main/java/tb_inheritance_animal/MainAnimal.java
@@ -3,5 +3,6 @@ package tb_inheritance_animal;
 public class MainAnimal {
     public static void main(String[] args) {
         Dog dog = new Dog("Burek", 1, 2, "fur");
+        dog.moveLegs(4);
     }
 }
