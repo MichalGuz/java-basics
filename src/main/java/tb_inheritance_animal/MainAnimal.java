d81257c1842629b4aae1ebc78e0ca6e98b42a187
MichalGuz <michalguz@gmail.com>
2021-02-16 23:10:26 +0100

    Instantiate Fish class in MainAnimal class

--- src/main/java/tb_inheritance_animal/MainAnimal.java
+++ src/main/java/tb_inheritance_animal/MainAnimal.java
@@ -4,5 +4,8 @@ public class MainAnimal {
     public static void main(String[] args) {
         Dog dog = new Dog("Burek", 1, 2, "fur");
         dog.moveLegs(4);
+
+        Fish fish = new Fish("Nemo", 2, 2, 3);
+
     }
 }
