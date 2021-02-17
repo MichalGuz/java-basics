7171978ea8279fa3f8c70c0d6b132ca4f717a375
MichalGuz <michalguz@gmail.com>
2021-02-17 23:08:28 +0100

    Create constructor in Cuboid class

--- src/main/java/tb_inheritance_volume_of_pool/Cuboid.java
+++ src/main/java/tb_inheritance_volume_of_pool/Cuboid.java
@@ -2,4 +2,9 @@ package tb_inheritance_volume_of_pool;
 
 public class Cuboid extends Rectangle {
     private double height;
+
+    public Cuboid(double length, double width, double height) {
+        super(length, width);
+        this.height = height;
+    }
 }
