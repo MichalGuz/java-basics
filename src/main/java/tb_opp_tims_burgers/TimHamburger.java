f0f38b4dc318d9abef368b55f8ce21fb93247ecf
MichalGuz <michalguz@gmail.com>
2021-02-18 23:18:53 +0100

    Create addAddition2 method in TimHamburger class

--- src/main/java/tb_opp_tims_burgers/TimHamburger.java
+++ src/main/java/tb_opp_tims_burgers/TimHamburger.java
@@ -27,4 +27,9 @@ public class TimHamburger {
         this.addition1Name = name;
         this.addition1Price = price;
     }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
 }
