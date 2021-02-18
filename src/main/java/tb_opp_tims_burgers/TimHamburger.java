79652ae9965799e9f44133497c8ab01b5307adb7
MichalGuz <michalguz@gmail.com>
2021-02-18 23:14:12 +0100

    Create constructor in TimHamburger class

--- src/main/java/tb_opp_tims_burgers/TimHamburger.java
+++ src/main/java/tb_opp_tims_burgers/TimHamburger.java
@@ -5,4 +5,11 @@ public class TimHamburger {
     private String meat;
     private double price;
     private String breadRollType;
+
+    public TimHamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
 }
