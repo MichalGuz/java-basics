fdacad7398cc2bbbfa4f719cdd55c888f22d56fd
MichalGuz <michalguz@gmail.com>
2021-02-19 22:07:53 +0100

    Create properties in TimHealthyBurger class

--- src/main/java/tb_opp_tims_burgers/TimHealthyBurger.java
+++ src/main/java/tb_opp_tims_burgers/TimHealthyBurger.java
@@ -1,4 +1,9 @@
 package tb_opp_tims_burgers;
 
 public class TimHealthyBurger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
 }
