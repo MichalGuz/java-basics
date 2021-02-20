a1fc3093d15cf5e343d813c7eb3bc68d44fc54c1
MichalGuz <michalguz@gmail.com>
2021-02-20 22:13:04 +0100

    Override methods of TimHamburger class in TimDeluxeBurger class

--- src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
+++ src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
@@ -4,4 +4,24 @@ public class TimDeluxeBurger extends TimHamburger {
     public TimDeluxeBurger() {
         super("deluxe", "bacon", 12.50, "white");
     }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        super.addHamburgerAddition1(name, price);
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        super.addHamburgerAddition2(name, price);
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        super.addHamburgerAddition3(name, price);
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        super.addHamburgerAddition4(name, price);
+    }
 }
