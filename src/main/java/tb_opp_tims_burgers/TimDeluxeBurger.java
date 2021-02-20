852a138bb442fa1e7ccdf56c1f86319ec158ee03
MichalGuz <michalguz@gmail.com>
2021-02-20 22:08:20 +0100

    Create constructor TimDeluxeBurger class

--- src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
+++ src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
@@ -1,5 +1,7 @@
 package tb_opp_tims_burgers;
 
 public class TimDeluxeBurger extends TimHamburger {
-
+    public TimDeluxeBurger(String name, String meat, double price, String breadRollType) {
+        super(name, meat, price, breadRollType);
+    }
 }
