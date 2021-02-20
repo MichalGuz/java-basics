c5878016e0c087cc7051dd77b20b9557c6170eff
MichalGuz <michalguz@gmail.com>
2021-02-20 22:10:07 +0100

    Amend the constructor inTimDeluxeBurger class

--- src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
+++ src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
@@ -1,7 +1,7 @@
 package tb_opp_tims_burgers;
 
 public class TimDeluxeBurger extends TimHamburger {
-    public TimDeluxeBurger(String name, String meat, double price, String breadRollType) {
-        super(name, meat, price, breadRollType);
+    public TimDeluxeBurger() {
+        super("deluxe", "bacon", 12.50, "white");
     }
 }
