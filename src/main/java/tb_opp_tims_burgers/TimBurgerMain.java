75d31a554de0ccbc8a48d7cbdfee439796e398ac
MichalGuz <michalguz@gmail.com>
2021-02-20 22:21:13 +0100

    Call addHamburgerAddition[1-3] methods in main method in TimBurgerMain class

--- src/main/java/tb_opp_tims_burgers/TimBurgerMain.java
+++ src/main/java/tb_opp_tims_burgers/TimBurgerMain.java
@@ -3,5 +3,8 @@ package tb_opp_tims_burgers;
 public class TimBurgerMain {
     public static void main(String[] args) {
         TimHamburger hamburger = new TimHamburger();
+        hamburger.addHamburgerAddition1("lettuce", 0.45);
+        hamburger.addHamburgerAddition2("tomato",0.65);
+        hamburger.addHamburgerAddition3("barbecue sauce", 0.30);
     }
 }
