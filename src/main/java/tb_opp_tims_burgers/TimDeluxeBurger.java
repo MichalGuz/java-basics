609ec1d05d35f9d7904500e85f2bbf9ed102231a
MichalGuz <michalguz@gmail.com>
2021-02-20 22:27:58 +0100

    Amend the constructor in TimDeluxeBurger class

--- src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
+++ src/main/java/tb_opp_tims_burgers/TimDeluxeBurger.java
@@ -3,9 +3,11 @@ package tb_opp_tims_burgers;
 public class TimDeluxeBurger extends TimHamburger {
     public TimDeluxeBurger() {
         super("deluxe", "bacon", 12.50, "white");
+        super.addHamburgerAddition1("chips", 1.75);
+        super.addHamburgerAddition2("drink", 0.90);
     }
 
-    @Override
+        @Override
     public void addHamburgerAddition1(String name, double price) {
         System.out.println("Cannot add additional items to a deluxe burger");
     }
