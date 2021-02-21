0864c54bd282ddecf683a71090f487eb30dea00b
MichalGuz <michalguz@gmail.com>
2021-02-21 20:23:46 +0100

    Create constructor in ConstructorBankAccount class

--- src/main/java/tb_constructors_various_practices/ConstructorBankAccount.java
+++ src/main/java/tb_constructors_various_practices/ConstructorBankAccount.java
@@ -6,4 +6,13 @@ public class ConstructorBankAccount {
     private String nameOfCustomer;
     private String emailOfCustomer;
     private String phoneNumberOfCustomer;
+
+    public ConstructorBankAccount(String accountNumber, double balance, String nameOfCustomer, String emailOfCustomer, String phoneNumberOfCustomer) {
+        System.out.println("ConstructorBankAccount constructor with parameters called.");
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.nameOfCustomer = nameOfCustomer;
+        this.emailOfCustomer = emailOfCustomer;
+        this.phoneNumberOfCustomer = phoneNumberOfCustomer;
+    }
 }
