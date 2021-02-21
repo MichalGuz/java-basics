173b987752b4a5c6a9760a6c9f0a70c005713fe0
MichalGuz <michalguz@gmail.com>
2021-02-21 20:25:07 +0100

    Create getters in ConstructorBankAccount class

--- src/main/java/tb_constructors_various_practices/ConstructorBankAccount.java
+++ src/main/java/tb_constructors_various_practices/ConstructorBankAccount.java
@@ -20,4 +20,24 @@ public class ConstructorBankAccount {
         this("1234", 100.0,"Default name", "Default email", "Default phone number");
         System.out.println("ConstructorBankAccount constructor with default parameters called");
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getNameOfCustomer() {
+        return nameOfCustomer;
+    }
+
+    public String getEmailOfCustomer() {
+        return emailOfCustomer;
+    }
+
+    public String getPhoneNumberOfCustomer() {
+        return phoneNumberOfCustomer;
+    }
 }
