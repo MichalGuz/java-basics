277642bf50d288c1989eb18d45ff3ab51f073a21
MichalGuz <michalguz@gmail.com>
2021-02-21 19:17:31 +0100

    Create constructor of EncapPlayer class

--- src/main/java/tb_encapsulation/EncapPlayer.java
+++ src/main/java/tb_encapsulation/EncapPlayer.java
@@ -4,4 +4,10 @@ public class EncapPlayer {
     private String name;
     private int hitPoint = 100;
     private int weapon;
+
+    public EncapPlayer(String name, int hitPoint, int weapon) {
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.weapon = weapon;
+    }
 }
