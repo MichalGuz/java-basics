2cdb9360589b45182c852cb4d85572098251d681
MichalGuz <michalguz@gmail.com>
2021-02-21 19:19:31 +0100

    Amend constructor of EncapPlayer class

--- src/main/java/tb_encapsulation/EncapPlayer.java
+++ src/main/java/tb_encapsulation/EncapPlayer.java
@@ -5,9 +5,11 @@ public class EncapPlayer {
     private int hitPoint = 100;
     private int weapon;
 
-    public EncapPlayer(String name, int hitPoint, int weapon) {
+    public EncapPlayer(String name, int health, int weapon) {
         this.name = name;
-        this.hitPoint = hitPoint;
+        if (health > 0 && health <= 100) {
+            this.hitPoint = health;
+        }
         this.weapon = weapon;
     }
 }
