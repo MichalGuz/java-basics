a33f7bc16233f7cc2822761c81efeeb4a28d1648
MichalGuz <michalguz@gmail.com>
2021-02-21 20:22:46 +0100

    Create properties in ConstructorBankAccount class

--- src/main/java/tb_constructors_various_practices/ConstructorBankAccount.java
+++ src/main/java/tb_constructors_various_practices/ConstructorBankAccount.java
@@ -1,4 +1,9 @@
 package tb_constructors_various_practices;
 
 public class ConstructorBankAccount {
+    private String accountNumber;
+    private double balance;
+    private String nameOfCustomer;
+    private String emailOfCustomer;
+    private String phoneNumberOfCustomer;
 }
