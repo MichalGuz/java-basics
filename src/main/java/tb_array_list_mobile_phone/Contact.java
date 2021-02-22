56b90be2ab4e5e8a9fcb897cb036759f30ae760f
MichalGuz <michalguz@gmail.com>
2021-02-22 22:02:31 +0100

    Create constructor in Contact class

--- src/main/java/tb_array_list_mobile_phone/Contact.java
+++ src/main/java/tb_array_list_mobile_phone/Contact.java
@@ -3,4 +3,9 @@ package tb_array_list_mobile_phone;
 public class Contact {
     private String name;
     private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
