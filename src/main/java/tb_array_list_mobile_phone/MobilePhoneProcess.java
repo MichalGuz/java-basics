166fa1eddaf34b1c905291d66a5e03adbe5b95f0
MichalGuz <michalguz@gmail.com>
2021-02-24 23:10:48 +0100

    Instantiate Scaner class in MobilePhoneProcess class

--- src/main/java/tb_array_list_mobile_phone/MobilePhoneProcess.java
+++ src/main/java/tb_array_list_mobile_phone/MobilePhoneProcess.java
@@ -1,5 +1,9 @@
 package tb_array_list_mobile_phone;
 
+import java.util.Scanner;
+
 public class MobilePhoneProcess {
     MobilePhone mobilePhone = new MobilePhone("999111999");
+
+    Scanner scanner = new Scanner(System.in);
 }
