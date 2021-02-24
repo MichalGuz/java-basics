aeac254ff928b12802df455b57472cf7236b818b
MichalGuz <michalguz@gmail.com>
2021-02-24 23:13:24 +0100

    Call println method in the body of printInstructions method in MobilePhoneProcess class

--- src/main/java/tb_array_list_mobile_phone/MobilePhoneProcess.java
+++ src/main/java/tb_array_list_mobile_phone/MobilePhoneProcess.java
@@ -11,5 +11,13 @@ public class MobilePhoneProcess {
     }
 
     private void printInstructions() {
+        System.out.println("Available actions:\npress:");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a new contact\n" +
+                "3 - to update an existing contact\n" +
+                "4 - to remove an existing contact\n" +
+                "5 - to query an existing contact\n" +
+                "6 - to print a list of available actions");
     }
 }
