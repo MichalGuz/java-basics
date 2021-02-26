f227ce9a53ed117d17109ddbb7605f104269aa2f
MichalGuz <michalguz@gmail.com>
2021-02-26 22:51:55 +0100

    Create printInstructions method in MobPhoneLonger class

--- src/main/java/tb_array_list_mobile_phone/MobPhoneLonger.java
+++ src/main/java/tb_array_list_mobile_phone/MobPhoneLonger.java
@@ -1,4 +1,14 @@
 package tb_array_list_mobile_phone;
 
 public class MobPhoneLonger {
+    public void printInstructions(){
+        System.out.println("Press ");
+        System.out.println("0 - To show the instructions." );
+        System.out.println("1 - To print the list of contacts.");
+        System.out.println("2 - To add a contact to the list.");
+        System.out.println("3 - To modify a contact in the list.");
+        System.out.println("4 - To remove a contact from the list.");
+        System.out.println("5 - To search for a contact in the list.");
+        System.out.println("6 - To quit the application.");
+    }
 }
