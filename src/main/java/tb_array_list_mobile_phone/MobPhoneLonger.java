26478b620dcd4534f8c0d0170c2c72caace054a8
MichalGuz <michalguz@gmail.com>
2021-02-27 22:57:38 +0100

    Create if condition with call of searchOnList method in updateContact method in MobPhoneLonger class

--- src/main/java/tb_array_list_mobile_phone/MobPhoneLonger.java
+++ src/main/java/tb_array_list_mobile_phone/MobPhoneLonger.java
@@ -36,6 +36,8 @@ public class MobPhoneLonger {
     public void updateContact() {
         System.out.print("Choose contact to update - enter the name:\n");
         String name = scanner.nextLine();
+        if(searchOnList(name)) {
+        }
     }
 
         private void add(Contact contact) {
