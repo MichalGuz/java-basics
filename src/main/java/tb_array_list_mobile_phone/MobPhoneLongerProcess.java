70c971397ec53a09d4aac64ab6b4b1957ce32212
MichalGuz <michalguz@gmail.com>
2021-02-28 22:53:23 +0100

    Create new instances of MobPhoneLonger and Scanner class MobPhoneLongerProcess class

--- src/main/java/tb_array_list_mobile_phone/MobPhoneLongerProcess.java
+++ src/main/java/tb_array_list_mobile_phone/MobPhoneLongerProcess.java
@@ -1,4 +1,8 @@
 package tb_array_list_mobile_phone;
 
+import java.util.Scanner;
+
 public class MobPhoneLongerProcess {
+    MobPhoneLonger mobPhoneLonger = new MobPhoneLonger();
+    Scanner scanner = new Scanner(System.in);
 }
