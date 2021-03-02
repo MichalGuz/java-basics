96497193e672657b00868b6e36b55ec11337ffc4
MichalGuz <michalguz@gmail.com>
2021-03-02 23:05:30 +0100

    Create readInteger method with call of nextInt method in FindMin class

--- src/main/java/tb_arrays/FindMin.java
+++ src/main/java/tb_arrays/FindMin.java
@@ -5,4 +5,9 @@ import java.util.Scanner;
 public class FindMin {
     static Scanner scanner = new Scanner(System.in);
 
+    private static int readInteger(){
+        System.out.println("Enter an integer:");
+        int elements = scanner.nextInt();
+        return elements;
+    }
 }
