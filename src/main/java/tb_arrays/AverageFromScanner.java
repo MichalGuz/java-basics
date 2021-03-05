c5a105017fad58d8bf12c3c1672422ca9aa4378c
MichalGuz <michalguz@gmail.com>
2021-03-05 23:00:31 +0100

    Add up whole the values from the array im for loop in getAverage method in AverageFromScanner class

--- src/main/java/tb_arrays/AverageFromScanner.java
+++ src/main/java/tb_arrays/AverageFromScanner.java
@@ -21,6 +21,7 @@ public class AverageFromScanner {
     public static double getAverage(int[] numbers) {
         int sum = 0;
         for(int i=0; i<numbers.length; i++) {
+            sum += numbers[i];
         }
     }
 }
