fdfdd3f4c5dfc040e85feab68b4f5db0e846c2b2
MichalGuz <michalguz@gmail.com>
2021-03-05 22:54:27 +0100

    Create for loop in getNumbers method in AverageFromScanner class

--- src/main/java/tb_arrays/AverageFromScanner.java
+++ src/main/java/tb_arrays/AverageFromScanner.java
@@ -11,5 +11,8 @@ public class AverageFromScanner {
     public static int[] getNumbers(int number) {
         System.out.println("Enter " + number + " integer values.");
         int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+        }
     }
 }
