2e377688457f6220a62f46508b64d8af922e92a4
MichalGuz <michalguz@gmail.com>
2021-03-08 21:41:04 +0100

    Create getNumbers method in DescendingSortedArray class

--- src/main/java/tb_arrays/DescendingSortedArray.java
+++ src/main/java/tb_arrays/DescendingSortedArray.java
@@ -4,4 +4,7 @@ import java.util.Scanner;
 
 public class DescendingSortedArray {
     private static Scanner scanner = new Scanner(System.in);
+
+    public static int[] getNumbers(int capacity) {
+    }
 }
