c5f0887a879aa03ef8ab0b1ff599d9140cb28417
MichalGuz <michalguz@gmail.com>
2021-03-09 22:30:02 +0100

    Call println method in fillIntArray method in PrintArray class

--- src/main/java/tb_arrays/PrintArray.java
+++ src/main/java/tb_arrays/PrintArray.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class PrintArray {
     public static void fillIntArray() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to set the range of array:");
         int range = scanner.nextInt();
+
     }
 
 
