e92c553a2ff57f371f19dc7eb1bc9bc4c15a5959
MichalGuz <michalguz@gmail.com>
2021-03-09 22:27:02 +0100

    Create fillIntArray method in PrintArray class

--- src/main/java/tb_arrays/PrintArray.java
+++ src/main/java/tb_arrays/PrintArray.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class PrintArray {
     Scanner scanner = new Scanner(System.in);
+
+    public static void fillIntArray() {}
     int range = scanner.nextInt();
+
 
 }
