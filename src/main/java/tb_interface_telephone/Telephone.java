819b0a08d058d678ce484ba5ad22eab5f34379fb
MichalGuz <michalguz@gmail.com>
2021-05-05 22:52:31 +0200

    Create methods answer and callPhone in interface Telephone.

--- src/main/java/tb_interface_telephone/Telephone.java
+++ src/main/java/tb_interface_telephone/Telephone.java
@@ -3,4 +3,6 @@ package tb_interface_telephone;
 public interface Telephone {
     public void powerOn();
     public void dial(int phoneNumber);
+    public void answer();
+    public boolean callPhone(int phoneNumber);
 }
