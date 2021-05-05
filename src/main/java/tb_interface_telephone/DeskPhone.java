d691928f6169d99a8752d3f0493148354ca4c7d2
MichalGuz <michalguz@gmail.com>
2021-05-05 22:59:43 +0200

    Create two fields and a constructor in DeskPhone class

--- src/main/java/tb_interface_telephone/DeskPhone.java
+++ src/main/java/tb_interface_telephone/DeskPhone.java
@@ -1,5 +1,10 @@
 package tb_interface_telephone;
 
 public class DeskPhone implements Telephone {
+    private int myNumber;
+    private boolean isRinging;
 
+    public DeskPhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
 }
