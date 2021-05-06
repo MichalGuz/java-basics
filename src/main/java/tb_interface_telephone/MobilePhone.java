f1012708564788d019ff9cfd088cce3306caa266
MichalGuz <michalguz@gmail.com>
2021-05-06 23:54:05 +0200

    Create constructor in MobilePhone class

--- src/main/java/tb_interface_telephone/MobilePhone.java
+++ src/main/java/tb_interface_telephone/MobilePhone.java
@@ -1,4 +1,11 @@
 package tb_interface_telephone;
 
 public class MobilePhone implements Telephone {
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn;
+
+    public MobilePhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
 }
