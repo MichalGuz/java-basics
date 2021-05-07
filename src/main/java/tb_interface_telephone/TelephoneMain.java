22637d5e5368140e6c8b4f68b16371340923613f
MichalGuz <michalguz@gmail.com>
2021-05-07 22:58:04 +0200

    Instantiate DeskPhone class in main method and call its methods

--- src/main/java/tb_interface_telephone/TelephoneMain.java
+++ src/main/java/tb_interface_telephone/TelephoneMain.java
@@ -3,5 +3,10 @@ package tb_interface_telephone;
 public class TelephoneMain {
     public static void main(String[] args) {
         Telephone telephone;
+
+        telephone = new DeskPhone(564564544);
+        telephone.powerOn();
+        telephone.callPhone(564564544);
+        telephone.answer();
     }
 }
