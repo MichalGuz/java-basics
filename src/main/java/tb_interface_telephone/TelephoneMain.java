c0d2bcc1d742cf2a480f7e20168f3fa79f97f10f
MichalGuz <michalguz@gmail.com>
2021-05-07 23:38:13 +0200

    Call isRinging() method in TelephoneMain class

--- src/main/java/tb_interface_telephone/TelephoneMain.java
+++ src/main/java/tb_interface_telephone/TelephoneMain.java
@@ -11,8 +11,9 @@ public class TelephoneMain {
 
         telephone = new MobilePhone(433245454);
         telephone.powerOn();
-        telephone.dial(777334585);
+        telephone.dial(433245454);
         telephone.callPhone(777334585);
+        telephone.isRinging();
         telephone.answer();
     }
 }
