7675db9f59092ad1334af439a69f729951523707
MichalGuz <michalguz@gmail.com>
2021-05-11 23:29:27 +0200

    Call println and  showSupplierMessage methods and after that call setters in  main method in MainSaveable class

--- src/main/java/tb_interface_savable/MainSaveable.java
+++ src/main/java/tb_interface_savable/MainSaveable.java
@@ -3,6 +3,10 @@ package tb_interface_savable;
 public class MainSaveable {
     public static void main(String[] args) {
         Supplier one = new Supplier("one", 111, 1111, "Big_one");
+        System.out.println(one);
+        System.out.println(one.showSupplierMessage());
+        one.setAccount(1001);
+        one.setLogin("big1");
     }
 }
 
