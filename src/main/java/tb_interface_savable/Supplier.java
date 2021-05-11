9def75c9d3fd4539b9edb2d59d97981efd532b42
MichalGuz <michalguz@gmail.com>
2021-05-11 23:11:27 +0200

    Create fields inSupplier class

--- src/main/java/tb_interface_savable/Supplier.java
+++ src/main/java/tb_interface_savable/Supplier.java
@@ -1,4 +1,8 @@
 package tb_interface_savable;
 
 public class Supplier implements Saveable {
+    private String name;
+    private int number;
+    private int account;
+    private String login;
 }
