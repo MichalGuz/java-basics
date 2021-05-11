0f4df524eec6c7ce1d4417fc95de684be3f45034
MichalGuz <michalguz@gmail.com>
2021-05-11 23:26:15 +0200

    Create main method and intantiate Supplier class in MainSaveable class

--- src/main/java/tb_interface_savable/MainSaveable.java
+++ src/main/java/tb_interface_savable/MainSaveable.java
@@ -1,4 +1,8 @@
 package tb_interface_savable;
 
 public class MainSaveable {
+    public static void main(String[] args) {
+        Supplier one = new Supplier("one", 111, 1111, "Big_one");
+    }
 }
+
