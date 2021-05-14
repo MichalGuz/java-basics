a442c62934c4c9e9df791066e69aa78c2a2f8182
MichalGuz <michalguz@gmail.com>
2021-05-14 22:26:21 +0200

    Create new Supplier object in main method in MainSaveable class

--- src/main/java/tb_interface_savable/MainSaveable.java
+++ src/main/java/tb_interface_savable/MainSaveable.java
@@ -12,6 +12,9 @@ public class MainSaveable {
         Customer two = new Customer("two", 112, 600500600, "two@two.org");
         System.out.println(two);
         System.out.println(two.showCustomerMessage());
+
+        Saveable three = new Supplier("three", 113, 3333, "mat3");
+
     }
 }
 
