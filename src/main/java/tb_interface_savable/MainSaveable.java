83c192ae29e74975151504e10b14faea0bf9c36d
MichalGuz <michalguz@gmail.com>
2021-05-14 22:49:10 +0200

    Correct the indentation in MainSaveable class

--- src/main/java/tb_interface_savable/MainSaveable.java
+++ src/main/java/tb_interface_savable/MainSaveable.java
@@ -18,43 +18,44 @@ public class MainSaveable {
 
         Saveable three = new Supplier("three", 113, 3333, "mat3");
         System.out.println(three);
-        System.out.println(((Supplier)three).showSupplierMessage());
+        System.out.println(((Supplier) three).showSupplierMessage());
 
         Saveable four = new Customer("four", 114, 4444, "Ropex");
         System.out.println(four);
-        System.out.println(((Customer)four).showCustomerMessage());;
+        System.out.println(((Customer) four).showCustomerMessage());
+        ;
+    }
 
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
 
-        public static ArrayList<String> readValues() {
-            ArrayList<String> values = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
 
-            Scanner scanner = new Scanner(System.in);
-            boolean quit = false;
-            int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a number\n" +
+                "0 to quit");
 
-            System.out.println("Choose\n" +
-                    "1 to enter a number\n" +
-                    "0 to quit");
-
-            while (!quit) {
-                System.out.println("Choose an option 0 or 1 four times in a row and then 0:");
-                int choice = scanner.nextInt();
-                scanner.nextLine();
-                switch (choice) {
-                    case 0:
-                        quit = true;
-                        break;
-                    case 1:
-                        System.out.println("Enter a number");
-                        String stringInput = scanner.nextLine();
-                        values.add(index, stringInput);
-                        index++;
-                        break;
-                }
+        while (!quit) {
+            System.out.println("Choose an option 0 or 1 four times in a row and then 0:");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a number");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
             }
-            return values;
         }
+        return values;
     }
 }
+
 
 
