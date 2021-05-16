c5a102367b8157d4694f2e657ce06187d5bba394
MichalGuz <michalguz@gmail.com>
2021-05-16 23:44:20 +0200

    Create main method in ArrayReferenceOrPrimitiveValee and initialize two int variables

--- src/main/java/tb_arrays/ArrayReferenceOrPrimitiveValue.java
+++ src/main/java/tb_arrays/ArrayReferenceOrPrimitiveValue.java
@@ -1,4 +1,10 @@
 package tb_arrays;
 
 public class ArrayReferenceOrPrimitiveValue {
+    public static void main(String[] args) {
+        // primitive variables "holds" its values
+        int value = 10;
+        int anotherValue = value;
+
+    }
 }
