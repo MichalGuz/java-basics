ad3618ac3f2b4bd8f62977cb6dc5e286bcac955c
MichalGuz <michalguz@gmail.com>
2021-05-18 22:07:29 +0200

    Create changeAge method in ReferenceVsPrimitive

--- src/main/java/tb_arrays/ReferenceAndPrimitive.java
+++ src/main/java/tb_arrays/ReferenceAndPrimitive.java
@@ -6,4 +6,10 @@ public class ReferenceAndPrimitive {
         int age = 18;
         System.out.println(age); // 18
         }
+
+    // variable primitive
+    static void changeAge(int age) {
+        age = 21;
+        System.out.println(age); // 21
+    }
 }
