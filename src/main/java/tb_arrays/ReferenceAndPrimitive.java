237f8c99a40070ff3271c59e3e954e98ee98a14a
MichalGuz <michalguz@gmail.com>
2021-05-18 22:06:58 +0200

    Create testAge method in ReferenceVsPrimitive

--- src/main/java/tb_arrays/ReferenceAndPrimitive.java
+++ src/main/java/tb_arrays/ReferenceAndPrimitive.java
@@ -1,4 +1,9 @@
 package tb_arrays;
 
 public class ReferenceAndPrimitive {
+    // variable primitive
+    static void testAge() {
+        int age = 18;
+        System.out.println(age); // 18
+        }
 }
