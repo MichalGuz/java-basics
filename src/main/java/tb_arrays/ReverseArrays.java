629ebb7eae8e41ece27e2cef49baa767767ac8bc
MichalGuz <michalguz@gmail.com>
2021-05-19 22:50:43 +0200

    Create enterNumberOfElements method in ReverseArrays class

--- src/main/java/tb_arrays/ReverseArrays.java
+++ src/main/java/tb_arrays/ReverseArrays.java
@@ -4,4 +4,8 @@ import java.util.Scanner;
 
 public class ReverseArrays {
     private static Scanner scanner = new Scanner(System.in);
+
+    public static int enterNumberOfElements() {
+        return 0;
+    }
 }
